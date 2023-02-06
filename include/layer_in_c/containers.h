163da6377bf2610965fc0ac3b4bb646aa18c8411
Jonas Eschmann <jonas.eschmann@gmail.com>
2023-02-05 20:55:53 -0500

    breaking out matrix layout

--- include/layer_in_c/containers.h
+++ include/layer_in_c/containers.h
@@ -11,17 +11,23 @@ namespace layer_in_c{
         RowMajor,
         ColMajor,
     };
-    template <typename T_T, typename T_TI, T_TI T_ROWS, T_TI T_COLS, T_TI T_ROW_PITCH=T_COLS, T_TI T_COL_PITCH=1> // row-major by default
+    template <typename TI, TI ROWS, TI COLS, TI ALIGNMENT = 1>
+    struct RowMajorAligned{
+        static constexpr TI ROW_PITCH = (COLS + ALIGNMENT - 1) / ALIGNMENT * ALIGNMENT;
+        static constexpr TI COL_PITCH = 1;
+    };
+    template <typename T_T, typename T_TI, T_TI T_ROWS, T_TI T_COLS, typename T_LAYOUT = RowMajorAligned<T_TI, T_ROWS, T_COLS>> // row-major by default
     struct MatrixSpecification{
         using T = T_T;
         using TI = T_TI;
-        static_assert(T_COL_PITCH * T_COLS <= T_ROW_PITCH || T_ROW_PITCH * T_ROWS <= T_COL_PITCH, "Pitches of the matrix dimensions are not compatible");
-        static constexpr TI SIZE = std::max(T_COL_PITCH * T_COLS, T_ROW_PITCH * T_ROWS);
+        using LAYOUT = T_LAYOUT;
+        static constexpr TI ROW_PITCH = LAYOUT::ROW_PITCH;
+        static constexpr TI COL_PITCH = LAYOUT::COL_PITCH;
+        static_assert(COL_PITCH * T_COLS <= ROW_PITCH || ROW_PITCH * T_ROWS <= COL_PITCH, "Pitches of the matrix dimensions are not compatible");
+        static constexpr TI SIZE = std::max(COL_PITCH * T_COLS, ROW_PITCH * T_ROWS);
         static constexpr TI SIZE_BYTES = SIZE * sizeof(T);
         static constexpr TI ROWS = T_ROWS;
         static constexpr TI COLS = T_COLS;
-        static constexpr TI ROW_PITCH = T_ROW_PITCH;
-        static constexpr TI COL_PITCH = T_COL_PITCH;
     };
     template<typename T_SPEC>
     struct Matrix{
