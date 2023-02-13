0a491265e993f651422e49de2882471338130818
Jonas Eschmann <jonas.eschmann@gmail.com>
2023-02-13 17:05:08 -0500

    modularizing replay buffer test

--- tests/src/rl/components/replay_buffer.cpp
+++ tests/src/rl/components/replay_buffer.cpp
@@ -3,6 +3,9 @@
 #include <layer_in_c/rl/components/replay_buffer/operations_cpu.h>
 #include <layer_in_c/rl/components/replay_buffer/persist.h>
 
+#include "replay_buffer.h"
+
+
 #include <gtest/gtest.h>
 #include <highfive/H5File.hpp>
 
@@ -24,17 +27,7 @@ TEST(LAYER_IN_C_RL_COMPONENTS_REPLAY_BUFFER, PERSISTENCE) {
     auto rng = lic::random::default_engine(DEVICE::SPEC::RANDOM());
     {
         lic::malloc(device, replay_buffer_1);
-        lic::randn(device, replay_buffer_1.observations, rng);
-        lic::randn(device, replay_buffer_1.actions, rng);
-        lic::randn(device, replay_buffer_1.next_observations, rng);
-        lic::randn(device, replay_buffer_1.rewards, rng);
-
-        for (typename DEVICE::index_t row_i = 0; row_i < CAPACITY; row_i++) {
-            bool terminated = lic::random::normal_distribution(DEVICE::SPEC::RANDOM(), (DTYPE) 0, (DTYPE) 1, rng) > 0.5;
-            bool truncated = terminated || lic::random::normal_distribution(DEVICE::SPEC::RANDOM(), (DTYPE) 0, (DTYPE) 1, rng) > 0.5;
-            set(replay_buffer_1.terminated, row_i, 0, terminated);
-            set(replay_buffer_1.truncated, row_i, 0, truncated);
-        }
+        lic::test::rl::components::replay_buffer::sample(device, replay_buffer_1, rng);
         set(replay_buffer_1.next_observations, 7, 0, 1337);
         auto data_file = HighFive::File(replay_buffer_path, HighFive::File::Overwrite);
         lic::save(device, replay_buffer_1, data_file.createGroup("replay_buffer"));
---
+++ tests/src/rl/components/replay_buffer.h
@@ -0,0 +1,19 @@
+#include <layer_in_c/rl/components/replay_buffer/replay_buffer.h>
+
+namespace layer_in_c::test::rl::components::replay_buffer{
+    template <typename DEVICE, typename SPEC, typename RNG>
+    void sample(DEVICE& device, layer_in_c::rl::components::ReplayBuffer<SPEC>& rb, RNG& rng){
+        using T = typename SPEC::T;
+        randn(device, rb.observations, rng);
+        randn(device, rb.actions, rng);
+        randn(device, rb.next_observations, rng);
+        randn(device, rb.rewards, rng);
+
+        for (typename DEVICE::index_t row_i = 0; row_i < SPEC::CAPACITY; row_i++) {
+            bool terminated = random::normal_distribution(typename DEVICE::SPEC::RANDOM(), (T) 0, (T) 1, rng) > 0.5;
+            bool truncated = terminated || random::normal_distribution(typename DEVICE::SPEC::RANDOM(), (T) 0, (T) 1, rng) > 0.5;
+            set(rb.terminated, row_i, 0, terminated);
+            set(rb.truncated, row_i, 0, truncated);
+        }
+    }
+}
