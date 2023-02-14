93a25743a3557c7349d1ff0a84bea5c1b9119293
Jonas Eschmann <jonas.eschmann@gmail.com>
2023-02-14 11:10:21 -0500

    test fixture

--- tests/src/rl/cuda/rl.cu
+++ tests/src/rl/cuda/rl.cu
@@ -31,13 +31,13 @@
 
 namespace lic = layer_in_c;
 
-
-TEST(LAYER_IN_C_RL_CUDA, GATHER_BATCH) {
+class LAYER_IN_C_RL_CUDA : public ::testing::Test {
+public:
     using DEVICE_CPU = lic::devices::DefaultCPU;
     using DEVICE_GPU = lic::devices::DefaultCUDA;
     using DTYPE = float;
-    constexpr DEVICE_CPU::index_t CAPACITY = 2000;
-    constexpr DEVICE_CPU::index_t BATCH_SIZE = 20;
+    static constexpr DEVICE_CPU::index_t CAPACITY = 2000;
+    static constexpr DEVICE_CPU::index_t BATCH_SIZE = 20;
 //    using REPLAY_BUFFER_SPEC = lic::rl::components::replay_buffer::Specification<DTYPE, DEVICE_CPU::index_t, OBSERVATION_DIM, ACTION_DIM, CAPACITY>;
 //    using REPLAY_BUFFER = lic::rl::components::ReplayBuffer<REPLAY_BUFFER_SPEC>;
     using PENDULUM_SPEC = lic::rl::environments::pendulum::Specification<DTYPE, DEVICE_CPU::index_t, lic::rl::environments::pendulum::DefaultParameters<DTYPE>>;
@@ -48,8 +48,6 @@ TEST(LAYER_IN_C_RL_CUDA, GATHER_BATCH) {
     using BATCH_TYPE = lic::rl::components::off_policy_runner::Batch<BATCH_SPEC>;
     DEVICE_CPU device_cpu;
     DEVICE_GPU device_gpu;
-    auto rng_cpu = lic::random::default_engine(DEVICE_CPU::SPEC::RANDOM());
-    auto rng_gpu = lic::random::default_engine(DEVICE_GPU::SPEC::RANDOM());
     OFF_POLICY_RUNNER_TYPE off_policy_runner_cpu;
     OFF_POLICY_RUNNER_TYPE off_policy_runner_cpu_2;
     OFF_POLICY_RUNNER_TYPE off_policy_runner_gpu_cpu;
@@ -57,27 +55,49 @@ TEST(LAYER_IN_C_RL_CUDA, GATHER_BATCH) {
     BATCH_TYPE batch_cpu, batch_cpu_2;
     BATCH_TYPE batch_gpu;
     BATCH_TYPE* batch_gpu_struct;
+protected:
+    void SetUp() override {
 
-    // alloc
-    lic::malloc(device_cpu, off_policy_runner_cpu);
-    lic::malloc(device_cpu, off_policy_runner_cpu_2);
-    lic::malloc(device_gpu, off_policy_runner_gpu_cpu);
-    lic::malloc(device_cpu, batch_cpu);
-    lic::malloc(device_cpu, batch_cpu_2);
-    lic::malloc(device_gpu, batch_gpu);
-    cudaMalloc(&off_policy_runner_gpu_struct, sizeof(OFF_POLICY_RUNNER_TYPE));
-    cudaMalloc(&batch_gpu_struct, sizeof(BATCH_TYPE));
+        auto rng_cpu = lic::random::default_engine(DEVICE_CPU::SPEC::RANDOM());
+        auto rng_gpu = lic::random::default_engine(DEVICE_GPU::SPEC::RANDOM());
+        // alloc
+        lic::malloc(device_cpu, off_policy_runner_cpu);
+        lic::malloc(device_cpu, off_policy_runner_cpu_2);
+        lic::malloc(device_gpu, off_policy_runner_gpu_cpu);
+        lic::malloc(device_cpu, batch_cpu);
+        lic::malloc(device_cpu, batch_cpu_2);
+        lic::malloc(device_gpu, batch_gpu);
+        cudaMalloc(&off_policy_runner_gpu_struct, sizeof(OFF_POLICY_RUNNER_TYPE));
+        cudaMalloc(&batch_gpu_struct, sizeof(BATCH_TYPE));
 
-    // init
-    for(DEVICE_CPU::index_t rb_i = 0; rb_i < OFF_POLICY_RUNNER_SPEC::N_ENVIRONMENTS; rb_i++) {
-        lic::test::rl::components::replay_buffer::sample(device_cpu, off_policy_runner_cpu.replay_buffers[rb_i], rng_cpu);
-        lic::copy(device_gpu, device_cpu, off_policy_runner_gpu_cpu.replay_buffers[rb_i], off_policy_runner_cpu.replay_buffers[rb_i]);
+        // init
+        for(DEVICE_CPU::index_t rb_i = 0; rb_i < OFF_POLICY_RUNNER_SPEC::N_ENVIRONMENTS; rb_i++) {
+            lic::test::rl::components::replay_buffer::sample(device_cpu, off_policy_runner_cpu.replay_buffers[rb_i], rng_cpu);
+            lic::copy(device_gpu, device_cpu, off_policy_runner_gpu_cpu.replay_buffers[rb_i], off_policy_runner_cpu.replay_buffers[rb_i]);
+        }
+
+        // copy
+        cudaMemcpy(off_policy_runner_gpu_struct, &off_policy_runner_gpu_cpu, sizeof(OFF_POLICY_RUNNER_TYPE), cudaMemcpyHostToDevice);
+        cudaMemcpy(batch_gpu_struct, &batch_gpu, sizeof(BATCH_TYPE), cudaMemcpyHostToDevice);
     }
 
-    // copy
-    cudaMemcpy(off_policy_runner_gpu_struct, &off_policy_runner_gpu_cpu, sizeof(OFF_POLICY_RUNNER_TYPE), cudaMemcpyHostToDevice);
-    cudaMemcpy(batch_gpu_struct, &batch_gpu, sizeof(BATCH_TYPE), cudaMemcpyHostToDevice);
+    void TearDown() override {
+        lic::free(device_cpu, off_policy_runner_cpu);
+        lic::free(device_cpu, off_policy_runner_cpu_2);
+        lic::free(device_gpu, off_policy_runner_gpu_cpu);
+        lic::free(device_cpu, batch_cpu);
+        lic::free(device_cpu, batch_cpu_2);
+        lic::free(device_gpu, batch_gpu);
+        cudaFree(off_policy_runner_gpu_struct);
+        cudaFree(batch_gpu_struct);
+    }
+};
 
+
+TEST_F(LAYER_IN_C_RL_CUDA, GATHER_BATCH) {
+
+    auto rng_cpu = lic::random::default_engine(DEVICE_CPU::SPEC::RANDOM());
+    auto rng_gpu = lic::random::default_engine(DEVICE_GPU::SPEC::RANDOM());
     for(DEVICE_CPU::index_t rb_i = 0; rb_i < OFF_POLICY_RUNNER_SPEC::N_ENVIRONMENTS; rb_i++) {
         lic::copy(device_cpu, device_gpu, off_policy_runner_cpu_2.replay_buffers[rb_i], off_policy_runner_gpu_cpu.replay_buffers[rb_i]);
         auto abs_diff = lic::abs_diff(device_cpu, off_policy_runner_cpu.replay_buffers[rb_i], off_policy_runner_cpu_2.replay_buffers[rb_i]);
@@ -86,7 +106,6 @@ TEST(LAYER_IN_C_RL_CUDA, GATHER_BATCH) {
 
     lic::gather_batch<DEVICE_CPU, OFF_POLICY_RUNNER_SPEC, BATCH_SPEC, decltype(rng_cpu), true>(device_cpu, off_policy_runner_cpu, batch_cpu, rng_cpu);
     lic::gather_batch<typename DEVICE_GPU::SPEC, OFF_POLICY_RUNNER_SPEC, BATCH_SPEC, decltype(rng_gpu), true>(device_gpu, off_policy_runner_gpu_struct, batch_gpu_struct, rng_gpu);
-    cudaDeviceSynchronize();
 
     auto batch_observation_view = lic::view<DEVICE_CPU, typename decltype(off_policy_runner_cpu.replay_buffers[0].observations)::SPEC, BATCH_SIZE, BATCH_TYPE::OBSERVATION_DIM>(device_cpu, off_policy_runner_cpu.replay_buffers[0].observations, 0, 0);
     lic::print(device_cpu, batch_observation_view);
@@ -98,9 +117,4 @@ TEST(LAYER_IN_C_RL_CUDA, GATHER_BATCH) {
 
     auto abs_diff_observations = lic::abs_diff(device_cpu, batch_cpu.observations, batch_cpu_2.observations);
     ASSERT_FLOAT_EQ(abs_diff_observations, 0);
-
-
-    lic::free(device_cpu, off_policy_runner_cpu);
-    lic::free(device_gpu, off_policy_runner_gpu_cpu);
-    cudaFree(off_policy_runner_gpu_struct);
 }
