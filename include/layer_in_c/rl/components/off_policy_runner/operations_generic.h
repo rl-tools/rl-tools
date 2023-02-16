0d4466579f62c2b68a2cf96afb9458a91c5fcc02
Jonas Eschmann <jonas.eschmann@gmail.com>
2023-02-16 09:45:16 -0500

    reshuffling public and private interfaces for off-policy-runner

--- include/layer_in_c/rl/components/off_policy_runner/operations_generic.h
+++ include/layer_in_c/rl/components/off_policy_runner/operations_generic.h
@@ -6,6 +6,102 @@
 
 #include <layer_in_c/rl/components/replay_buffer/operations_generic.h>
 
+namespace layer_in_c::rl::components::off_policy_runner{
+    template<typename DEVICE, typename SPEC, typename RNG>
+    void prologue(DEVICE& device, rl::components::OffPolicyRunner<SPEC> &runner, RNG &rng, typename DEVICE::index_t env_i) {
+        using T = typename SPEC::T;
+        using TI = typename SPEC::TI;
+        // if the episode is done (step limit activated for STEP_LIMIT > 0) or if the step is the first step for this runner, reset the environment
+        using RUNNER = rl::components::OffPolicyRunner<SPEC>;
+        using ENVIRONMENT = typename SPEC::ENVIRONMENT;
+        auto& env = runner.envs[env_i];
+        auto& state = get(runner.states, 0, env_i);
+        if (get(runner.truncated, 0, env_i)){
+            sample_initial_state(device, env, state, rng);
+            set(runner.episode_step, 0, env_i, 0);
+            set(runner.episode_return, 0, env_i, 0);
+        }
+        auto observation = view<DEVICE, typename decltype(runner.buffers.observations)::SPEC, 1, ENVIRONMENT::OBSERVATION_DIM>(device, runner.buffers.observations, env_i, 0);
+        observe(device, env, state, observation);
+    }
+    template<typename DEVICE, typename SPEC, typename POLICY>
+    void interlude(DEVICE& device, rl::components::OffPolicyRunner<SPEC> &runner, POLICY &policy, typename POLICY::template Buffers<SPEC::N_ENVIRONMENTS>& policy_eval_buffers) {
+        evaluate(device, policy, runner.buffers.observations, runner.buffers.actions, policy_eval_buffers);
+    }
+
+    template<typename DEVICE, typename SPEC, typename RNG>
+    void epilogue(DEVICE& device, rl::components::OffPolicyRunner<SPEC>* runner, RNG &rng, typename DEVICE::index_t env_i) {
+        using T = typename SPEC::T;
+        using TI = typename SPEC::TI;
+        using ENVIRONMENT = typename SPEC::ENVIRONMENT;
+        using PARAMETERS = typename SPEC::PARAMETERS;
+//        for(typename DEVICE::index_t i = 0; i < ENVIRONMENT::ACTION_DIM; i++) {
+//            auto name = "action/" + std::to_string(i);
+//            add_scalar(device.logger, name.c_str(), action[i]);
+//        }
+        auto observation = view<DEVICE, typename decltype(runner->buffers.observations)::SPEC, 1, ENVIRONMENT::OBSERVATION_DIM>(device, runner->buffers.observations, env_i, 0);
+        auto next_observation = view<DEVICE, typename decltype(runner->buffers.observations)::SPEC, 1, ENVIRONMENT::OBSERVATION_DIM>(device, runner->buffers.next_observations, env_i, 0);
+        auto action = view<DEVICE, typename decltype(runner->buffers.actions)::SPEC, 1, ENVIRONMENT::ACTION_DIM>(device, runner->buffers.actions, env_i, 0);
+        auto& env = runner->envs[env_i];
+        auto& state = get(runner->states, 0, env_i);
+        typename ENVIRONMENT::State next_state;
+
+        for (typename DEVICE::index_t i = 0; i < ENVIRONMENT::ACTION_DIM; i++) {
+            T action_noisy = get(action, 0, i) + random::normal_distribution(typename DEVICE::SPEC::RANDOM(), (T) 0, PARAMETERS::EXPLORATION_NOISE, rng);
+            set(action, 0, i, math::clamp<T>(action_noisy, -1, 1));
+        }
+//        for(typename DEVICE::index_t i = 0; i < ENVIRONMENT::ACTION_DIM; i++) {
+//            auto name = "action_exploration/" + std::to_string(i);
+//            add_scalar(device.logger, name.c_str(), action[i]);
+//        }
+        step(device, env, state, action, next_state);
+        T reward_value = reward(device, env, state, action, next_state);
+//        if constexpr(DEVICE::DEBUG::PRINT_REWARD){
+//            std::cout << "reward: " << reward_value << std::endl;
+//        }
+
+        observe(device, env, next_state, next_observation);
+
+        bool terminated_flag = terminated(device, env, next_state);
+        increment(runner->episode_step, 0, env_i, 1);
+        increment(runner->episode_return, 0, env_i, reward_value);
+        bool truncated = terminated_flag || get(runner->episode_step, 0, env_i) == SPEC::STEP_LIMIT;
+        set(runner->truncated, 0, env_i, truncated);
+        if(truncated){
+            add_scalar(device, device.logger, "episode_return", get(runner->episode_return, 0, env_i));
+            add_scalar(device, device.logger, "episode_steps", (T)get(runner->episode_step, 0, env_i));
+        }
+        // todo: add truncation / termination handling (stemming from the environment)
+        add(device, runner->replay_buffers[env_i], observation, action, reward_value, next_observation, terminated_flag, truncated);
+
+        // state progression needs to come after the addition to the replay buffer because "observation" can point to the memory of runner_state.state (in the case of REQUIRES_OBSERVATION=false)
+        state = next_state;
+    }
+
+
+    template <typename DEVICE, typename SPEC, typename BATCH_SPEC, typename RNG, bool DETERMINISTIC = false>
+    void gather_batch(DEVICE& device, const rl::components::ReplayBuffer<SPEC>& replay_buffer, rl::components::off_policy_runner::Batch<BATCH_SPEC>& batch, typename DEVICE::index_t batch_step_i, RNG& rng) {
+        typename DEVICE::index_t sample_index_max = (replay_buffer.full ? SPEC::CAPACITY : replay_buffer.position) - 1;
+        typename DEVICE::index_t sample_index = DETERMINISTIC ? batch_step_i : random::uniform_int_distribution( typename DEVICE::SPEC::RANDOM(), (typename DEVICE::index_t) 0, sample_index_max, rng);
+
+        auto observation_target = view<DEVICE, typename decltype(batch.observations)::SPEC, 1, SPEC::OBSERVATION_DIM>(device, batch.observations, batch_step_i, 0);
+        auto observation_source = view<DEVICE, typename decltype(replay_buffer.observations)::SPEC, 1, SPEC::OBSERVATION_DIM>(device, replay_buffer.observations, sample_index, 0);
+        copy(device, device, observation_target, observation_source);
+
+        auto action_target = view<DEVICE, typename decltype(batch.actions)::SPEC, 1, SPEC::ACTION_DIM>(device, batch.actions, batch_step_i, 0);
+        auto action_source = view<DEVICE, typename decltype(replay_buffer.actions)::SPEC, 1, SPEC::ACTION_DIM>(device, replay_buffer.actions, sample_index, 0);
+        copy(device, device, action_target, action_source);
+
+        auto next_observation_target = view<DEVICE, typename decltype(batch.next_observations)::SPEC, 1, SPEC::OBSERVATION_DIM>(device, batch.next_observations, batch_step_i, 0);
+        auto next_observation_source = view<DEVICE, typename decltype(replay_buffer.next_observations)::SPEC, 1, SPEC::OBSERVATION_DIM>(device, replay_buffer.next_observations, sample_index, 0);
+        copy(device, device, next_observation_target, next_observation_source);
+
+        set(batch.rewards, 0, batch_step_i, get(replay_buffer.rewards, sample_index, 0));
+        set(batch.terminated, 0, batch_step_i, get(replay_buffer.terminated, sample_index, 0));
+        set(batch.truncated, 0, batch_step_i, get(replay_buffer.truncated,  sample_index, 0));
+    }
+}
+
 namespace layer_in_c{
     template <typename DEVICE, typename SPEC>
     void malloc(DEVICE& device, rl::components::off_policy_runner::Buffers<SPEC>& buffers) {
@@ -78,77 +174,6 @@ namespace layer_in_c{
         runner.initialized = true;
 #endif
     }
-    template<typename DEVICE, typename SPEC, typename RNG>
-    void prologue(DEVICE& device, rl::components::OffPolicyRunner<SPEC> &runner, RNG &rng, typename DEVICE::index_t env_i) {
-        using T = typename SPEC::T;
-        using TI = typename SPEC::TI;
-        // if the episode is done (step limit activated for STEP_LIMIT > 0) or if the step is the first step for this runner, reset the environment
-        using RUNNER = rl::components::OffPolicyRunner<SPEC>;
-        using ENVIRONMENT = typename SPEC::ENVIRONMENT;
-        auto& env = runner.envs[env_i];
-        auto& state = get(runner.states, 0, env_i);
-        if (get(runner.truncated, 0, env_i)){
-            sample_initial_state(device, env, state, rng);
-            set(runner.episode_step, 0, env_i, 0);
-            set(runner.episode_return, 0, env_i, 0);
-        }
-        auto observation = view<DEVICE, typename decltype(runner.buffers.observations)::SPEC, 1, ENVIRONMENT::OBSERVATION_DIM>(device, runner.buffers.observations, env_i, 0);
-        observe(device, env, state, observation);
-    }
-    template<typename DEVICE, typename SPEC, typename POLICY>
-    void interlude(DEVICE& device, rl::components::OffPolicyRunner<SPEC> &runner, POLICY &policy, typename POLICY::template Buffers<SPEC::N_ENVIRONMENTS>& policy_eval_buffers) {
-        evaluate(device, policy, runner.buffers.observations, runner.buffers.actions, policy_eval_buffers);
-    }
-
-    template<typename DEVICE, typename SPEC, typename RNG>
-    void epilogue(DEVICE& device, rl::components::OffPolicyRunner<SPEC>* runner, RNG &rng, typename DEVICE::index_t env_i) {
-        using T = typename SPEC::T;
-        using TI = typename SPEC::TI;
-        using ENVIRONMENT = typename SPEC::ENVIRONMENT;
-        using PARAMETERS = typename SPEC::PARAMETERS;
-//        for(typename DEVICE::index_t i = 0; i < ENVIRONMENT::ACTION_DIM; i++) {
-//            auto name = "action/" + std::to_string(i);
-//            add_scalar(device.logger, name.c_str(), action[i]);
-//        }
-        auto observation = view<DEVICE, typename decltype(runner->buffers.observations)::SPEC, 1, ENVIRONMENT::OBSERVATION_DIM>(device, runner->buffers.observations, env_i, 0);
-        auto next_observation = view<DEVICE, typename decltype(runner->buffers.observations)::SPEC, 1, ENVIRONMENT::OBSERVATION_DIM>(device, runner->buffers.next_observations, env_i, 0);
-        auto action = view<DEVICE, typename decltype(runner->buffers.actions)::SPEC, 1, ENVIRONMENT::ACTION_DIM>(device, runner->buffers.actions, env_i, 0);
-        auto& env = runner->envs[env_i];
-        auto& state = get(runner->states, 0, env_i);
-        typename ENVIRONMENT::State next_state;
-
-        for (typename DEVICE::index_t i = 0; i < ENVIRONMENT::ACTION_DIM; i++) {
-            T action_noisy = get(action, 0, i) + random::normal_distribution(typename DEVICE::SPEC::RANDOM(), (T) 0, PARAMETERS::EXPLORATION_NOISE, rng);
-            set(action, 0, i, math::clamp<T>(action_noisy, -1, 1));
-        }
-//        for(typename DEVICE::index_t i = 0; i < ENVIRONMENT::ACTION_DIM; i++) {
-//            auto name = "action_exploration/" + std::to_string(i);
-//            add_scalar(device.logger, name.c_str(), action[i]);
-//        }
-        step(device, env, state, action, next_state);
-        T reward_value = reward(device, env, state, action, next_state);
-//        if constexpr(DEVICE::DEBUG::PRINT_REWARD){
-//            std::cout << "reward: " << reward_value << std::endl;
-//        }
-
-        observe(device, env, next_state, next_observation);
-
-        bool terminated_flag = terminated(device, env, next_state);
-        increment(runner->episode_step, 0, env_i, 1);
-        increment(runner->episode_return, 0, env_i, reward_value);
-        bool truncated = terminated_flag || get(runner->episode_step, 0, env_i) == SPEC::STEP_LIMIT;
-        set(runner->truncated, 0, env_i, truncated);
-        if(truncated){
-            add_scalar(device, device.logger, "episode_return", get(runner->episode_return, 0, env_i));
-            add_scalar(device, device.logger, "episode_steps", (T)get(runner->episode_step, 0, env_i));
-        }
-        // todo: add truncation / termination handling (stemming from the environment)
-        add(device, runner->replay_buffers[env_i], observation, action, reward_value, next_observation, terminated_flag, truncated);
-
-        // state progression needs to come after the addition to the replay buffer because "observation" can point to the memory of runner_state.state (in the case of REQUIRES_OBSERVATION=false)
-        state = next_state;
-    }
-
     template<typename DEVICE, typename SPEC, typename POLICY, typename RNG>
     void step(DEVICE& device, rl::components::OffPolicyRunner<SPEC> &runner, POLICY &policy, typename POLICY::template Buffers<SPEC::N_ENVIRONMENTS>& policy_eval_buffers, RNG &rng) {
 #ifdef LAYER_IN_C_DEBUG_RL_COMPONENTS_OFF_POLICY_RUNNER_CHECK_INIT
@@ -171,28 +196,6 @@ namespace layer_in_c{
             epilogue(device, &runner, rng, env_i);
         }
         runner.step += SPEC::N_ENVIRONMENTS;
-    }
-
-    template <typename DEVICE, typename SPEC, typename BATCH_SPEC, typename RNG, bool DETERMINISTIC = false>
-    void gather_batch(DEVICE& device, const rl::components::ReplayBuffer<SPEC>& replay_buffer, rl::components::off_policy_runner::Batch<BATCH_SPEC>& batch, typename DEVICE::index_t batch_step_i, RNG& rng) {
-        typename DEVICE::index_t sample_index_max = (replay_buffer.full ? SPEC::CAPACITY : replay_buffer.position) - 1;
-        typename DEVICE::index_t sample_index = DETERMINISTIC ? batch_step_i : random::uniform_int_distribution( typename DEVICE::SPEC::RANDOM(), (typename DEVICE::index_t) 0, sample_index_max, rng);
-
-        auto observation_target = view<DEVICE, typename decltype(batch.observations)::SPEC, 1, SPEC::OBSERVATION_DIM>(device, batch.observations, batch_step_i, 0);
-        auto observation_source = view<DEVICE, typename decltype(replay_buffer.observations)::SPEC, 1, SPEC::OBSERVATION_DIM>(device, replay_buffer.observations, sample_index, 0);
-        copy(device, device, observation_target, observation_source);
-
-        auto action_target = view<DEVICE, typename decltype(batch.actions)::SPEC, 1, SPEC::ACTION_DIM>(device, batch.actions, batch_step_i, 0);
-        auto action_source = view<DEVICE, typename decltype(replay_buffer.actions)::SPEC, 1, SPEC::ACTION_DIM>(device, replay_buffer.actions, sample_index, 0);
-        copy(device, device, action_target, action_source);
-
-        auto next_observation_target = view<DEVICE, typename decltype(batch.next_observations)::SPEC, 1, SPEC::OBSERVATION_DIM>(device, batch.next_observations, batch_step_i, 0);
-        auto next_observation_source = view<DEVICE, typename decltype(replay_buffer.next_observations)::SPEC, 1, SPEC::OBSERVATION_DIM>(device, replay_buffer.next_observations, sample_index, 0);
-        copy(device, device, next_observation_target, next_observation_source);
-
-        set(batch.rewards, 0, batch_step_i, get(replay_buffer.rewards, sample_index, 0));
-        set(batch.terminated, 0, batch_step_i, get(replay_buffer.terminated, sample_index, 0));
-        set(batch.truncated, 0, batch_step_i, get(replay_buffer.truncated,  sample_index, 0));
     }
     template <typename DEVICE, typename SPEC, typename BATCH_SPEC, typename RNG, bool DETERMINISTIC=false>
     void gather_batch(DEVICE& device, const rl::components::OffPolicyRunner<SPEC>& runner, rl::components::off_policy_runner::Batch<BATCH_SPEC>& batch, RNG& rng) {
