a47391f7ac55dd2d36ac494120dd036e9bad2888
Jonas Eschmann <jonas.eschmann@gmail.com>
2023-02-28 18:13:45 -0500

    addendum #2

--- tests/src/container/persist_code_store.cpp
+++ tests/src/container/persist_code_store.cpp
@@ -58,7 +58,7 @@ TEST(LAYER_IN_C_CONTAINER_PERSIST_CODE_STORE, TEST_MLP){
     using DTYPE = float;
     DEVICE device;
     auto rng = lic::random::default_engine(DEVICE::SPEC::RANDOM());
-    using SPEC = lic::nn_models::mlp::InferenceSpecification<lic::nn_models::mlp::StructureSpecification<DTYPE, typename DEVICE::index_t, 3, 3, 3, 3, lic::nn::activation_functions::ActivationFunction::RELU, lic::nn::activation_functions::ActivationFunction::IDENTITY, 1, true, lic::matrix::layouts::RowMajorAlignment<typename DEVICE::index_t, 1>>>;
+    using SPEC = lic::nn_models::mlp::InferenceSpecification<lic::nn_models::mlp::StructureSpecification<DTYPE, typename DEVICE::index_t, 13, 4, 3, 64, lic::nn::activation_functions::ActivationFunction::RELU, lic::nn::activation_functions::ActivationFunction::IDENTITY, 1, true, lic::matrix::layouts::RowMajorAlignment<typename DEVICE::index_t, 1>>>;
     lic::nn_models::mlp::NeuralNetwork<SPEC> mlp;
     lic::malloc(device, mlp);
     lic::init_weights(device, mlp, rng);
