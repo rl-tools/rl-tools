be2ead211ab1e6a798254d8d61634998ead59c12
Jonas Eschmann <jonas.eschmann@gmail.com>
2023-05-04 10:26:08 -0400

    more generic conan setup

--- include/backprop_tools/rl/environments/mujoco/ant/ant.h
+++ include/backprop_tools/rl/environments/mujoco/ant/ant.h
@@ -44,7 +44,7 @@ namespace backprop_tools::rl::environments::mujoco{
     struct Ant{
         using SPEC = T_SPEC;
         using T = typename SPEC::T;
-        static_assert(utils::typing::is_same_v<T, mjtNum>);
+        static_assert(backprop_tools::utils::typing::is_same_v<T, mjtNum>);
         using TI = typename SPEC::TI;
         using State = ant::State<SPEC>;
         mjModel* model;
