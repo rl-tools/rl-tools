abf80d8fb530487295352ed34716706a2f17341d
Jonas Eschmann <jonas.eschmann@gmail.com>
2023-05-28 17:10:57 -0400

    longest flight

--- include/backprop_tools/rl/environments/multirotor/parameters/reward_functions/default.h
+++ include/backprop_tools/rl/environments/multirotor/parameters/reward_functions/default.h
@@ -70,7 +70,7 @@ namespace backprop_tools::rl::environments::multirotor::parameters::reward_funct
             0, // linear acceleration
             0, // angular acceleration
             BACKPROP_TOOLS_RL_ENVIRONMENTS_MULTIROTOR_PARAMETERS_REWARD_FUNCTIONS_DEFAULT_ACTION_BASELINE, // action baseline
-            5 // divide by to because actions are transformed from -1 -> 1 to 0 to 2 by the baseline => norm will be 2x
+            1 // divide by to because actions are transformed from -1 -> 1 to 0 to 2 by the baseline => norm will be 2x
         }
     };
 
