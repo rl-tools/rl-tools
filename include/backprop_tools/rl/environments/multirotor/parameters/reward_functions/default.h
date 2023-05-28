77745bfaa7a76c88a9c04cc595ca8a99fcb4b1d7
Jonas Eschmann <jonas.eschmann@gmail.com>
2023-05-28 13:46:28 -0400

    best real run yet

--- include/backprop_tools/rl/environments/multirotor/parameters/reward_functions/default.h
+++ include/backprop_tools/rl/environments/multirotor/parameters/reward_functions/default.h
@@ -42,7 +42,7 @@ namespace backprop_tools::rl::environments::multirotor::parameters::reward_funct
             0, // linear acceleration
             0, // angular acceleration
             0.33, // action baseline
-            10 // divide by to because actions are transformed from -1 -> 1 to 0 to 2 by the baseline => norm will be 2x
+            1 // divide by to because actions are transformed from -1 -> 1 to 0 to 2 by the baseline => norm will be 2x
     };
 
     template<typename T, typename TI>
--- src/rl/environments/multirotor/parameters.h
+++ src/rl/environments/multirotor/parameters.h
@@ -9,7 +9,7 @@ namespace parameters{
     namespace bpt = backprop_tools;
     template<typename T, typename TI>
     struct environment{
-        static constexpr auto reward_function = backprop_tools::rl::environments::multirotor::parameters::reward_functions::reward_old_but_gold<T>;
+        static constexpr auto reward_function = backprop_tools::rl::environments::multirotor::parameters::reward_functions::reward_old_but_gold_1<T>;
 //        static constexpr auto reward_function = backprop_tools::rl::environments::multirotor::parameters::reward_functions::reward_mm<T, TI>;
 //        static constexpr auto reward_function = backprop_tools::rl::environments::multirotor::parameters::reward_functions::reward_squraed_3<T>;
         using REWARD_FUNCTION_CONST = typename backprop_tools::utils::typing::remove_cv_t<decltype(reward_function)>;
