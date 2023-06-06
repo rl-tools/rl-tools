900ae61ef3921290d8f4ba007d413ec527d27fc2
Jonas Eschmann <jonas.eschmann@gmail.com>
2023-06-06 20:32:10 +0400

    testing increased disturbance; very good results but bad torque rejection (yaw)

--- src/rl/environments/multirotor/parameters.h
+++ src/rl/environments/multirotor/parameters.h
@@ -39,7 +39,7 @@ namespace parameters_sim2real{
                         backprop_tools::rl::environments::multirotor::parameters::termination::fast_learning<T, TI, 4, REWARD_FUNCTION>
                 },
                 typename PARAMETERS_TYPE::Disturbances{
-                        typename PARAMETERS_TYPE::Disturbances::UnivariateGaussian{0, 0.027 * 9.81 / 100} // random_force;
+                        typename PARAMETERS_TYPE::Disturbances::UnivariateGaussian{0, 0.027 * 9.81 / 20} // random_force;
 //                        typename PARAMETERS_TYPE::Disturbances::UnivariateGaussian{0, 0} // random_force;
                 }
 
