953c65e3b476f91eb99ce4722b2b6918a10f0035
Jonas Eschmann <jonas.eschmann@gmail.com>
2023-08-08 12:14:56 +0400

    back to old init

--- src/rl/environments/multirotor/parameters.h
+++ src/rl/environments/multirotor/parameters.h
@@ -37,7 +37,7 @@ namespace parameters{
             using PARAMETERS_TYPE = backprop_tools::rl::environments::multirotor::ParametersDisturbances<T, TI, 4, REWARD_FUNCTION>;
 
             static constexpr auto init_params = ABLATION_SPEC::INIT_NORMAL ?
-                backprop_tools::rl::environments::multirotor::parameters::init::orientation_bigger_angle<T, TI, 4, REWARD_FUNCTION>:
+                backprop_tools::rl::environments::multirotor::parameters::init::all_around_2<T, TI, 4, REWARD_FUNCTION>:
                 backprop_tools::rl::environments::multirotor::parameters::init::orientation_small_angle<T, TI, 4, REWARD_FUNCTION>;
 
             static constexpr PARAMETERS_TYPE parameters = {
