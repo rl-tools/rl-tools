4ef3b6454530f07c22641772ea5953f8eb2a6288
Jonas Eschmann <jonas.eschmann@gmail.com>
2023-10-24 15:34:39 -0400

    unbiased seed

--- src/rl/environments/multirotor/ui/training.h
+++ src/rl/environments/multirotor/ui/training.h
@@ -171,7 +171,7 @@ namespace multirotor_training{
 //            static constexpr TI REPLAY_BUFFER_LIMIT = 3000000;
             static constexpr TI REPLAY_BUFFER_CAP = STEP_LIMIT;
             static constexpr TI ENVIRONMENT_STEP_LIMIT = 500;
-            static constexpr TI BASE_SEED = 30;
+            static constexpr TI BASE_SEED = 0;
             static constexpr bool CONSTRUCT_LOGGER = false;
             using OFF_POLICY_RUNNER_SPEC = bpt::rl::components::off_policy_runner::Specification<T, TI, ENVIRONMENT, N_ENVIRONMENTS, ASYMMETRIC_OBSERVATIONS, REPLAY_BUFFER_CAP, ENVIRONMENT_STEP_LIMIT, bpt::rl::components::off_policy_runner::DefaultParameters<T>, false, true, 1000>;
             using OFF_POLICY_RUNNER_TYPE = bpt::rl::components::OffPolicyRunner<OFF_POLICY_RUNNER_SPEC>;
