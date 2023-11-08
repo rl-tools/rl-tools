ff41c57bf69f14fa3f391e2f8433fa8c73bb8ea0
Jonas Eschmann <jonas.eschmann@gmail.com>
2023-11-08 10:20:15 -0500

    removing linux only stuff; ablation identical

--- src/rl/environments/multirotor/ui/training.cpp
+++ src/rl/environments/multirotor/ui/training.cpp
@@ -1,6 +1,5 @@
 
 #include "training.h"
-#include <malloc.h>
 
 
 template <typename T_ABLATION_SPEC>
@@ -12,24 +11,14 @@ void run(){
 
     for (TI run_i = 0; run_i < 10; run_i++){
         std::cout << "Run " << run_i << "\n";
-        struct mallinfo mi = mallinfo();
-        std::cout << "Total allocated space before declaration: " << mi.uordblks << " bytes\n";
         multirotor_training::operations::TrainingState<CONFIG> ts;
-        mi = mallinfo();
-        std::cout << "Total allocated space after declaration: " << mi.uordblks << " bytes\n";
         multirotor_training::operations::init(ts, run_i);
-        mi = mallinfo();
-        std::cout << "Total allocated space after init: " << mi.uordblks << " bytes\n";
 
         for(TI step_i=0; step_i < CONFIG::STEP_LIMIT; step_i++){
             multirotor_training::operations::step(ts);
         }
 
-        mi = mallinfo();
-        std::cout << "Total allocated space after training: " << mi.uordblks << " bytes\n";
         multirotor_training::operations::destroy(ts);
-        mi = mallinfo();
-        std::cout << "Total allocated space after destruction: " << mi.uordblks << " bytes\n";
     }
 }
 
