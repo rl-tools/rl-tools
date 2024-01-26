ea9b52af37705c0d7d0a6b6fea9e670f429ac911
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-01-25 19:17:07 -0500

    copying loop from sac to ppo

---
+++ include/rl_tools/rl/algorithms/ppo/loop/core/config.h
@@ -0,0 +1,143 @@
+#include "../../../../../version.h"
+#if (defined(RL_TOOLS_DISABLE_INCLUDE_GUARDS) || !defined(RL_TOOLS_RL_ALGORITHMS_SAC_LOOP_CORE_CONFIG_H)) && (RL_TOOLS_USE_THIS_VERSION == 1)
+#pragma once
+#define RL_TOOLS_RL_ALGORITHMS_SAC_LOOP_CORE_CONFIG_H
+
+#include "../../../../../nn_models/sequential/model.h"
+#include "../../../../../nn_models/mlp/network.h"
+#include "../../../../../rl/algorithms/sac/sac.h"
+#include "../../../../../nn/optimizers/adam/adam.h"
+#include "state.h"
+
+RL_TOOLS_NAMESPACE_WRAPPER_START
+namespace rl_tools::rl::algorithms::sac::loop::core{
+    // Config State (Init/Step)
+    using namespace nn_models::sequential::interface;
+
+    template<typename T, typename TI, typename ENVIRONMENT>
+    struct DefaultParameters{
+        using SAC_PARAMETERS = rl::algorithms::sac::DefaultParameters<T, TI, ENVIRONMENT::ACTION_DIM>;
+        static constexpr int N_WARMUP_STEPS = SAC_PARAMETERS::ACTOR_BATCH_SIZE;
+        static constexpr TI STEP_LIMIT = 10000;
+        static constexpr TI REPLAY_BUFFER_CAP = STEP_LIMIT;
+        static constexpr TI ENVIRONMENT_STEP_LIMIT = 200;
+
+        static constexpr TI ACTOR_HIDDEN_DIM = 64;
+        static constexpr TI ACTOR_NUM_LAYERS = 3;
+        static constexpr auto ACTOR_ACTIVATION_FUNCTION = nn::activation_functions::ActivationFunction::RELU;
+        static constexpr TI CRITIC_HIDDEN_DIM = 64;
+        static constexpr TI CRITIC_NUM_LAYERS = 3;
+        static constexpr auto CRITIC_ACTIVATION_FUNCTION = nn::activation_functions::ActivationFunction::RELU;
+
+        static constexpr bool COLLECT_EPISODE_STATS = true;
+        static constexpr TI EPISODE_STATS_BUFFER_SIZE = 1000;
+    };
+
+    // The approximator config sets up any types that support the usual rl_tools::forward and rl_tools::backward operations (can be custom as well)
+    // We provide approximators based on the sequential and mlp models. The latter (mlp) allows for a variable number of layers, but is restricted to a uniform hidden layer size while the former allows for arbitrary layers to be combined in a sequential manner. Both support compile-time autodiff
+    template<typename T, typename TI, typename ENVIRONMENT, typename PARAMETERS>
+    struct DefaultConfigApproximatorsSequential{
+        template <typename PARAMETER_TYPE, template<typename> class LAYER_TYPE = nn::layers::dense::LayerBackwardGradient>
+        struct ACTOR{
+            static constexpr TI HIDDEN_DIM = PARAMETERS::ACTOR_HIDDEN_DIM;
+            static constexpr TI BATCH_SIZE = PARAMETERS::SAC_PARAMETERS::ACTOR_BATCH_SIZE;
+            static constexpr auto ACTIVATION_FUNCTION = PARAMETERS::ACTOR_ACTIVATION_FUNCTION;
+            using LAYER_1_SPEC = nn::layers::dense::Specification<T, TI, ENVIRONMENT::OBSERVATION_DIM, HIDDEN_DIM, ACTIVATION_FUNCTION, PARAMETER_TYPE, BATCH_SIZE>;
+            using LAYER_1 = LAYER_TYPE<LAYER_1_SPEC>;
+            using LAYER_2_SPEC = nn::layers::dense::Specification<T, TI, HIDDEN_DIM, HIDDEN_DIM, ACTIVATION_FUNCTION, PARAMETER_TYPE, BATCH_SIZE>;
+            using LAYER_2 = LAYER_TYPE<LAYER_2_SPEC>;
+            static constexpr TI ACTOR_OUTPUT_DIM = ENVIRONMENT::ACTION_DIM * 2; // to express mean and log_std for each action
+            using LAYER_3_SPEC = nn::layers::dense::Specification<T, TI, HIDDEN_DIM, ACTOR_OUTPUT_DIM, nn::activation_functions::ActivationFunction::IDENTITY, PARAMETER_TYPE, BATCH_SIZE>; // note the output activation should be identity because we want to sample from a gaussian and then squash afterwards (taking into account the squashing in the distribution)
+            using LAYER_3 = LAYER_TYPE<LAYER_3_SPEC>;
+
+            using MODEL = Module<LAYER_1, Module<LAYER_2, Module<LAYER_3>>>;
+        };
+
+        template <typename PARAMETER_TYPE, template<typename> class LAYER_TYPE = nn::layers::dense::LayerBackwardGradient>
+        struct CRITIC{
+            static constexpr TI HIDDEN_DIM = PARAMETERS::CRITIC_HIDDEN_DIM;
+            static constexpr TI BATCH_SIZE = PARAMETERS::SAC_PARAMETERS::CRITIC_BATCH_SIZE;
+            static constexpr auto ACTIVATION_FUNCTION = PARAMETERS::CRITIC_ACTIVATION_FUNCTION;
+
+            using LAYER_1_SPEC = nn::layers::dense::Specification<T, TI, ENVIRONMENT::OBSERVATION_DIM + ENVIRONMENT::ACTION_DIM, HIDDEN_DIM, ACTIVATION_FUNCTION, PARAMETER_TYPE, BATCH_SIZE>;
+            using LAYER_1 = LAYER_TYPE<LAYER_1_SPEC>;
+            using LAYER_2_SPEC = nn::layers::dense::Specification<T, TI, HIDDEN_DIM, HIDDEN_DIM, ACTIVATION_FUNCTION, PARAMETER_TYPE, BATCH_SIZE>;
+            using LAYER_2 = LAYER_TYPE<LAYER_2_SPEC>;
+            using LAYER_3_SPEC = nn::layers::dense::Specification<T, TI, HIDDEN_DIM, 1, nn::activation_functions::ActivationFunction::IDENTITY, PARAMETER_TYPE, BATCH_SIZE>;
+            using LAYER_3 = LAYER_TYPE<LAYER_3_SPEC>;
+
+            using MODEL = Module<LAYER_1, Module<LAYER_2, Module<LAYER_3>>>;
+        };
+
+        using OPTIMIZER_SPEC = nn::optimizers::adam::Specification<T, TI>;
+
+        using OPTIMIZER = nn::optimizers::Adam<OPTIMIZER_SPEC>;
+
+        using ACTOR_TYPE = typename ACTOR<nn::parameters::Adam>::MODEL;
+        using ACTOR_TARGET_TYPE = typename ACTOR<nn::parameters::Adam, nn::layers::dense::Layer>::MODEL;
+        using CRITIC_TYPE = typename CRITIC<nn::parameters::Adam>::MODEL;
+        using CRITIC_TARGET_TYPE = typename CRITIC<nn::parameters::Adam, nn::layers::dense::Layer>::MODEL;
+    };
+
+    template<typename T, typename TI, typename ENVIRONMENT, typename PARAMETERS>
+    struct DefaultConfigApproximatorsMLP{
+        using ACTOR_STRUCTURE_SPEC = nn_models::mlp::StructureSpecification<T, TI, ENVIRONMENT::OBSERVATION_DIM, 2*ENVIRONMENT::ACTION_DIM, PARAMETERS::ACTOR_NUM_LAYERS, PARAMETERS::ACTOR_HIDDEN_DIM, PARAMETERS::ACTOR_ACTIVATION_FUNCTION, nn::activation_functions::TANH, PARAMETERS::SAC_PARAMETERS::ACTOR_BATCH_SIZE>;
+        using CRITIC_STRUCTURE_SPEC = nn_models::mlp::StructureSpecification<T, TI, ENVIRONMENT::OBSERVATION_DIM + ENVIRONMENT::ACTION_DIM, 1, PARAMETERS::CRITIC_NUM_LAYERS, PARAMETERS::CRITIC_HIDDEN_DIM, PARAMETERS::CRITIC_ACTIVATION_FUNCTION, nn::activation_functions::IDENTITY, PARAMETERS::SAC_PARAMETERS::CRITIC_BATCH_SIZE>;
+        using OPTIMIZER_SPEC = typename nn::optimizers::adam::Specification<T, TI>;
+        using OPTIMIZER = nn::optimizers::Adam<OPTIMIZER_SPEC>;
+        using ACTOR_SPEC = nn_models::mlp::AdamSpecification<ACTOR_STRUCTURE_SPEC >;
+        using ACTOR_TYPE = nn_models::mlp::NeuralNetworkAdam<ACTOR_SPEC>;
+
+        using ACTOR_TARGET_SPEC = nn_models::mlp::InferenceSpecification<ACTOR_STRUCTURE_SPEC>;
+        using ACTOR_TARGET_TYPE = nn_models::mlp::NeuralNetwork<ACTOR_TARGET_SPEC>;
+
+        using CRITIC_SPEC = nn_models::mlp::AdamSpecification<CRITIC_STRUCTURE_SPEC>;
+        using CRITIC_TYPE = nn_models::mlp::NeuralNetworkAdam<CRITIC_SPEC>;
+
+        using CRITIC_TARGET_SPEC = nn_models::mlp::InferenceSpecification<CRITIC_STRUCTURE_SPEC >;
+        using CRITIC_TARGET_TYPE = nn_models::mlp::NeuralNetwork<CRITIC_TARGET_SPEC>;
+    };
+
+    template<typename T_DEVICE, typename T_T, typename T_ENVIRONMENT, typename T_PARAMETERS = DefaultParameters<T_T, typename T_DEVICE::index_t, T_ENVIRONMENT>, template<typename, typename, typename, typename> class APPROXIMATOR_CONFIG=DefaultConfigApproximatorsSequential>
+    struct DefaultConfig{
+        using DEVICE = T_DEVICE;
+        using T = T_T;
+        using TI = typename DEVICE::index_t;
+        using ENVIRONMENT = T_ENVIRONMENT;
+        using ENVIRONMENT_EVALUATION = T_ENVIRONMENT;
+        using UI = bool;
+
+        using NN = APPROXIMATOR_CONFIG<T, TI, T_ENVIRONMENT, T_PARAMETERS>;
+//        using NN = DefaultConfigApproximatorsMLP<T, TI, T_ENVIRONMENT, T_PARAMETERS>;
+
+        using PARAMETERS = T_PARAMETERS;
+
+
+        using ALPHA_PARAMETER_TYPE = nn::parameters::Adam;
+        using ALPHA_OPTIMIZER = nn::optimizers::Adam<typename NN::OPTIMIZER_SPEC>;
+
+        using ACTOR_CRITIC_SPEC = rl::algorithms::sac::Specification<T, TI, ENVIRONMENT, typename NN::ACTOR_TYPE, typename NN::ACTOR_TARGET_TYPE, typename NN::CRITIC_TYPE, typename NN::CRITIC_TARGET_TYPE, ALPHA_PARAMETER_TYPE, typename NN::OPTIMIZER, typename NN::OPTIMIZER, ALPHA_OPTIMIZER, typename PARAMETERS::SAC_PARAMETERS>;
+        using ACTOR_CRITIC_TYPE = rl::algorithms::sac::ActorCritic<ACTOR_CRITIC_SPEC>;
+
+        static constexpr bool STOCHASTIC_POLICY = true;
+        using OFF_POLICY_RUNNER_SPEC = rl::components::off_policy_runner::Specification<
+                T,
+                TI,
+                ENVIRONMENT,
+                1,
+                false,
+                PARAMETERS::REPLAY_BUFFER_CAP,
+                PARAMETERS::ENVIRONMENT_STEP_LIMIT,
+                rl::components::off_policy_runner::DefaultParameters<T>,
+                STOCHASTIC_POLICY,
+                PARAMETERS::COLLECT_EPISODE_STATS,
+                PARAMETERS::EPISODE_STATS_BUFFER_SIZE
+        >;
+        static_assert(ACTOR_CRITIC_TYPE::SPEC::PARAMETERS::ACTOR_BATCH_SIZE == ACTOR_CRITIC_TYPE::SPEC::PARAMETERS::CRITIC_BATCH_SIZE);
+        template <typename CONFIG>
+        using State = TrainingState<CONFIG>;
+    };
+}
+
+#endif
+
---
+++ include/rl_tools/rl/algorithms/ppo/loop/core/operations.h
@@ -0,0 +1,97 @@
+#include "../../../../../version.h"
+#if (defined(RL_TOOLS_DISABLE_INCLUDE_GUARDS) || !defined(RL_TOOLS_RL_ALGORITHMS_SAC_LOOP_CORE_OPERATIONS_H)) && (RL_TOOLS_USE_THIS_VERSION == 1)
+#pragma once
+#define RL_TOOLS_RL_ALGORITHMS_SAC_LOOP_CORE_OPERATIONS_H
+
+#include "../../../../../rl/algorithms/sac/operations_generic.h"
+#include "../../../../../rl/components/off_policy_runner/operations_generic.h"
+#include "../../../../../rl/utils/evaluation.h"
+
+#include "config.h"
+
+RL_TOOLS_NAMESPACE_WRAPPER_START
+namespace rl_tools{
+    template <typename T_CONFIG>
+    void init(rl::algorithms::sac::loop::core::TrainingState<T_CONFIG>& ts, typename T_CONFIG::TI seed = 0){
+        using CONFIG = T_CONFIG;
+        using T = typename CONFIG::T;
+
+        ts.rng = random::default_engine(typename CONFIG::DEVICE::SPEC::RANDOM(), seed);
+
+        malloc(ts.device, ts.actor_critic);
+        init(ts.device, ts.actor_critic, ts.rng);
+
+        malloc(ts.device, ts.off_policy_runner);
+        init(ts.device, ts.off_policy_runner, ts.envs);
+        rl_tools::init(ts.device, ts.envs[0], ts.ui);
+
+        malloc(ts.device, ts.critic_batch);
+        malloc(ts.device, ts.critic_training_buffers);
+        malloc(ts.device, ts.critic_buffers[0]);
+        malloc(ts.device, ts.critic_buffers[1]);
+
+        malloc(ts.device, ts.actor_batch);
+        malloc(ts.device, ts.actor_training_buffers);
+        malloc(ts.device, ts.actor_buffers_eval);
+        malloc(ts.device, ts.actor_buffers[0]);
+        malloc(ts.device, ts.actor_buffers[1]);
+
+        malloc(ts.device, ts.observations_mean);
+        malloc(ts.device, ts.observations_std);
+
+        malloc(ts.device, ts.actor_deterministic_evaluation_buffers);
+
+        set_all(ts.device, ts.observations_mean, 0);
+        set_all(ts.device, ts.observations_std, 1);
+
+        ts.off_policy_runner.parameters = rl::components::off_policy_runner::default_parameters<T>;
+
+        construct(ts.device, ts.device.logger);
+
+        ts.step = 0;
+    }
+
+    template <typename T_CONFIG>
+    void destroy(rl::algorithms::sac::loop::core::TrainingState<T_CONFIG>& ts){
+        free(ts.device, ts.critic_batch);
+        free(ts.device, ts.critic_training_buffers);
+        free(ts.device, ts.actor_batch);
+        free(ts.device, ts.actor_training_buffers);
+        free(ts.device, ts.off_policy_runner);
+        free(ts.device, ts.actor_critic);
+        free(ts.device, ts.observations_mean);
+        free(ts.device, ts.observations_std);
+    }
+
+
+    template <typename T_CONFIG>
+    bool step(rl::algorithms::sac::loop::core::TrainingState<T_CONFIG>& ts){
+        bool finished = false;
+        using CONFIG = T_CONFIG;
+        step(ts.device, ts.off_policy_runner, ts.actor_critic.actor, ts.actor_buffers_eval, ts.rng);
+        if(ts.step > CONFIG::PARAMETERS::N_WARMUP_STEPS){
+            for(int critic_i = 0; critic_i < 2; critic_i++){
+                gather_batch(ts.device, ts.off_policy_runner, ts.critic_batch, ts.rng);
+                train_critic(ts.device, ts.actor_critic, critic_i == 0 ? ts.actor_critic.critic_1 : ts.actor_critic.critic_2, ts.critic_batch, ts.critic_optimizers[critic_i], ts.actor_buffers[critic_i], ts.critic_buffers[critic_i], ts.critic_training_buffers, ts.rng);
+            }
+            if(ts.step % 1 == 0){
+                {
+                    gather_batch(ts.device, ts.off_policy_runner, ts.actor_batch, ts.rng);
+                    train_actor(ts.device, ts.actor_critic, ts.actor_batch, ts.actor_optimizer, ts.actor_buffers[0], ts.critic_buffers[0], ts.actor_training_buffers, ts.rng);
+                }
+                update_critic_targets(ts.device, ts.actor_critic);
+            }
+        }
+        ts.step++;
+        if(ts.step > CONFIG::PARAMETERS::STEP_LIMIT){
+            return true;
+        }
+        else{
+            return finished;
+        }
+    }
+}
+RL_TOOLS_NAMESPACE_WRAPPER_END
+
+
+#endif
