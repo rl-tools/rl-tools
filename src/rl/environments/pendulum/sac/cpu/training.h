5c89116f94cd07a6441ea347f08dc56b04a67643
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-02-01 19:57:40 -0500

    ios benchmark

--- src/rl/environments/pendulum/sac/cpu/training.h
+++ src/rl/environments/pendulum/sac/cpu/training.h
@@ -1,53 +1,65 @@
-#include "config.h"
-#ifdef RL_TOOLS_ENABLE_HDF5
-#include <highfive/H5DataSet.hpp>
-#include <highfive/H5DataSpace.hpp>
-#include <highfive/H5File.hpp>
+#include <rl_tools/operations/cpu_mux.h>
+#include <rl_tools/nn/operations_cpu_mux.h>
+#include <rl_tools/rl/environments/pendulum/operations_cpu.h>
+#include <rl_tools/nn_models/sequential/operations_generic.h>
+#include <rl_tools/nn_models/mlp/operations_generic.h>
+
+
+#include <rl_tools/rl/algorithms/sac/loop/core/config.h>
+#include <rl_tools/rl/loop/steps/evaluation/config.h>
+#include <rl_tools/rl/loop/steps/timing/config.h>
+#include <rl_tools/rl/algorithms/sac/loop/core/operations.h>
+#include <rl_tools/rl/loop/steps/evaluation/operations.h>
+#include <rl_tools/rl/loop/steps/timing/operations.h>
+
+namespace rlt = rl_tools;
+
+using DEVICE = rlt::devices::DEVICE_FACTORY<>;
+using RNG = decltype(rlt::random::default_engine(typename DEVICE::SPEC::RANDOM{}));
+using T = float;
+using TI = typename DEVICE::index_t;
+
+using PENDULUM_SPEC = rlt::rl::environments::pendulum::Specification<T, TI, rlt::rl::environments::pendulum::DefaultParameters<T>>;
+using ENVIRONMENT = rlt::rl::environments::Pendulum<PENDULUM_SPEC>;
+struct LOOP_CORE_PARAMETERS: rlt::rl::algorithms::sac::loop::core::DefaultParameters<T, TI, ENVIRONMENT>{
+    struct SAC_PARAMETERS: rlt::rl::algorithms::sac::DefaultParameters<T, TI, ENVIRONMENT::ACTION_DIM>{
+        static constexpr TI ACTOR_BATCH_SIZE = 100;
+        static constexpr TI CRITIC_BATCH_SIZE = 100;
+    };
+    static constexpr TI STEP_LIMIT = 10000;
+    static constexpr TI ACTOR_NUM_LAYERS = 3;
+    static constexpr TI ACTOR_HIDDEN_DIM = 64;
+    static constexpr TI CRITIC_NUM_LAYERS = 3;
+    static constexpr TI CRITIC_HIDDEN_DIM = 64;
+};
+#ifdef BENCHMARK
+using LOOP_CORE_CONFIG = rlt::rl::algorithms::sac::loop::core::Config<T, TI, RNG, ENVIRONMENT, LOOP_CORE_PARAMETERS>;
+using LOOP_TIMING_CONFIG = rlt::rl::loop::steps::timing::Config<LOOP_CORE_CONFIG>;
+using LOOP_CONFIG = LOOP_TIMING_CONFIG;
+#else
+using RNG = decltype(rlt::random::default_engine(typename DEVICE::SPEC::RANDOM{}));
+using LOOP_CORE_CONFIG = rlt::rl::algorithms::sac::loop::core::Config<T, TI, RNG, ENVIRONMENT, LOOP_CORE_PARAMETERS, rlt::rl::algorithms::sac::loop::core::ConfigApproximatorsMLP>;
+using LOOP_EVAL_CONFIG = rlt::rl::loop::steps::evaluation::Config<LOOP_CORE_CONFIG>;
+using LOOP_TIMING_CONFIG = rlt::rl::loop::steps::timing::Config<LOOP_EVAL_CONFIG>;
+using LOOP_CONFIG = LOOP_TIMING_CONFIG;
 #endif
 
-#include <chrono>
+using LOOP_STATE = LOOP_CONFIG::State<LOOP_CONFIG>;
 
-using TrainingConfig = training_config::TrainingConfig;
-
-int run(int base_seed = 0){
-    using T = typename TrainingConfig::T;
-    using TI = typename TrainingConfig::TI;
-    using DEVICE = typename TrainingConfig::DEVICE;
-    TI NUM_RUNS = 1;
-#ifdef RL_TOOLS_ENABLE_HDF5
-    std::string DATA_FILE_PATH = "rl_environments_pendulum_sac_learning_curves.h5";
-    auto data_file = HighFive::File(DATA_FILE_PATH, HighFive::File::Overwrite);
-#endif
-
-    for(TI run_i = 0; run_i < NUM_RUNS; run_i++){
-        auto start = std::chrono::high_resolution_clock::now();
-        std::cout << "Run: " << run_i << std::endl;
-        rlt::rl::algorithms::sac::loop::TrainingState<TrainingConfig> ts;
-        TI seed = base_seed + run_i;
-        rlt::rl::algorithms::sac::loop::init(ts, seed);
-        ts.off_policy_runner.parameters.exploration_noise = 0;
-#ifdef RL_TOOLS_ENABLE_HDF5
-        auto run_group = data_file.createGroup(std::to_string(run_i));
-#endif
-        for(TI step_i=0; step_i < TrainingConfig::STEP_LIMIT; step_i++){
-            rlt::rl::algorithms::sac::loop::step(ts);
-//            if(step_i % 1000 == 0){
-////                std::cout << "alpha: " << rlt::math::exp(DEVICE::SPEC::MATH{}, rlt::get(ts.actor_critic.log_alpha.parameters, 0, 0)) << std::endl;
-//            }
-        }
+void run(TI seed = 0){
+    DEVICE device;
+    LOOP_STATE ts;
+    rlt::malloc(device, ts);
+    rlt::init(device, ts, 0);
+    while(!rlt::step(device, ts)){
 #ifndef BENCHMARK
-        std::vector<size_t> dims{decltype(ts)::N_EVALUATIONS};
-        std::vector<T> mean_returns;
-        for(TI i=0; i < decltype(ts)::N_EVALUATIONS; i++){
-            mean_returns.push_back(ts.evaluation_results[i].returns_mean);
+        if(ts.step == 5000){
+            std::cout << "steppin yourself > callbacks 'n' hooks: " << ts.step << std::endl;
         }
-#ifdef RL_TOOLS_ENABLE_HDF5
-        run_group.createDataSet("episode_returns", mean_returns);
 #endif
-#endif
-        rlt::rl::algorithms::sac::loop::destroy(ts);
-        auto end = std::chrono::high_resolution_clock::now();
-        std::cout << "Run time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() / 1000.0 << "s" << std::endl;
     }
-    return 0;
+    rlt::free(device, ts);
 }
+
+
+// benchmark training should take < 2s on P1, < 0.75 on M3
