516934f4b6a324deb5535e1b798d0d38df244411
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-02-04 21:28:08 -0500

    wasm builds

--- include/rl_tools/ui_server/client/client.h
+++ include/rl_tools/ui_server/client/client.h
@@ -2,19 +2,10 @@
 #if (defined(RL_TOOLS_DISABLE_INCLUDE_GUARDS) || !defined(RL_TOOLS_UI_SERVER_CLIENT_CLIENT_H)) && (RL_TOOLS_USE_THIS_VERSION == 1)
 #pragma once
 #define RL_TOOLS_UI_SERVER_CLIENT_CLIENT_H
-#include <boost/beast/core.hpp>
-#include <boost/beast/websocket.hpp>
-#include <boost/asio/connect.hpp>
-#include <boost/asio/ip/tcp.hpp>
 #include <queue>
 RL_TOOLS_NAMESPACE_WRAPPER_START
 
 namespace rl_tools::ui_server::client{
-    namespace beast = boost::beast;
-    namespace http = beast::http;
-    namespace websocket = beast::websocket;
-    namespace net = boost::asio;
-    using tcp = boost::asio::ip::tcp;
 
     template<typename T_ENVIRONMENT>
     struct UIBuffered{
@@ -22,14 +13,6 @@ namespace rl_tools::ui_server::client{
         std::queue<std::string> buffer;
     };
 
-    template<typename T_ENVIRONMENT>
-    struct UI: UIBuffered<T_ENVIRONMENT>{
-        using ENVIRONMENT = T_ENVIRONMENT;
-        std::string host = "localhost";
-        std::string port = "8000";
-        net::io_context ioc;
-        websocket::stream <tcp::socket> ws{ioc};
-    };
 }
 RL_TOOLS_NAMESPACE_WRAPPER_END
 
---
+++ src/rl/environments/car/wasm_interface.cpp
@@ -0,0 +1,41 @@
+#include "training.h"
+#include <emscripten.h>
+
+DEVICE device;
+extern "C" {
+    EMSCRIPTEN_KEEPALIVE
+    State* proxy_create(int seed){
+        return create(seed);
+    }
+
+    EMSCRIPTEN_KEEPALIVE
+    float proxy_step(State* ts){
+        return step(ts);
+    }
+
+    EMSCRIPTEN_KEEPALIVE
+    float proxy_step_message(State* ts, char* message){
+        return step(ts, message);
+    }
+
+    EMSCRIPTEN_KEEPALIVE
+    void proxy_destroy(State* ts){
+        destroy(ts);
+    }
+    int proxy_num_messages(State* ts){
+        return ts->ts.ui.buffer.size();
+    }
+    char* proxy_pop_message(State* ts){
+        if(ts->ts.ui.buffer.empty()){
+            return nullptr;
+        }
+        std::string message = ts->ts.ui.buffer.front();
+        ts->ts.ui.buffer.pop();
+        char* cstr = new char[message.length() + 1];
+        strcpy(cstr, message.c_str());
+        return cstr;
+    }
+    void proxy_delete_message(char* message){
+        delete[] message;
+    }
+}
