eca01ea2d152ef855f2d77a39dd8b8d88c9cc7eb
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-02-04 23:39:31 -0500

    doing something

--- include/rl_tools/ui_server/static/car/training_worker.js
+++ include/rl_tools/ui_server/static/car/training_worker.js
@@ -8,33 +8,34 @@ let state_dim = null;
 let current_episode = null;
 
 async function async_main(){
-    const loaded = await createRLtoolsInterface().then((rlt_emscripten) => {
-        console.log("Initializing worker");
-        self.addEventListener("message", async (event) => {
-            console.log("Message received from main script: ", event.data);
-        })
-        return {
-            module: rlt_emscripten,
-            bindings: {
-                proxy_create: rlt_emscripten._proxy_create,
-                proxy_step: rlt_emscripten._proxy_step,
-                proxy_step_message: rlt_emscripten._proxy_step_message,
-                proxy_destroy: rlt_emscripten._proxy_destroy,
-                proxy_num_messages: rlt_emscripten._proxy_num_messages,
-                proxy_pop_message: rlt_emscripten._proxy_pop_message,
-                proxy_delete_message: rlt_emscripten._proxy_delete_message,
-            }
-        }
-    })
-    const rlt = loaded.bindings;
-    let training_state = rlt.proxy_create(0);
+    const rlt = await createRLtoolsInterface()
+    console.log("Initializing worker");
+    let training_state = rlt._proxy_create(0);
     console.log("Training state: ", training_state);
-    console.log("Num messages: ", )
-    while(rlt.proxy_num_messages(training_state) > 0){
-        const message_pointer = rlt.proxy_pop_message(training_state);
-        const message = loaded.module.UTF8ToString(message_pointer);
-        
+
+    let messages = []
+    self.addEventListener("message", async (event) => {
+        messages.push(event.data)
+    })
+
+
+    let main = () =>{
+        let sleep = 0;
+        while(messages.length > 0){
+            const message = messages.shift();
+            const message_string = JSON.stringify(message)
+            let message_ptr = rlt.stringToNewUTF8(message_string);
+            sleep += rlt._proxy_step_message(training_state, message_ptr)
+        }
+        sleep += rlt._proxy_step(training_state)
+        while(rlt._proxy_num_messages(training_state) > 0){
+            const message_pointer = rlt._proxy_pop_message(training_state);
+            const message = rlt.UTF8ToString(message_pointer);
+            self.postMessage(JSON.parse(message))
+        }
+        setTimeout(main, sleep);
     }
+    main()
 }
 async_main()
     // console.log("Message received from main script: ", event.data);
