05213aac4a780f4127405a04f0349f68ef259074
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-04-23 18:27:46 -0400

    using log-prob functions

--- include/rl_tools/rl/components/on_policy_runner/operations_generic_per_env.h
+++ include/rl_tools/rl/components/on_policy_runner/operations_generic_per_env.h
@@ -36,10 +36,12 @@ namespace rl_tools::rl::components::on_policy_runner::per_env{
 //                    std::stringstream topic;
 //                    topic << "action/" << action_i;
 //                    add_scalar(device, device.logger, topic.str(), action_mu);
-            T action_std = math::exp(device.math, get(action_log_std, 0, action_i));
+            T current_action_log_std = get(action_log_std, env_i, action_i);
+            T action_std = math::exp(device.math, current_action_log_std);
             T action_noisy = random::normal_distribution::sample(typename DEVICE::SPEC::RANDOM(), action_mean, action_std, rng);
-            T action_by_action_std = (action_noisy-action_mean) / action_std;
-            action_log_prob += -0.5 * action_by_action_std * action_by_action_std - math::log(device.math, action_std) - 0.5 * math::log(device.math, 2 * math::PI<T>);
+//            T action_by_action_std = (action_noisy-action_mean) / action_std;
+//            action_log_prob += -0.5 * action_by_action_std * action_by_action_std - math::log(device.math, action_std) - 0.5 * math::log(device.math, 2 * math::PI<T>);
+            action_log_prob += random::normal_distribution::log_prob(device.random, action_mean, current_action_log_std, action_noisy);
             set(actions, env_i, action_i, action_noisy);
         }
         set(dataset.action_log_probs, pos, 0, action_log_prob);
