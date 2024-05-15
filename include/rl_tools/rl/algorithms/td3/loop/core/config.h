9a1f9fcbe08e6683bb4261aa80c2faf3fb08ef4f
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-05-15 12:05:59 -0400

    adapting td3 loop to new capa

--- include/rl_tools/rl/algorithms/td3/loop/core/config.h
+++ include/rl_tools/rl/algorithms/td3/loop/core/config.h
@@ -43,34 +43,34 @@ namespace rl_tools::rl::algorithms::td3::loop::core{
     // We provide approximators based on the sequential and mlp models. The latter (mlp) allows for a variable number of layers, but is restricted to a uniform hidden layer size while the former allows for arbitrary layers to be combined in a sequential manner. Both support compile-time autodiff
     template<typename T, typename TI, typename ENVIRONMENT, typename PARAMETERS>
     struct ConfigApproximatorsSequential{
-        template <nn::LayerCapability CAPABILITY, typename PARAMETER_TYPE>
+        template <typename CAPABILITY>
         struct ACTOR{
             static constexpr TI HIDDEN_DIM = PARAMETERS::ACTOR_HIDDEN_DIM;
             static constexpr TI BATCH_SIZE = PARAMETERS::TD3_PARAMETERS::ACTOR_BATCH_SIZE;
             static constexpr auto ACTIVATION_FUNCTION = PARAMETERS::ACTOR_ACTIVATION_FUNCTION;
-            using LAYER_1_SPEC = nn::layers::dense::Specification<T, TI, ENVIRONMENT::OBSERVATION_DIM, HIDDEN_DIM, ACTIVATION_FUNCTION, PARAMETER_TYPE, BATCH_SIZE>;
+            using LAYER_1_SPEC = nn::layers::dense::Specification<T, TI, ENVIRONMENT::OBSERVATION_DIM, HIDDEN_DIM, ACTIVATION_FUNCTION, BATCH_SIZE>;
             using LAYER_1 = nn::layers::dense::BindSpecification<LAYER_1_SPEC>;
-            using LAYER_2_SPEC = nn::layers::dense::Specification<T, TI, HIDDEN_DIM, HIDDEN_DIM, ACTIVATION_FUNCTION, PARAMETER_TYPE, BATCH_SIZE>;
+            using LAYER_2_SPEC = nn::layers::dense::Specification<T, TI, HIDDEN_DIM, HIDDEN_DIM, ACTIVATION_FUNCTION, BATCH_SIZE>;
             using LAYER_2 = nn::layers::dense::BindSpecification<LAYER_2_SPEC>;
             static constexpr TI ACTOR_OUTPUT_DIM = ENVIRONMENT::ACTION_DIM;
-            using LAYER_3_SPEC = nn::layers::dense::Specification<T, TI, HIDDEN_DIM, ACTOR_OUTPUT_DIM, nn::activation_functions::ActivationFunction::TANH, PARAMETER_TYPE, BATCH_SIZE>;
+            using LAYER_3_SPEC = nn::layers::dense::Specification<T, TI, HIDDEN_DIM, ACTOR_OUTPUT_DIM, nn::activation_functions::ActivationFunction::TANH, BATCH_SIZE>;
             using LAYER_3 = nn::layers::dense::BindSpecification<LAYER_3_SPEC>;
 
             using IF = nn_models::sequential::Interface<CAPABILITY>;
             using MODEL = typename IF::template Module<LAYER_1::template Layer, typename IF::template Module<LAYER_2::template Layer, typename IF::template Module<LAYER_3::template Layer>>>;
         };
 
-        template <nn::LayerCapability CAPABILITY, typename PARAMETER_TYPE>
+        template <typename CAPABILITY>
         struct CRITIC{
             static constexpr TI HIDDEN_DIM = PARAMETERS::CRITIC_HIDDEN_DIM;
             static constexpr TI BATCH_SIZE = PARAMETERS::TD3_PARAMETERS::CRITIC_BATCH_SIZE;
             static constexpr auto ACTIVATION_FUNCTION = PARAMETERS::CRITIC_ACTIVATION_FUNCTION;
 
-            using LAYER_1_SPEC = nn::layers::dense::Specification<T, TI, ENVIRONMENT::OBSERVATION_DIM + ENVIRONMENT::ACTION_DIM, HIDDEN_DIM, ACTIVATION_FUNCTION, PARAMETER_TYPE, BATCH_SIZE>;
+            using LAYER_1_SPEC = nn::layers::dense::Specification<T, TI, ENVIRONMENT::OBSERVATION_DIM + ENVIRONMENT::ACTION_DIM, HIDDEN_DIM, ACTIVATION_FUNCTION, BATCH_SIZE>;
             using LAYER_1 = nn::layers::dense::BindSpecification<LAYER_1_SPEC>;
-            using LAYER_2_SPEC = nn::layers::dense::Specification<T, TI, HIDDEN_DIM, HIDDEN_DIM, ACTIVATION_FUNCTION, PARAMETER_TYPE, BATCH_SIZE>;
+            using LAYER_2_SPEC = nn::layers::dense::Specification<T, TI, HIDDEN_DIM, HIDDEN_DIM, ACTIVATION_FUNCTION, BATCH_SIZE>;
             using LAYER_2 = nn::layers::dense::BindSpecification<LAYER_2_SPEC>;
-            using LAYER_3_SPEC = nn::layers::dense::Specification<T, TI, HIDDEN_DIM, 1, nn::activation_functions::ActivationFunction::IDENTITY, PARAMETER_TYPE, BATCH_SIZE>;
+            using LAYER_3_SPEC = nn::layers::dense::Specification<T, TI, HIDDEN_DIM, 1, nn::activation_functions::ActivationFunction::IDENTITY, BATCH_SIZE>;
             using LAYER_3 = nn::layers::dense::BindSpecification<LAYER_3_SPEC>;
 
             using IF = nn_models::sequential::Interface<CAPABILITY>;
@@ -81,10 +81,10 @@ namespace rl_tools::rl::algorithms::td3::loop::core{
 
         using OPTIMIZER = nn::optimizers::Adam<OPTIMIZER_SPEC>;
 
-        using ACTOR_TYPE = typename ACTOR<nn::LayerCapability::Gradient, nn::parameters::Adam>::MODEL;
-        using ACTOR_TARGET_TYPE = typename ACTOR<nn::LayerCapability::Forward, nn::parameters::Adam>::MODEL;
-        using CRITIC_TYPE = typename CRITIC<nn::LayerCapability::Gradient, nn::parameters::Adam>::MODEL;
-        using CRITIC_TARGET_TYPE = typename CRITIC<nn::LayerCapability::Forward, nn::parameters::Adam>::MODEL;
+        using ACTOR_TYPE = typename ACTOR<nn::layer_capability::Gradient<nn::parameters::Adam>>::MODEL;
+        using ACTOR_TARGET_TYPE = typename ACTOR<nn::layer_capability::Forward>::MODEL;
+        using CRITIC_TYPE = typename CRITIC<nn::layer_capability::Gradient<nn::parameters::Adam>>::MODEL;
+        using CRITIC_TARGET_TYPE = typename CRITIC<nn::layer_capability::Forward>::MODEL;
     };
 
     template<typename T, typename TI, typename ENVIRONMENT, typename PARAMETERS>
