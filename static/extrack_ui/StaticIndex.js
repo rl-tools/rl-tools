54857d0ca73db71232d5182f8bccf740dca8e4bb
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-05-26 14:27:30 -0400

    bumping docs

---
+++ static/extrack_ui/StaticIndex.js
@@ -0,0 +1,9 @@
+export class Index{
+    constructor(experiments_base_path, static_index){
+        this.experiments_base_path = experiments_base_path;
+        this.static = static_index;
+        this.runs
+    }
+    refresh(){
+    }
+}
