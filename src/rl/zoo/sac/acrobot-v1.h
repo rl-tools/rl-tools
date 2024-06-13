0b60c582cd5e8a9fe63004fed8d8440a3a4a03c7
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-06-13 15:52:32 +0400

    sac pendulum-v1 => acrobot-v1

---
+++ src/rl/zoo/sac/acrobot-v1.h
@@ -0,0 +1,31 @@
+#include <rl_tools/rl/environments/pendulum/operations_cpu.h>
+
+#include <rl_tools/rl/algorithms/sac/loop/core/config.h>
+#include <rl_tools/rl/loop/steps/extrack/config.h>
+#include <rl_tools/rl/loop/steps/checkpoint/config.h>
+#include <rl_tools/rl/loop/steps/evaluation/config.h>
+#include <rl_tools/rl/loop/steps/save_trajectories/config.h>
+#include <rl_tools/rl/loop/steps/timing/config.h>
+
+namespace rlt = rl_tools;
+
+namespace rl_tools::rl::zoo::sac{
+    template <typename DEVICE, typename T, typename TI, typename RNG>
+    struct PendulumV1{
+        using ENVIRONMENT_SPEC = rlt::rl::environments::pendulum::Specification<T, TI, rlt::rl::environments::pendulum::DefaultParameters<T>>;
+        using ENVIRONMENT = rlt::rl::environments::Pendulum<ENVIRONMENT_SPEC>;
+        struct LOOP_CORE_PARAMETERS: rlt::rl::algorithms::sac::loop::core::DefaultParameters<T, TI, ENVIRONMENT>{
+            struct SAC_PARAMETERS: rl::algorithms::sac::DefaultParameters<T, TI, ENVIRONMENT::ACTION_DIM>{
+                static constexpr TI ACTOR_BATCH_SIZE = 100;
+                static constexpr TI CRITIC_BATCH_SIZE = 100;
+            };
+            static constexpr TI STEP_LIMIT = 20000;
+            static constexpr TI ACTOR_NUM_LAYERS = 3;
+            static constexpr TI ACTOR_HIDDEN_DIM = 64;
+            static constexpr TI CRITIC_NUM_LAYERS = 3;
+            static constexpr TI CRITIC_HIDDEN_DIM = 64;
+            static constexpr T ALPHA = 1.0;
+        };
+        using LOOP_CORE_CONFIG = rlt::rl::algorithms::sac::loop::core::Config<T, TI, RNG, ENVIRONMENT, LOOP_CORE_PARAMETERS, rlt::rl::algorithms::sac::loop::core::ConfigApproximatorsSequential>;
+    };
+}
