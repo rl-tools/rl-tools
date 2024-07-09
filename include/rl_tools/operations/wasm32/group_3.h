25471370cdd84c03c7aa522b60261b8b753861e5
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-07-09 11:29:14 +0400

    adjusting wasm32 device to new container structure

--- include/rl_tools/operations/wasm32/group_3.h
+++ include/rl_tools/operations/wasm32/group_3.h
@@ -4,7 +4,7 @@
 #define RL_TOOLS_OPERATIONS_WASM32_GROUP_3_H
 #if defined(RL_TOOLS_DISABLE_INCLUDE_GUARDS) || !defined(RL_TOOLS_OPERATIONS_WASM32_GROUP_3)
     #define RL_TOOLS_OPERATIONS_WASM32_GROUP_3
-    #include "../../containers/operations_wasm32.h"
+    #include "../../containers/matrix/operations_wasm32.h"
 #else
     #error "Group 3 already imported"
 #endif
