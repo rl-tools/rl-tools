640a67a6e601b828b563ed1e8af9502ba2781d1f
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-07-31 17:50:52 +0400

    adding dense decoder helps

--- tests/src/nn/layers/gru/gru_training.cpp
+++ tests/src/nn/layers/gru/gru_training.cpp
@@ -2,6 +2,7 @@
 #include <rl_tools/nn/optimizers/adam/instance/operations_generic.h>
 #include <rl_tools/nn/layers/embedding/operations_generic.h>
 #include <rl_tools/nn/layers/gru/operations_generic.h>
+#include <rl_tools/nn/layers/dense/operations_generic.h>
 #include <rl_tools/nn/optimizers/adam/operations_generic.h>
 #include <rl_tools/nn/loss_functions/categorical_cross_entropy/operations_generic.h>
 #include "dataset.h"
@@ -16,6 +17,7 @@ constexpr TI NUM_CLASSES = 2<<7;
 constexpr TI EMBEDDING_DIM = 32;
 constexpr TI BATCH_SIZE = 32;
 constexpr TI SEQUENCE_LENGTH = 32;
+constexpr TI HIDDEN_DIM = 32;
 constexpr TI OUTPUT_DIM = NUM_CLASSES;
 
 template <TI BATCH_SIZE>
@@ -45,18 +47,26 @@ int main() {
     using INPUT_SPEC = rlt::tensor::Specification<unsigned char, TI, INPUT_SHAPE>;
 
     using CAPABILITY = rlt::nn::layer_capability::Gradient<rlt::nn::parameters::Adam, BATCH_SIZE>;
+    using DENSE_CAPABILITY = rlt::nn::layer_capability::Gradient<rlt::nn::parameters::Adam, SEQUENCE_LENGTH*BATCH_SIZE>;
 
     using EMBEDDING_LAYER_SPEC = rlt::nn::layers::embedding::Specification<T, TI, NUM_CLASSES, EMBEDDING_DIM, INPUT_SHAPE_TEMPLATE>;
     using EMBEDDING_LAYER = rlt::nn::layers::embedding::Layer<CAPABILITY, EMBEDDING_LAYER_SPEC>;
     EMBEDDING_LAYER embedding_layer;
     EMBEDDING_LAYER::Buffer<BATCH_SIZE> embedding_buffer;
 
-    using GRU_SPEC = rlt::nn::layers::gru::Specification<T, TI, SEQUENCE_LENGTH, EMBEDDING_DIM, OUTPUT_DIM, rlt::nn::parameters::Gradient>;
+    using GRU_SPEC = rlt::nn::layers::gru::Specification<T, TI, SEQUENCE_LENGTH, EMBEDDING_DIM, HIDDEN_DIM, rlt::nn::parameters::Gradient>;
     rlt::nn::layers::gru::Layer<CAPABILITY, GRU_SPEC> gru;
     decltype(gru)::Buffer<BATCH_SIZE> gru_buffer;
 
+    using DENSE_LAYER_SPEC = rlt::nn::layers::dense::Specification<T, TI, HIDDEN_DIM, OUTPUT_DIM, rlt::nn::activation_functions::ActivationFunction::IDENTITY>;
+    using DENSE_LAYER = rlt::nn::layers::dense::Layer<DENSE_CAPABILITY, DENSE_LAYER_SPEC>;
+    DENSE_LAYER dense_layer;
+    DENSE_LAYER::Buffer<BATCH_SIZE> dense_buffer;
+
     using EMBEDDING_OUTPUT_SPEC = rlt::tensor::Specification<T, TI, decltype(embedding_layer)::OUTPUT_SHAPE>;
-    using OUTPUT_SPEC = rlt::tensor::Specification<T, TI, decltype(gru)::OUTPUT_SHAPE>;
+    using OUTPUT_SHAPE = rlt::tensor::Shape<TI, SEQUENCE_LENGTH, BATCH_SIZE, OUTPUT_DIM>;
+    using OUTPUT_SPEC = rlt::tensor::Specification<T, TI, OUTPUT_SHAPE>;
+    using GRU_OUTPUT_SPEC = rlt::tensor::Specification<T, TI, decltype(gru)::OUTPUT_SHAPE>;
     using OUTPUT_TARGET_SHAPE = rlt::tensor::Shape<TI, SEQUENCE_LENGTH, BATCH_SIZE, 1>;
     using OUTPUT_TARGET_SPEC = rlt::tensor::Specification<T, TI, OUTPUT_TARGET_SHAPE>;
     using ADAM_SPEC = rlt::nn::optimizers::adam::Specification<T, TI>;
@@ -65,19 +75,25 @@ int main() {
     rlt::Tensor<INPUT_SPEC> input;
     rlt::Tensor<EMBEDDING_OUTPUT_SPEC> d_embedding_output;
     rlt::Tensor<OUTPUT_SPEC> d_output;
+    rlt::Tensor<GRU_OUTPUT_SPEC> d_gru_output;
     rlt::Tensor<OUTPUT_TARGET_SPEC> output_target;
     rlt::malloc(device, embedding_layer);
     rlt::malloc(device, embedding_buffer);
     rlt::malloc(device, gru);
     rlt::malloc(device, gru_buffer);
+    rlt::malloc(device, dense_layer);
+    rlt::malloc(device, dense_buffer);
     rlt::malloc(device, input);
     rlt::malloc(device, d_embedding_output);
     rlt::malloc(device, d_output);
+    rlt::malloc(device, d_gru_output);
     rlt::malloc(device, output_target);
     rlt::init_weights(device, embedding_layer, rng);
     rlt::init_weights(device, gru, rng);
+    rlt::init_weights(device, dense_layer, rng);
     rlt::reset_optimizer_state(device, optimizer, embedding_layer);
     rlt::reset_optimizer_state(device, optimizer, gru);
+    rlt::reset_optimizer_state(device, optimizer, dense_layer);
     rlt::print(device, embedding_layer.weights.parameters);
     for(TI epoch_i=0; epoch_i < 10; epoch_i++){
         std::shuffle(dataset.begin(), dataset.end(), rng);
@@ -90,19 +106,25 @@ int main() {
             }
             rlt::forward(device, embedding_layer, input, embedding_buffer, rng);
             rlt::forward(device, gru, rlt::output(embedding_layer), gru_buffer, rng);
-            auto output_logits = rlt::output(gru);
-            auto output_logits_matrix_view = rlt::matrix_view(device, output_logits);
+            auto hidden_state = rlt::matrix_view(device, rlt::output(gru));
+            rlt::forward(device, dense_layer, hidden_state, dense_buffer, rng);
+            auto output_logits = rlt::output(dense_layer);
+//            auto output_logits_matrix_view = rlt::matrix_view(device, output_logits);
             auto output_target_matrix_view = rlt::matrix_view(device, output_target);
             auto d_output_matrix_view = rlt::matrix_view(device, d_output);
-            rlt::nn::loss_functions::categorical_cross_entropy::gradient(device, output_logits_matrix_view, output_target_matrix_view, d_output_matrix_view);
-            T loss = rlt::nn::loss_functions::categorical_cross_entropy::evaluate(device, output_logits_matrix_view, output_target_matrix_view);
-            std::cout << "Sample: " << sample_i << " Loss: " << loss << std::endl;
+            rlt::nn::loss_functions::categorical_cross_entropy::gradient(device, output_logits, output_target_matrix_view, d_output_matrix_view);
+            T loss = rlt::nn::loss_functions::categorical_cross_entropy::evaluate(device, output_logits, output_target_matrix_view);
+            std::cout << "Sample: " << sample_i << " Batch: " << sample_i/BATCH_SIZE << " Loss: " << loss << std::endl;
             rlt::zero_gradient(device, embedding_layer);
             rlt::zero_gradient(device, gru);
-            rlt::backward_full(device, gru, rlt::output(embedding_layer), d_output, d_embedding_output, gru_buffer);
+            rlt::zero_gradient(device, dense_layer);
+            auto d_gru_output_matrix_view = rlt::matrix_view(device, d_gru_output);
+            rlt::backward_full(device, dense_layer, hidden_state, d_output_matrix_view, d_gru_output_matrix_view, dense_buffer);
+            rlt::backward_full(device, gru, rlt::output(embedding_layer), d_gru_output, d_embedding_output, gru_buffer);
             rlt::backward(device, embedding_layer, input, d_embedding_output, embedding_buffer);
             rlt::step(device, optimizer, embedding_layer);
             rlt::step(device, optimizer, gru);
+            rlt::step(device, optimizer, dense_layer);
         }
     }
 
