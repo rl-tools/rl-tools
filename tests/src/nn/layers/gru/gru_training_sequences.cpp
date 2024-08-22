ae622cab8a0b6358689ae837b6da27cc3bfcbbee
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-08-22 11:37:46 -0400

    gru good for 100 steps

--- tests/src/nn/layers/gru/gru_training_sequences.cpp
+++ tests/src/nn/layers/gru/gru_training_sequences.cpp
@@ -84,13 +84,42 @@ struct Config{
     using OUTPUT_TARGET_SHAPE = rlt::tensor::Shape<TI, PARAMS::SEQUENCE_LENGTH, PARAMS::BATCH_SIZE, 1>;
     using OUTPUT_TARGET_SPEC = rlt::tensor::Specification<T, TI, OUTPUT_TARGET_SHAPE>;
     struct ADAM_PARAMS: rlt::nn::optimizers::adam::DEFAULT_PARAMETERS_TENSORFLOW<T>{
-        static constexpr T ALPHA = 0.01;
+        static constexpr T ALPHA = 1e-2;
     };
     using ADAM_SPEC = rlt::nn::optimizers::adam::Specification<T, TI, ADAM_PARAMS>;
     using ADAM = rlt::nn::optimizers::Adam<ADAM_SPEC>;
 };
 
 using CONFIG = Config<T, TI>;
+
+template <bool RESET, typename DEVICE, typename INPUT, typename OUTPUT, typename RESET_CONTAINER, typename RNG>
+void synthetic_sample(DEVICE& device, INPUT& input, OUTPUT& output_target, RESET_CONTAINER& reset, RNG& rng){
+    for(TI batch_i = 0; batch_i < CONFIG::PARAMS::BATCH_SIZE; batch_i++){
+        std::vector<T> values;
+        for(TI sequence_i = 0; sequence_i < CONFIG::PARAMS::SEQUENCE_LENGTH; sequence_i++){
+            // sum in window
+            bool reset_now = RESET && (rlt::random::uniform_real_distribution(device.random, (T)0, (T)1, rng) < 0.2/((T)CONFIG::PARAMS::HORIZON));
+            if(sequence_i == 0 || reset_now){
+                values.clear();
+                rlt::set(device, reset, true, sequence_i, batch_i, 0);
+            }
+            else{
+                rlt::set(device, reset, false, sequence_i, batch_i, 0);
+            }
+            T new_value = 0; //rlt::random::normal_distribution::sample(device.random, (T)0, (T)1, rng);
+            if(rlt::random::uniform_real_distribution(device.random, (T)0, (T)1, rng) < CONFIG::PARAMS::PROBABILITY){
+                new_value = 1;
+            }
+            values.push_back(new_value);
+            while(values.size() > CONFIG::PARAMS::HORIZON){
+                values.erase(values.begin());
+            }
+            T number_of_ones = std::count(values.begin(), values.end(), 1);
+            set(device, input, new_value, sequence_i, batch_i, 0);
+            set(device, output_target, (T)number_of_ones/(CONFIG::PARAMS::HORIZON * CONFIG::PARAMS::PROBABILITY), sequence_i, batch_i, 0);
+        }
+    }
+}
 
 int main(){
     DEVICE device;
@@ -118,50 +147,7 @@ int main(){
         auto start_time = std::chrono::high_resolution_clock::now();
         auto last_print = start_time;
         for(TI sample_i=0; sample_i < CONFIG::PARAMS::DATASET_SIZE; sample_i += CONFIG::PARAMS::BATCH_SIZE){
-            for(TI batch_i = 0; batch_i < CONFIG::PARAMS::BATCH_SIZE; batch_i++){
-                std::vector<T> values;
-                for(TI sequence_i = 0; sequence_i < CONFIG::PARAMS::SEQUENCE_LENGTH; sequence_i++){
-                    constexpr bool MAX_VALUE = false;
-                    if constexpr(MAX_VALUE){
-                        // max value
-                        T new_value = rlt::random::normal_distribution::sample(device.random, (T)0, (T)1, rng);
-                        values.push_back(new_value);
-                        while(values.size() > CONFIG::PARAMS::HORIZON){
-                            values.erase(values.begin());
-                        }
-                        set(device, input, new_value, sequence_i, batch_i, 0);
-                        T max_value = *std::max_element(values.begin(), values.end());
-                        set(device, output_target, max_value, sequence_i, batch_i, 0);
-                        bool reset_now = false; //rlt::random::uniform_real_distribution(device.random, (T)0, (T)1, rng) < 0.1;
-                        if(reset_now){
-                            values.clear();
-                        }
-                    }
-                    else{
-                        // needle in haystack
-                        bool reset_now = rlt::random::uniform_real_distribution(device.random, (T)0, (T)1, rng) < 0.2/((T)CONFIG::PARAMS::HORIZON);
-                        if(sequence_i == 0 || reset_now){
-                            values.clear();
-                            rlt::set(device, reset, true, sequence_i, batch_i, 0);
-                        }
-                        else{
-                            rlt::set(device, reset, false, sequence_i, batch_i, 0);
-                        }
-                        T new_value = 0; //rlt::random::normal_distribution::sample(device.random, (T)0, (T)1, rng);
-                        if(rlt::random::uniform_real_distribution(device.random, (T)0, (T)1, rng) < CONFIG::PARAMS::PROBABILITY){
-                            new_value = 1;
-                        }
-                        values.push_back(new_value);
-                        while(values.size() > CONFIG::PARAMS::HORIZON){
-                            values.erase(values.begin());
-                        }
-                        T number_of_ones = std::count(values.begin(), values.end(), 1);
-                        set(device, input, new_value, sequence_i, batch_i, 0);
-                        set(device, output_target, (T)number_of_ones/(CONFIG::PARAMS::HORIZON * CONFIG::PARAMS::PROBABILITY), sequence_i, batch_i, 0);
-                    }
-
-                }
-            }
+            synthetic_sample<true>(device, input, output_target, reset, rng);
             using RESET_MODE_SPEC = rlt::nn::layers::gru::ResetModeSpecification<rlt::nn::mode::Default, decltype(reset)>;
             using RESET_MODE = rlt::nn::layers::gru::ResetMode<RESET_MODE_SPEC>;
             rlt::nn::Mode<RESET_MODE> reset_mode;
@@ -174,7 +160,13 @@ int main(){
             rlt::nn::loss_functions::mse::gradient(device, output_matrix_view, output_target_matrix_view, d_output_matrix_view);
             T elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_time).count() / 1000.0;
             T elapsed_print = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - last_print).count() / 1000.0;
+            rlt::zero_gradient(device, model);
+            rlt::backward(device, model, input, d_output, buffer, reset_mode);
+            rlt::step(device, optimizer, model);
+
             if(elapsed_print > 2.0 || sample_i % 1000000 == 0){
+                synthetic_sample<false>(device, input, output_target, reset, rng);
+                rlt::forward(device, model, input, buffer, rng, reset_mode);
 #ifdef RL_TOOLS_ENABLE_JSON
                 nlohmann::json batch;
 #endif
@@ -201,25 +193,24 @@ int main(){
 #endif
                     }
                 }
+                T loss = rlt::nn::loss_functions::mse::evaluate(device, output_matrix_view, output_target_matrix_view);
 #ifdef RL_TOOLS_ENABLE_JSON
                 nlohmann::json data;
                 data["meta"]["BATCH_SIZE"] = CONFIG::PARAMS::BATCH_SIZE;
                 data["meta"]["SEQUENCE_LENGTH"] = CONFIG::PARAMS::SEQUENCE_LENGTH;
                 data["meta"]["HORIZON"] = CONFIG::PARAMS::HORIZON;
                 data["meta"]["PROBABILITY"] = CONFIG::PARAMS::PROBABILITY;
+                data["meta"]["HIDDEN_DIM"] = CONFIG::PARAMS::HIDDEN_DIM;
+                data["meta"]["LOSS"] = loss;
                 data["batch"] = batch;
                 std::fstream file;
                 file.open("gru_training_sequences.json", std::ios::out);
                 file << data.dump(4);
                 file.close();
 #endif
-                T loss = rlt::nn::loss_functions::mse::evaluate(device, output_matrix_view, output_target_matrix_view);
                 last_print = std::chrono::high_resolution_clock::now();
                 std::cout << "Epoch: " << epoch_i << " Sample: " << sample_i << " Batch: " << sample_i/CONFIG::PARAMS::BATCH_SIZE << " (" << sample_i/CONFIG::PARAMS::BATCH_SIZE/elapsed << " batch/s)" << " Loss: " << loss << std::endl;
             }
-            rlt::zero_gradient(device, model);
-            rlt::backward(device, model, input, d_output, buffer, reset_mode);
-            rlt::step(device, optimizer, model);
         }
     }
     return 0;
