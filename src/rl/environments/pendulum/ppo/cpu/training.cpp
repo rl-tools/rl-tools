0ded36c23d59c7947737087ac8b1915546d05653
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-10-01 14:35:56 -0400

    cleaning up pendulum ppo

--- src/rl/environments/pendulum/ppo/cpu/training.cpp
+++ src/rl/environments/pendulum/ppo/cpu/training.cpp
@@ -43,42 +43,26 @@ enum class BENCHMARK_MODE: TI{
     SMALL = 2,
     TINY = 3,
 };
-template <typename MODE>
-auto constexpr name(MODE mode){
-    if(mode == BENCHMARK_MODE::LARGE){
-        return "LARGE";
-    }else if(mode == BENCHMARK_MODE::MEDIUM){
-        return "MEDIUM";
-    }else if(mode == BENCHMARK_MODE::SMALL){
-        return "SMALL";
-    }else if(mode == BENCHMARK_MODE::TINY){
-        return "TINY";
-    }else{
-        return "UNKNOWN";
-    }
-}
 
-template <BENCHMARK_MODE MODE>
 struct LOOP_CORE_PARAMETERS: rlt::rl::algorithms::ppo::loop::core::DefaultParameters<T, TI, ENVIRONMENT>{
-    static constexpr TI BATCH_SIZE = MODE == BENCHMARK_MODE::LARGE ? 512 : (MODE == BENCHMARK_MODE::MEDIUM ? 256 : (MODE == BENCHMARK_MODE::SMALL ? 64 : (MODE == BENCHMARK_MODE::TINY ? 32 : 64)));
-    static constexpr TI ACTOR_HIDDEN_DIM = MODE == BENCHMARK_MODE::LARGE ? 256 : (MODE == BENCHMARK_MODE::MEDIUM ? 64 : (MODE == BENCHMARK_MODE::SMALL ? 32 : (MODE == BENCHMARK_MODE::TINY ? 16 : 64)));
-    static constexpr TI CRITIC_HIDDEN_DIM = MODE == BENCHMARK_MODE::LARGE ? 256 : (MODE == BENCHMARK_MODE::MEDIUM ? 64 : (MODE == BENCHMARK_MODE::SMALL ? 32 : (MODE == BENCHMARK_MODE::TINY ? 16 : 64)));
-    static constexpr TI ON_POLICY_RUNNER_STEPS_PER_ENV = MODE == BENCHMARK_MODE::MEDIUM ? 1024 : 256;
-    static constexpr TI N_ENVIRONMENTS = MODE == BENCHMARK_MODE::LARGE ? 16 : 4;
+    static constexpr TI BATCH_SIZE = 256;
+    static constexpr TI ACTOR_HIDDEN_DIM = 64;
+    static constexpr TI CRITIC_HIDDEN_DIM = 64;
+    static constexpr TI ON_POLICY_RUNNER_STEPS_PER_ENV = 1024;
+    static constexpr TI N_ENVIRONMENTS = 4;
     static constexpr TI TOTAL_STEP_LIMIT = 300000;
     static constexpr TI STEP_LIMIT = TOTAL_STEP_LIMIT/(ON_POLICY_RUNNER_STEPS_PER_ENV * N_ENVIRONMENTS) + 1;
     static constexpr TI EPISODE_STEP_LIMIT = 200;
     using OPTIMIZER_PARAMETERS = rlt::nn::optimizers::adam::DEFAULT_PARAMETERS_PYTORCH<T>;
     struct PPO_PARAMETERS: rlt::rl::algorithms::ppo::DefaultParameters<T, TI, BATCH_SIZE>{
         static constexpr T ACTION_ENTROPY_COEFFICIENT = 0.0;
-        static constexpr TI N_EPOCHS = MODE == BENCHMARK_MODE::LARGE ? 5 : (MODE == BENCHMARK_MODE::TINY ? 1 : 2);
+        static constexpr TI N_EPOCHS = 2;
         static constexpr T GAMMA = 0.9;
         static constexpr T INITIAL_ACTION_STD = 2.0;
         static constexpr bool NORMALIZE_OBSERVATIONS = true;
     };
 };
-template <BENCHMARK_MODE MODE>
-using LOOP_CORE_CONFIG = rlt::rl::algorithms::ppo::loop::core::Config<T, TI, RNG, ENVIRONMENT, LOOP_CORE_PARAMETERS<MODE>, rlt::rl::algorithms::ppo::loop::core::ConfigApproximatorsSequential>;
+using LOOP_CORE_CONFIG = rlt::rl::algorithms::ppo::loop::core::Config<T, TI, RNG, ENVIRONMENT, LOOP_CORE_PARAMETERS, rlt::rl::algorithms::ppo::loop::core::ConfigApproximatorsSequential>;
 template <typename NEXT>
 struct LOOP_EVAL_PARAMETERS: rlt::rl::loop::steps::evaluation::Parameters<T, TI, NEXT>{
     static constexpr TI EVALUATION_INTERVAL = 10;
@@ -86,17 +70,16 @@ struct LOOP_EVAL_PARAMETERS: rlt::rl::loop::steps::evaluation::Parameters<T, TI,
     static constexpr TI N_EVALUATIONS = NEXT::CORE_PARAMETERS::STEP_LIMIT / EVALUATION_INTERVAL;
 };
 
-template <BENCHMARK_MODE MODE, TI NUM_EPISODES_FINAL_EVAL>
+template <TI NUM_EPISODES_FINAL_EVAL>
 auto run(TI seed, bool verbose){
     DEVICE device;
 #ifndef RL_TOOLS_RL_ENVIRONMENTS_PENDULUM_PPO_BENCHMARK
-    using LOOP_EVAL_CONFIG = rlt::rl::loop::steps::evaluation::Config<LOOP_CORE_CONFIG<MODE>, LOOP_EVAL_PARAMETERS<LOOP_CORE_CONFIG<MODE>>>;
+    using LOOP_EVAL_CONFIG = rlt::rl::loop::steps::evaluation::Config<LOOP_CORE_CONFIG, LOOP_EVAL_PARAMETERS<LOOP_CORE_CONFIG>>;
     using LOOP_TIMING_CONFIG = rlt::rl::loop::steps::timing::Config<LOOP_EVAL_CONFIG>;
 #else
     using LOOP_TIMING_CONFIG = rlt::rl::loop::steps::timing::Config<LOOP_CORE_CONFIG<MODE>>;
 #endif
     if(verbose){
-        std::cout << "Benchmark mode: " << name(MODE) << std::endl;
         rlt::log(device, LOOP_TIMING_CONFIG{});
     }
     using LOOP_CONFIG = LOOP_TIMING_CONFIG;
@@ -116,45 +99,12 @@ auto run(TI seed, bool verbose){
 }
 
 static constexpr TI NUM_EPISODES_FINAL_EVAL = 1000;
-auto run(TI seed, bool verbose, BENCHMARK_MODE mode){
-    if(mode == BENCHMARK_MODE::LARGE){
-        return run<BENCHMARK_MODE::LARGE, NUM_EPISODES_FINAL_EVAL>(seed, verbose);
-    }else if(mode == BENCHMARK_MODE::MEDIUM){
-        return run<BENCHMARK_MODE::MEDIUM, NUM_EPISODES_FINAL_EVAL>(seed, verbose);
-    }else if(mode == BENCHMARK_MODE::SMALL){
-        return run<BENCHMARK_MODE::SMALL, NUM_EPISODES_FINAL_EVAL>(seed, verbose);
-    }else if(mode == BENCHMARK_MODE::TINY){
-        return run<BENCHMARK_MODE::TINY, NUM_EPISODES_FINAL_EVAL>(seed, verbose);
-    }else{
-        std::cout << "Unknown benchmark mode: " << static_cast<TI>(mode) << std::endl;
-        return decltype(run<BENCHMARK_MODE::TINY, NUM_EPISODES_FINAL_EVAL>(seed, verbose)){};
-    }
-}
-
-
-//int main(int argc, char** argv) {
-//    TI seed = 0;
-//    if (argc > 1) {
-//        seed = std::atoi(argv[1]);
-//    }
-//    BENCHMARK_MODE mode = BENCHMARK_MODE::MEDIUM;
-//    if (argc > 2) {
-//        mode = static_cast<BENCHMARK_MODE>(std::atoi(argv[2]));
-//    }
-//    bool verbose = true;
-//    if (argc > 3) {
-//        verbose = std::atoi(argv[3]);
-//    }
-//    run(seed, verbose, mode);
-//    return 0;
-//}
 
 int main(int argc, char** argv) {
-    BENCHMARK_MODE mode = BENCHMARK_MODE::MEDIUM;
     bool verbose = true;
-    std::vector<decltype(run(0, verbose, mode))> returns;
+    std::vector<decltype(run<NUM_EPISODES_FINAL_EVAL>(0, verbose))> returns;
     for (TI seed=0; seed < 1; seed++){
-        auto return_stats = run(seed, verbose, mode);
+        auto return_stats = run<NUM_EPISODES_FINAL_EVAL>(seed, verbose);
         returns.push_back(return_stats);
     }
     T sum = 0;
