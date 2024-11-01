f4f6f27ef82bbaa963217f3d65fb139a29399c22
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-11-01 12:32:48 -0400

    splitting out dr_sac l2f; ident

---
+++ src/rl/environments/l2f/dr_sac/l2f_dr_sac.cpp
@@ -0,0 +1,147 @@
+#include <rl_tools/operations/cpu_mux.h>
+
+#include <rl_tools/nn/optimizers/adam/instance/operations_generic.h>
+#include <rl_tools/nn/operations_cpu_mux.h>
+#include <rl_tools/nn/layers/sample_and_squash/operations_generic.h>
+#include <rl_tools/nn/layers/td3_sampling/operations_generic.h>
+#include <rl_tools/nn/layers/standardize/operations_generic.h>
+#include <rl_tools/nn_models/mlp/operations_generic.h>
+#include <rl_tools/nn_models/mlp_unconditional_stddev/operations_generic.h>
+#include <rl_tools/nn_models/random_uniform/operations_generic.h>
+#include <rl_tools/nn_models/sequential/operations_generic.h>
+#include <rl_tools/nn_models/multi_agent_wrapper/operations_generic.h>
+#include <rl_tools/nn/optimizers/adam/operations_generic.h>
+
+#ifdef RL_TOOLS_ENABLE_HDF5
+#include <rl_tools/nn/layers/sample_and_squash/persist.h>
+#include <rl_tools/nn/layers/dense/persist.h>
+#include <rl_tools/nn/layers/standardize/persist.h>
+#include <rl_tools/nn/layers/td3_sampling/persist.h>
+#include <rl_tools/nn_models/mlp/persist.h>
+#include <rl_tools/nn_models/sequential/persist.h>
+#include <rl_tools/nn_models/multi_agent_wrapper/persist.h>
+#endif
+
+#include <rl_tools/nn/optimizers/adam/instance/persist_code.h>
+#include <rl_tools/nn/layers/dense/persist_code.h>
+#include <rl_tools/nn/layers/standardize/persist_code.h>
+#include <rl_tools/nn/layers/sample_and_squash/persist_code.h>
+#include <rl_tools/nn/layers/td3_sampling/persist_code.h>
+#include <rl_tools/nn_models/mlp/persist_code.h>
+#include <rl_tools/nn_models/sequential/persist_code.h>
+#include <rl_tools/nn_models/multi_agent_wrapper/persist_code.h>
+
+#include <rl_tools/rl/environments/l2f/operations_cpu.h>
+#include <rl_tools/rl/environments/l2f/parameters/reward_functions/squared.h>
+#include <rl_tools/rl/environments/l2f/parameters/reward_functions/default.h>
+#include <rl_tools/rl/environments/l2f/parameters/default.h>
+#include <rl_tools/rl/environments/l2f/parameters/dynamics/crazyflie.h>
+#include <rl_tools/rl/environments/l2f/parameters/dynamics/race.h>
+#include <rl_tools/rl/environments/l2f/parameters/dynamics/x500_sim.h>
+#include <rl_tools/rl/environments/l2f/parameters/dynamics/x500_real.h>
+#include <rl_tools/rl/environments/l2f/parameters/init/default.h>
+#include <rl_tools/rl/environments/l2f/parameters/termination/default.h>
+
+#include <rl_tools/rl/algorithms/sac/loop/core/operations_generic.h>
+#include <rl_tools/rl/loop/steps/extrack/operations_cpu.h>
+#include <rl_tools/rl/loop/steps/checkpoint/operations_cpu.h>
+#include <rl_tools/rl/loop/steps/evaluation/operations_generic.h>
+#include <rl_tools/rl/loop/steps/save_trajectories/operations_cpu.h>
+#include <rl_tools/rl/loop/steps/timing/operations_cpu.h>
+
+
+#include <rl_tools/utils/generic/typing.h>
+
+namespace rlt = rl_tools;
+
+using DEVICE = rlt::devices::DEVICE_FACTORY<>;
+using TI = typename DEVICE::index_t;
+using RNG = decltype(rlt::random::default_engine(typename DEVICE::SPEC::RANDOM{}));
+using T = float;
+constexpr TI BASE_SEED = 0;
+
+struct ENVIRONMENT_CONFIG{
+    static constexpr bool ZERO_ORIENTATION_INIT = false;
+};
+using ENVIRONMENT = typename rlt::rl::environments::l2f::parameters::DefaultParameters<T, TI, ENVIRONMENT_CONFIG>::ENVIRONMENT;
+struct LOOP_CORE_PARAMETERS: rlt::rl::algorithms::sac::loop::core::DefaultParameters<T, TI, ENVIRONMENT>{
+    struct SAC_PARAMETERS: rlt::rl::algorithms::sac::DefaultParameters<T, TI>{
+        static constexpr TI ACTOR_BATCH_SIZE = 256;
+        static constexpr TI CRITIC_BATCH_SIZE = 256;
+        static constexpr TI TRAINING_INTERVAL = 10;
+        static constexpr TI CRITIC_TRAINING_INTERVAL = 1 * TRAINING_INTERVAL;
+        static constexpr TI ACTOR_TRAINING_INTERVAL = 2 * TRAINING_INTERVAL;
+        static constexpr TI CRITIC_TARGET_UPDATE_INTERVAL = 1 * TRAINING_INTERVAL;
+        static constexpr T TARGET_NEXT_ACTION_NOISE_CLIP = 0.9;
+        static constexpr T TARGET_NEXT_ACTION_NOISE_STD = 0.3;
+        static constexpr T GAMMA = 0.99;
+        static constexpr bool IGNORE_TERMINATION = false;
+        static constexpr T TARGET_ENTROPY = -((T)4);
+    };
+    static constexpr TI STEP_LIMIT = 10000000;
+    static constexpr TI REPLAY_BUFFER_CAP = STEP_LIMIT;
+    static constexpr TI ACTOR_NUM_LAYERS = 3;
+    static constexpr TI ACTOR_HIDDEN_DIM = 64;
+    static constexpr auto ACTOR_ACTIVATION_FUNCTION = rlt::nn::activation_functions::ActivationFunction::FAST_TANH;
+    static constexpr TI CRITIC_NUM_LAYERS = 3;
+    static constexpr TI CRITIC_HIDDEN_DIM = 64;
+    static constexpr auto CRITIC_ACTIVATION_FUNCTION = rlt::nn::activation_functions::ActivationFunction::FAST_TANH;
+    static constexpr TI EPISODE_STEP_LIMIT = 500;
+//            static constexpr bool SHARED_BATCH = false;
+    struct ACTOR_OPTIMIZER_PARAMETERS: rlt::nn::optimizers::adam::DEFAULT_PARAMETERS_TENSORFLOW<T> {
+        static constexpr T ALPHA = 0.0001;
+    };
+    struct CRITIC_OPTIMIZER_PARAMETERS: rlt::nn::optimizers::adam::DEFAULT_PARAMETERS_TENSORFLOW<T> {
+        static constexpr T ALPHA = 0.0001;
+    };
+    struct ALPHA_OPTIMIZER_PARAMETERS: rlt::nn::optimizers::adam::DEFAULT_PARAMETERS_TENSORFLOW<T> {
+        static constexpr T ALPHA = 0.001;
+    };
+};
+
+using LOOP_CORE_CONFIG = rlt::rl::algorithms::sac::loop::core::Config<T, TI, RNG, ENVIRONMENT, LOOP_CORE_PARAMETERS, rlt::rl::algorithms::sac::loop::core::ConfigApproximatorsSequential>;
+
+constexpr TI NUM_CHECKPOINTS = 10;
+constexpr TI NUM_EVALUATIONS = 100;
+constexpr TI NUM_SAVE_TRAJECTORIES = 10;
+using LOOP_EXTRACK_CONFIG = rlt::rl::loop::steps::extrack::Config<LOOP_CORE_CONFIG>;
+struct LOOP_CHECKPOINT_PARAMETERS: rlt::rl::loop::steps::checkpoint::Parameters<T, TI>{
+    static constexpr TI CHECKPOINT_INTERVAL_TEMP = LOOP_CORE_CONFIG::CORE_PARAMETERS::STEP_LIMIT / NUM_CHECKPOINTS;
+    static constexpr TI CHECKPOINT_INTERVAL = CHECKPOINT_INTERVAL_TEMP == 0 ? 1 : CHECKPOINT_INTERVAL_TEMP;
+};
+using LOOP_CHECKPOINT_CONFIG = rlt::rl::loop::steps::checkpoint::Config<LOOP_EXTRACK_CONFIG, LOOP_CHECKPOINT_PARAMETERS>;
+struct LOOP_EVALUATION_PARAMETERS: rlt::rl::loop::steps::evaluation::Parameters<T, TI, LOOP_CHECKPOINT_CONFIG>{
+    static constexpr TI EVALUATION_INTERVAL_TEMP = LOOP_CORE_CONFIG::CORE_PARAMETERS::STEP_LIMIT / NUM_EVALUATIONS;
+    static constexpr TI EVALUATION_INTERVAL = EVALUATION_INTERVAL_TEMP == 0 ? 1 : EVALUATION_INTERVAL_TEMP;
+    static constexpr TI NUM_EVALUATION_EPISODES = 10;
+    static constexpr TI N_EVALUATIONS = LOOP_CORE_CONFIG::CORE_PARAMETERS::STEP_LIMIT / EVALUATION_INTERVAL;
+};
+using LOOP_EVALUATION_CONFIG = rlt::rl::loop::steps::evaluation::Config<LOOP_CHECKPOINT_CONFIG, LOOP_EVALUATION_PARAMETERS>;
+struct LOOP_SAVE_TRAJECTORIES_PARAMETERS: rlt::rl::loop::steps::save_trajectories::Parameters<T, TI, LOOP_CHECKPOINT_CONFIG>{
+    static constexpr TI INTERVAL_TEMP = LOOP_CORE_CONFIG::CORE_PARAMETERS::STEP_LIMIT / NUM_SAVE_TRAJECTORIES;
+    static constexpr TI INTERVAL = INTERVAL_TEMP == 0 ? 1 : INTERVAL_TEMP;
+    static constexpr TI NUM_EPISODES = 10;
+};
+using LOOP_SAVE_TRAJECTORIES_CONFIG = rlt::rl::loop::steps::save_trajectories::Config<LOOP_EVALUATION_CONFIG, LOOP_SAVE_TRAJECTORIES_PARAMETERS>;
+using LOOP_TIMING_CONFIG = rlt::rl::loop::steps::timing::Config<LOOP_SAVE_TRAJECTORIES_CONFIG>;
+using LOOP_CONFIG = LOOP_TIMING_CONFIG;
+
+using LOOP_STATE = typename LOOP_CONFIG::State<LOOP_CONFIG>;
+
+int main() {
+    DEVICE device;
+    LOOP_STATE ts;
+    TI seed = 11;
+    ts.extrack_name = "zoo";
+    ts.extrack_population_variates = "algorithm_environment";
+    ts.extrack_population_values = "dr-sac_l2f";
+    rlt::malloc(device);
+    rlt::init(device);
+    rlt::malloc(device, ts);
+    rlt::init(device, ts, seed);
+#ifdef RL_TOOLS_ENABLE_TENSORBOARD
+    rlt::init(device, device.logger, ts.extrack_seed_path);
+#endif
+    while(!rlt::step(device, ts)){
+    }
+}
