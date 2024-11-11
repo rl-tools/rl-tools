15acd8c347c5271c6f0e580c9cadc48c5c722654
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-11-11 13:11:20 -0500

    vendoring cblas

--- include/rl_tools/devices/cpu_openblas.h
+++ include/rl_tools/devices/cpu_openblas.h
@@ -8,7 +8,17 @@
 
 #include "cpu_blas.h"
 
-#include <cblas.h>
+// based on: https://github.com/OpenMathLib/OpenBLAS/blob/24c5152fbd8027a39759acf261e1595326ab0114/cblas.h
+extern "C" {
+    typedef enum CBLAS_ORDER     {CblasRowMajor=101, CblasColMajor=102} CBLAS_ORDER;
+    typedef enum CBLAS_TRANSPOSE {CblasNoTrans=111, CblasTrans=112, CblasConjTrans=113, CblasConjNoTrans=114} CBLAS_TRANSPOSE;
+    #ifndef OPENBLAS_CONST
+    # define OPENBLAS_CONST const
+    #endif
+    typedef int blasint;
+    void cblas_sgemm(OPENBLAS_CONST enum CBLAS_ORDER Order, OPENBLAS_CONST enum CBLAS_TRANSPOSE TransA, OPENBLAS_CONST enum CBLAS_TRANSPOSE TransB, OPENBLAS_CONST blasint M, OPENBLAS_CONST blasint N, OPENBLAS_CONST blasint K, OPENBLAS_CONST float alpha, OPENBLAS_CONST float *A, OPENBLAS_CONST blasint lda, OPENBLAS_CONST float *B, OPENBLAS_CONST blasint ldb, OPENBLAS_CONST float beta, float *C, OPENBLAS_CONST blasint ldc);
+    void cblas_dgemm(OPENBLAS_CONST enum CBLAS_ORDER Order, OPENBLAS_CONST enum CBLAS_TRANSPOSE TransA, OPENBLAS_CONST enum CBLAS_TRANSPOSE TransB, OPENBLAS_CONST blasint M, OPENBLAS_CONST blasint N, OPENBLAS_CONST blasint K, OPENBLAS_CONST double alpha, OPENBLAS_CONST double *A, OPENBLAS_CONST blasint lda, OPENBLAS_CONST double *B, OPENBLAS_CONST blasint ldb, OPENBLAS_CONST double beta, double *C, OPENBLAS_CONST blasint ldc);
+}
 
 RL_TOOLS_NAMESPACE_WRAPPER_START
 namespace rl_tools::devices{
