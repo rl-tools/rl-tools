4a79ebcf397bdaf9940d879f44274910f6946028
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-11-14 16:29:35 -0500

    l2f ident

--- src/rl/zoo/l2f/environment.h
+++ src/rl/zoo/l2f/environment.h
@@ -23,15 +23,11 @@ namespace rl_tools::rl::zoo::l2f{
     using namespace rl_tools::rl::environments::l2f;
     template <typename DEVICE, typename T, typename TI>
     struct ENVIRONMENT_FACTORY{
-        static constexpr bool IDENT = false;
-        static constexpr bool ZERO_ANGLE_INIT = false;
-        static constexpr bool SAMPLE_ENV_PARAMETERS = IDENT || true;
-        static constexpr bool SEQUENTIAL = false;
 
         static constexpr auto MODEL = rl_tools::rl::environments::l2f::parameters::dynamics::REGISTRY::crazyflie;
 
         constexpr static auto MODEL_NAME = rl_tools::rl::environments::l2f::parameters::dynamics::registry_name<MODEL>;
-        static constexpr auto reward_function = IDENT ? rl_tools::rl::environments::l2f::parameters::reward_functions::squared<T> : rl_tools::rl::environments::l2f::parameters::reward_functions::squared<T>;
+        static constexpr auto reward_function = rl_tools::rl::environments::l2f::parameters::reward_functions::squared<T>;
         using REWARD_FUNCTION_CONST = typename rl_tools::utils::typing::remove_cv_t<decltype(reward_function)>;
         using REWARD_FUNCTION = typename rl_tools::utils::typing::remove_cv<REWARD_FUNCTION_CONST>::type;
 
@@ -42,25 +38,18 @@ namespace rl_tools::rl::zoo::l2f{
         static constexpr typename PARAMETERS_TYPE::Integration integration = {
             0.01 // integration dt
         };
-        static constexpr typename PARAMETERS_TYPE::MDP::Initialization init = IDENT ? rl_tools::rl::environments::l2f::parameters::init::init_90_deg<PARAMETERS_SPEC> : (ZERO_ANGLE_INIT ? rl_tools::rl::environments::l2f::parameters::init::init_0_deg<PARAMETERS_SPEC> : rl_tools::rl::environments::l2f::parameters::init::init_90_deg<PARAMETERS_SPEC>);
+        static constexpr typename PARAMETERS_TYPE::MDP::Initialization init = rl_tools::rl::environments::l2f::parameters::init::init_90_deg<PARAMETERS_SPEC>;
         static constexpr typename PARAMETERS_TYPE::MDP::ObservationNoise observation_noise = {
-            0, // 0.01, // position
-            0, // 0.001, // orientation
-            0, // 0.01, // linear_velocity
-            0, // 0.01, // angular_velocity
-            0, // 0.0, // imu acceleration
+            0, // position
+            0, // orientation
+            0, // linear_velocity
+            0, // angular_velocity
+            0, // imu acceleration
         };
-        //        static constexpr typename PARAMETERS_TYPE::MDP::ObservationNoise observation_noise = {
-        //                0.0, // position
-        //                0.0, // orientation
-        //                0.0, // linear_velocity
-        //                0.0, // angular_velocity
-        //                0.0, // imu acceleration
-        //        };
         static constexpr typename PARAMETERS_TYPE::MDP::ActionNoise action_noise = {
             0, // std of additive gaussian noise onto the normalized action (-1, 1)
         };
-        static constexpr typename PARAMETERS_TYPE::MDP::Termination termination = IDENT ? rl_tools::rl::environments::l2f::parameters::termination::fast_learning<PARAMETERS_SPEC> : rl_tools::rl::environments::l2f::parameters::termination::narrow<PARAMETERS_SPEC>;
+        static constexpr typename PARAMETERS_TYPE::MDP::Termination termination = rl_tools::rl::environments::l2f::parameters::termination::narrow<PARAMETERS_SPEC>;
         static constexpr typename PARAMETERS_TYPE::MDP mdp = {
             init,
             reward_function,
@@ -69,15 +58,15 @@ namespace rl_tools::rl::zoo::l2f{
             termination
         };
         static constexpr typename PARAMETERS_TYPE::DomainRandomization domain_randomization = {
-            true || IDENT ? 0 : 2.0, // thrust_to_weight_min;
-            true || IDENT ? 0 : 5, // thrust_to_weight_max;
-            true || IDENT ? 0 : 0.0026034812863058926, // thrust_to_weight_by_torque_to_inertia_min;
-            true || IDENT ? 0 : 0.025, // thrust_to_weight_by_torque_to_inertia_max;
-            true || IDENT ? 0.0 : 0.02, // mass_min;
-            true || IDENT ? 0.0 : 5, // mass_max;
-            true || IDENT ? 0.0 : 0.1, // mass_size_deviation;
-            true || IDENT ? 0.0 : 0.1, // motor_time_constant;
-            true || IDENT ? 0.0 : 0.1 // rotor_torque_constant;
+            0.0, // thrust_to_weight_min;
+            0.0, // thrust_to_weight_max;
+            0.0, // thrust_to_weight_by_torque_to_inertia_min;
+            0.0, // thrust_to_weight_by_torque_to_inertia_max;
+            0.0, // mass_min;
+            0.0, // mass_max;
+            0.0, // mass_size_deviation;
+            0.0, // motor_time_constant;
+            0.0  // rotor_torque_constant;
         };
         static constexpr typename PARAMETERS_TYPE::Disturbances disturbances = {
             typename PARAMETERS_TYPE::Disturbances::UnivariateGaussian{0, 0}, // random_force;
@@ -94,7 +83,7 @@ namespace rl_tools::rl::zoo::l2f{
         };
 
         struct ENVIRONMENT_STATIC_PARAMETERS{
-            static constexpr TI ACTION_HISTORY_LENGTH = SEQUENTIAL ? 1 : 16;
+            static constexpr TI ACTION_HISTORY_LENGTH = 16;
             static constexpr TI CLOSED_FORM = false;
             using STATE_BASE = StateBase<T, TI>;
             using STATE_TYPE = StateRotorsHistory<T, TI, ACTION_HISTORY_LENGTH, CLOSED_FORM, StateRandomForce<T, TI, STATE_BASE>>;
