e715373474fe8e03ec5a9443107f8ca3d86c2ac1
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-11-14 16:21:47 -0500

    zoo sac l2f good

--- src/rl/zoo/l2f/environment.h
+++ src/rl/zoo/l2f/environment.h
@@ -20,12 +20,108 @@
 RL_TOOLS_NAMESPACE_WRAPPER_START
 namespace rl_tools::rl::zoo::l2f{
     namespace rlt = rl_tools;
+    using namespace rl_tools::rl::environments::l2f;
     template <typename DEVICE, typename T, typename TI>
     struct ENVIRONMENT_FACTORY{
-        struct ENVIRONMENT_CONFIG{
-            static constexpr bool ZERO_ORIENTATION_INIT = true;
+        static constexpr bool IDENT = false;
+        static constexpr bool ZERO_ANGLE_INIT = false;
+        static constexpr bool SAMPLE_ENV_PARAMETERS = IDENT || true;
+        static constexpr bool SEQUENTIAL = false;
+
+        static constexpr auto MODEL = rl_tools::rl::environments::l2f::parameters::dynamics::REGISTRY::crazyflie;
+
+        constexpr static auto MODEL_NAME = rl_tools::rl::environments::l2f::parameters::dynamics::registry_name<MODEL>;
+        static constexpr auto reward_function = IDENT ? rl_tools::rl::environments::l2f::parameters::reward_functions::squared<T> : rl_tools::rl::environments::l2f::parameters::reward_functions::squared<T>;
+        using REWARD_FUNCTION_CONST = typename rl_tools::utils::typing::remove_cv_t<decltype(reward_function)>;
+        using REWARD_FUNCTION = typename rl_tools::utils::typing::remove_cv<REWARD_FUNCTION_CONST>::type;
+
+        using PARAMETERS_SPEC = rl_tools::rl::environments::l2f::ParametersBaseSpecification<T, TI, 4, REWARD_FUNCTION, rl_tools::rl::environments::l2f::parameters::dynamics::REGISTRY, MODEL>;
+        using PARAMETERS_TYPE = rl_tools::rl::environments::l2f::ParametersDisturbances<T, TI, rl_tools::rl::environments::l2f::ParametersBase<PARAMETERS_SPEC>>;
+
+        static constexpr typename PARAMETERS_TYPE::Dynamics dynamics = rl_tools::rl::environments::l2f::parameters::dynamics::registry<PARAMETERS_SPEC>;
+        static constexpr typename PARAMETERS_TYPE::Integration integration = {
+            0.01 // integration dt
         };
-        using ENVIRONMENT = typename rl::environments::l2f::parameters::DefaultParameters<T, TI, ENVIRONMENT_CONFIG>::ENVIRONMENT;
+        static constexpr typename PARAMETERS_TYPE::MDP::Initialization init = IDENT ? rl_tools::rl::environments::l2f::parameters::init::init_90_deg<PARAMETERS_SPEC> : (ZERO_ANGLE_INIT ? rl_tools::rl::environments::l2f::parameters::init::init_0_deg<PARAMETERS_SPEC> : rl_tools::rl::environments::l2f::parameters::init::init_90_deg<PARAMETERS_SPEC>);
+        static constexpr typename PARAMETERS_TYPE::MDP::ObservationNoise observation_noise = {
+            0, // 0.01, // position
+            0, // 0.001, // orientation
+            0, // 0.01, // linear_velocity
+            0, // 0.01, // angular_velocity
+            0, // 0.0, // imu acceleration
+        };
+        //        static constexpr typename PARAMETERS_TYPE::MDP::ObservationNoise observation_noise = {
+        //                0.0, // position
+        //                0.0, // orientation
+        //                0.0, // linear_velocity
+        //                0.0, // angular_velocity
+        //                0.0, // imu acceleration
+        //        };
+        static constexpr typename PARAMETERS_TYPE::MDP::ActionNoise action_noise = {
+            0, // std of additive gaussian noise onto the normalized action (-1, 1)
+        };
+        static constexpr typename PARAMETERS_TYPE::MDP::Termination termination = IDENT ? rl_tools::rl::environments::l2f::parameters::termination::fast_learning<PARAMETERS_SPEC> : rl_tools::rl::environments::l2f::parameters::termination::narrow<PARAMETERS_SPEC>;
+        static constexpr typename PARAMETERS_TYPE::MDP mdp = {
+            init,
+            reward_function,
+            observation_noise,
+            action_noise,
+            termination
+        };
+        static constexpr typename PARAMETERS_TYPE::DomainRandomization domain_randomization = {
+            true || IDENT ? 0 : 2.0, // thrust_to_weight_min;
+            true || IDENT ? 0 : 5, // thrust_to_weight_max;
+            true || IDENT ? 0 : 0.0026034812863058926, // thrust_to_weight_by_torque_to_inertia_min;
+            true || IDENT ? 0 : 0.025, // thrust_to_weight_by_torque_to_inertia_max;
+            true || IDENT ? 0.0 : 0.02, // mass_min;
+            true || IDENT ? 0.0 : 5, // mass_max;
+            true || IDENT ? 0.0 : 0.1, // mass_size_deviation;
+            true || IDENT ? 0.0 : 0.1, // motor_time_constant;
+            true || IDENT ? 0.0 : 0.1 // rotor_torque_constant;
+        };
+        static constexpr typename PARAMETERS_TYPE::Disturbances disturbances = {
+            typename PARAMETERS_TYPE::Disturbances::UnivariateGaussian{0, 0}, // random_force;
+            typename PARAMETERS_TYPE::Disturbances::UnivariateGaussian{0, 0} // random_torque;
+        };
+        static constexpr PARAMETERS_TYPE nominal_parameters = {
+            {
+                dynamics,
+                integration,
+                mdp,
+                domain_randomization
+            },
+            disturbances
+        };
+
+        struct ENVIRONMENT_STATIC_PARAMETERS{
+            static constexpr TI ACTION_HISTORY_LENGTH = SEQUENTIAL ? 1 : 16;
+            static constexpr TI CLOSED_FORM = false;
+            using STATE_BASE = StateBase<T, TI>;
+            using STATE_TYPE = StateRotorsHistory<T, TI, ACTION_HISTORY_LENGTH, CLOSED_FORM, StateRandomForce<T, TI, STATE_BASE>>;
+            using OBSERVATION_TYPE = observation::Position<observation::PositionSpecification<T, TI,
+                    observation::OrientationRotationMatrix<observation::OrientationRotationMatrixSpecification<T, TI,
+                            observation::LinearVelocity<observation::LinearVelocitySpecification<T, TI,
+                                    observation::AngularVelocity<observation::AngularVelocitySpecification<T, TI,
+                                            observation::ActionHistory<observation::ActionHistorySpecification<T, TI, ACTION_HISTORY_LENGTH>>>>>>>>>>;
+            using OBSERVATION_TYPE_PRIVILEGED = observation::Position<observation::PositionSpecificationPrivileged<T, TI,
+                    observation::OrientationRotationMatrix<observation::OrientationRotationMatrixSpecificationPrivileged<T, TI,
+                            observation::LinearVelocity<observation::LinearVelocitySpecificationPrivileged<T, TI,
+                                    observation::AngularVelocity<observation::AngularVelocitySpecificationPrivileged<T, TI,
+                                            observation::RandomForce<observation::RandomForceSpecification<T, TI,
+                                                    observation::RotorSpeeds<observation::RotorSpeedsSpecification<T, TI>>
+                                            >
+                                            >
+                                    >>
+                            >>
+                    >>
+            >>;
+            static constexpr bool PRIVILEGED_OBSERVATION_NOISE = false;
+            using PARAMETERS = PARAMETERS_TYPE;
+            static constexpr auto PARAMETER_VALUES = nominal_parameters;
+        };
+
+        using ENVIRONMENT_SPEC = rl_tools::rl::environments::l2f::Specification<T, TI, ENVIRONMENT_STATIC_PARAMETERS>;
+        using ENVIRONMENT = rl_tools::rl::environments::Multirotor<ENVIRONMENT_SPEC>;
     };
 }
 RL_TOOLS_NAMESPACE_WRAPPER_END
--- src/rl/zoo/l2f/sac.h
+++ src/rl/zoo/l2f/sac.h
@@ -11,9 +11,9 @@ namespace rl_tools::rl::zoo::l2f::sac{
         using ENVIRONMENT = typename ENVIRONMENT_FACTORY<DEVICE, T, TI>::ENVIRONMENT;
         struct LOOP_CORE_PARAMETERS: rlt::rl::algorithms::sac::loop::core::DefaultParameters<T, TI, ENVIRONMENT>{
             struct SAC_PARAMETERS: rlt::rl::algorithms::sac::DefaultParameters<T, TI>{
-                static constexpr TI ACTOR_BATCH_SIZE = 256;
-                static constexpr TI CRITIC_BATCH_SIZE = 256;
-                static constexpr TI TRAINING_INTERVAL = 10;
+                static constexpr TI ACTOR_BATCH_SIZE = 64;
+                static constexpr TI CRITIC_BATCH_SIZE = 64;
+                static constexpr TI TRAINING_INTERVAL = 5;
                 static constexpr TI CRITIC_TRAINING_INTERVAL = 1 * TRAINING_INTERVAL;
                 static constexpr TI ACTOR_TRAINING_INTERVAL = 2 * TRAINING_INTERVAL;
                 static constexpr TI CRITIC_TARGET_UPDATE_INTERVAL = 1 * TRAINING_INTERVAL;
@@ -22,27 +22,36 @@ namespace rl_tools::rl::zoo::l2f::sac{
                 static constexpr T GAMMA = 0.99;
                 static constexpr bool IGNORE_TERMINATION = false;
                 static constexpr T TARGET_ENTROPY = -((T)4);
+                static constexpr TI SEQUENCE_LENGTH = 1;
             };
-            static constexpr TI STEP_LIMIT = 10000000;
+            static constexpr TI STEP_LIMIT = 200000;
             static constexpr TI REPLAY_BUFFER_CAP = STEP_LIMIT;
             static constexpr TI ACTOR_NUM_LAYERS = 3;
-            static constexpr TI ACTOR_HIDDEN_DIM = 64;
-            static constexpr auto ACTOR_ACTIVATION_FUNCTION = nn::activation_functions::ActivationFunction::FAST_TANH;
+            static constexpr TI ACTOR_HIDDEN_DIM = 32;
+            static constexpr auto ACTOR_ACTIVATION_FUNCTION = rlt::nn::activation_functions::ActivationFunction::FAST_TANH;
             static constexpr TI CRITIC_NUM_LAYERS = 3;
-            static constexpr TI CRITIC_HIDDEN_DIM = 64;
-            static constexpr auto CRITIC_ACTIVATION_FUNCTION = nn::activation_functions::ActivationFunction::FAST_TANH;
+            static constexpr TI CRITIC_HIDDEN_DIM = 32;
+            static constexpr auto CRITIC_ACTIVATION_FUNCTION = rlt::nn::activation_functions::ActivationFunction::FAST_TANH;
             static constexpr TI EPISODE_STEP_LIMIT = 500;
-//            static constexpr bool SHARED_BATCH = false;
-            struct ACTOR_OPTIMIZER_PARAMETERS: nn::optimizers::adam::DEFAULT_PARAMETERS_TENSORFLOW<T> {
-                static constexpr T ALPHA = 0.0001;
+        //            static constexpr bool SHARED_BATCH = false;
+            struct OPTIMIZER_PARAMETERS_COMMON: rlt::nn::optimizers::adam::DEFAULT_PARAMETERS_TENSORFLOW<T>{
+                static constexpr bool ENABLE_GRADIENT_CLIPPING = true;
+                static constexpr T GRADIENT_CLIP_VALUE = 1;
+                static constexpr bool ENABLE_WEIGHT_DECAY = true;
+                static constexpr T WEIGHT_DECAY = 0.0001;
             };
-            struct CRITIC_OPTIMIZER_PARAMETERS: nn::optimizers::adam::DEFAULT_PARAMETERS_TENSORFLOW<T> {
-                static constexpr T ALPHA = 0.0001;
+            struct ACTOR_OPTIMIZER_PARAMETERS: OPTIMIZER_PARAMETERS_COMMON{
+                static constexpr T ALPHA = 1e-3;
             };
-            struct ALPHA_OPTIMIZER_PARAMETERS: nn::optimizers::adam::DEFAULT_PARAMETERS_TENSORFLOW<T> {
-                static constexpr T ALPHA = 0.001;
+            struct CRITIC_OPTIMIZER_PARAMETERS: OPTIMIZER_PARAMETERS_COMMON{
+                static constexpr T ALPHA = 1e-3;
             };
+            struct ALPHA_OPTIMIZER_PARAMETERS: OPTIMIZER_PARAMETERS_COMMON{
+                static constexpr T ALPHA = 1e-3;
+            };
+            static constexpr bool SAMPLE_ENVIRONMENT_PARAMETERS = true;
         };
+
         using LOOP_CORE_CONFIG = rlt::rl::algorithms::sac::loop::core::Config<T, TI, RNG, ENVIRONMENT, LOOP_CORE_PARAMETERS, rlt::rl::algorithms::sac::loop::core::ConfigApproximatorsMLP>;
     };
 }
