eb41d9fb0de0f7dd6e75dde93ccbc9aff0f38701
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-11-17 00:53:47 -0500

    wasm pendulum compiles

--- src/rl/environments/pendulum/sac/wasm/wasm_interface.cpp
+++ src/rl/environments/pendulum/sac/wasm/wasm_interface.cpp
@@ -32,10 +32,11 @@ double proxy_get_state_value(TRAINING_STATE* ts, int env_index, int state_index)
     using T = typename TRAINING_STATE::CONFIG::T;
     static_assert(TRAINING_STATE::CONFIG::OFF_POLICY_RUNNER_SPEC::PARAMETERS::N_ENVIRONMENTS == 1);
     if(env_index < TRAINING_STATE::CONFIG::OFF_POLICY_RUNNER_SPEC::PARAMETERS::N_ENVIRONMENTS && state_index < TRAINING_STATE::CONFIG::ENVIRONMENT::State::DIM){
-        if(ts->off_policy_runner.replay_buffers[env_index].position > 0){
-            auto& env = ts->off_policy_runner.envs[env_index];
-            T c = rlt::get(ts->off_policy_runner.replay_buffers[env_index].observations, ts->off_policy_runner.replay_buffers[env_index].position - 1, 0);
-            T s = rlt::get(ts->off_policy_runner.replay_buffers[env_index].observations, ts->off_policy_runner.replay_buffers[env_index].position - 1, 1);
+        auto& rb = rlt::get(ts->off_policy_runner.replay_buffers, 0, env_index);
+        if(rb.position > 0){
+            auto& env = rlt::get(ts->off_policy_runner.envs, 0, env_index);
+            T c = rlt::get(rb.observations, rb.position - 1, 0);
+            T s = rlt::get(rb.observations, rb.position - 1, 1);
             T theta = rlt::math::atan2(device.math, s, c);
             return theta;
         }
@@ -52,7 +53,8 @@ EMSCRIPTEN_KEEPALIVE
 int proxy_get_episode(TRAINING_STATE* ts, int env_index){
     static_assert(TRAINING_STATE::CONFIG::OFF_POLICY_RUNNER_SPEC::PARAMETERS::N_ENVIRONMENTS == 1);
     if(env_index < TRAINING_STATE::CONFIG::OFF_POLICY_RUNNER_SPEC::PARAMETERS::N_ENVIRONMENTS){
-        auto episode = ts->off_policy_runner.episode_stats[env_index].next_episode_i;
+        auto& es = rlt::get(ts->off_policy_runner.episode_stats, 0, env_index);
+        auto episode = es.next_episode_i;
         return episode;
     }
     else{
@@ -64,7 +66,8 @@ EMSCRIPTEN_KEEPALIVE
 double proxy_get_episode_return(TRAINING_STATE* ts, int env_index, int episode_i){
     static_assert(TRAINING_STATE::CONFIG::OFF_POLICY_RUNNER_SPEC::PARAMETERS::N_ENVIRONMENTS == 1);
     if(env_index < TRAINING_STATE::CONFIG::OFF_POLICY_RUNNER_SPEC::PARAMETERS::N_ENVIRONMENTS && episode_i < TRAINING_STATE::CONFIG::OFF_POLICY_RUNNER_SPEC::PARAMETERS::EPISODE_STATS_BUFFER_SIZE){
-        return get(ts->off_policy_runner.episode_stats[env_index].returns, episode_i, 0);
+        auto& es = rlt::get(ts->off_policy_runner.episode_stats, 0, env_index);
+        return rlt::get(es.returns, episode_i, 0);
     }
     else{
         return -1337;
@@ -89,7 +92,7 @@ EMSCRIPTEN_KEEPALIVE
 
 EMSCRIPTEN_KEEPALIVE
     void proxy_destroy_training_state(TRAINING_STATE* ts){
-        free(device, *ts);
+        rlt::free(device, *ts);
         delete ts;
     }
 }
