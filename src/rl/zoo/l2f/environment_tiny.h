cddfcb0bf9bbf992f5aeb438ab8db01abd4a48bb
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-11-26 11:40:45 -0500

    simplifying tiny env; ident

--- src/rl/zoo/l2f/environment_tiny.h
+++ src/rl/zoo/l2f/environment_tiny.h
@@ -25,80 +25,25 @@ namespace rl_tools::rl::zoo::l2f{
     template <typename DEVICE, typename T, typename TI>
     struct ENVIRONMENT_TINY_FACTORY{
         using ENVIRONMENT_FACTORY = ENVIRONMENT_FACTORY<DEVICE, T, TI>;
-        static constexpr auto MODEL = rl_tools::rl::environments::l2f::parameters::dynamics::REGISTRY::crazyflie;
-        constexpr static auto MODEL_NAME = rl_tools::rl::environments::l2f::parameters::dynamics::registry_name<MODEL>;
+        using PARAMETERS_SPEC = typename ENVIRONMENT_FACTORY::PARAMETERS_SPEC;
+        using PARAMETERS_TYPE = typename ENVIRONMENT_FACTORY::PARAMETERS_TYPE;
 
-        using REWARD_FUNCTION = rl_tools::rl::environments::l2f::parameters::reward_functions::Squared<T>;
-        static constexpr REWARD_FUNCTION reward_function = {
-                false, // non-negative
-                00.10, // scale
-                01.00, // constant
-                00.00, // termination penalty
-                10.00, // position
-                02.50, // orientation
-                00.05, // linear_velocity
-                00.00, // angular_velocity
-                00.00, // linear_acceleration
-                00.00, // angular_acceleration
-                00.10, // action
-        };
-
-        using PARAMETERS_SPEC = rl_tools::rl::environments::l2f::ParametersBaseSpecification<T, TI, 4, REWARD_FUNCTION, rl_tools::rl::environments::l2f::parameters::dynamics::REGISTRY, MODEL>;
-        using PARAMETERS_TYPE = rl_tools::rl::environments::l2f::ParametersDisturbances<T, TI, rl_tools::rl::environments::l2f::ParametersBase<PARAMETERS_SPEC>>;
-
-        static constexpr typename PARAMETERS_TYPE::Dynamics dynamics = rl_tools::rl::environments::l2f::parameters::dynamics::registry<PARAMETERS_SPEC>;
-        static constexpr typename PARAMETERS_TYPE::Integration integration = {
-            0.01 // integration dt
-        };
         static constexpr typename PARAMETERS_TYPE::MDP::Initialization init = rl_tools::rl::environments::l2f::parameters::init::init_20_deg<PARAMETERS_SPEC>;
-        static constexpr typename PARAMETERS_TYPE::MDP::ObservationNoise observation_noise = {
-            0, // position
-            0, // orientation
-            0, // linear_velocity
-            0, // angular_velocity
-            0, // imu acceleration
-        };
-        static constexpr typename PARAMETERS_TYPE::MDP::ActionNoise action_noise = {
-            0, // std of additive gaussian noise onto the normalized action (-1, 1)
-        };
-        static constexpr typename PARAMETERS_TYPE::MDP::Termination termination = {
-            true,  // enable
-            1,     // position
-            10,    // linear velocity
-            35,    // angular velocity
-            10000, // position integral
-            50000, // orientation integral
-        };
         static constexpr typename PARAMETERS_TYPE::MDP mdp = {
             init,
-            reward_function,
-            observation_noise,
-            action_noise,
-            termination
-        };
-        static constexpr typename PARAMETERS_TYPE::DomainRandomization domain_randomization = {
-            0.0, // thrust_to_weight_min;
-            0.0, // thrust_to_weight_max;
-            0.0, // thrust_to_weight_by_torque_to_inertia_min;
-            0.0, // thrust_to_weight_by_torque_to_inertia_max;
-            0.0, // mass_min;
-            0.0, // mass_max;
-            0.0, // mass_size_deviation;
-            0.0, // motor_time_constant;
-            0.0  // rotor_torque_constant;
-        };
-        static constexpr typename PARAMETERS_TYPE::Disturbances disturbances = {
-            typename PARAMETERS_TYPE::Disturbances::UnivariateGaussian{0, 0}, // random_force;
-            typename PARAMETERS_TYPE::Disturbances::UnivariateGaussian{0, 0} // random_torque;
+            ENVIRONMENT_FACTORY::reward_function,
+            ENVIRONMENT_FACTORY::observation_noise,
+            ENVIRONMENT_FACTORY::action_noise,
+            ENVIRONMENT_FACTORY::termination
         };
         static constexpr PARAMETERS_TYPE nominal_parameters = {
             {
-                dynamics,
-                integration,
+                ENVIRONMENT_FACTORY::dynamics,
+                ENVIRONMENT_FACTORY::integration,
                 mdp,
-                domain_randomization
+                ENVIRONMENT_FACTORY::domain_randomization
             },
-            disturbances
+            ENVIRONMENT_FACTORY::disturbances
         };
 
         struct ENVIRONMENT_STATIC_PARAMETERS{
@@ -106,23 +51,8 @@ namespace rl_tools::rl::zoo::l2f{
             static constexpr TI CLOSED_FORM = false;
             using STATE_BASE = StateBase<T, TI>;
             using STATE_TYPE = StateRotorsHistory<T, TI, ACTION_HISTORY_LENGTH, CLOSED_FORM, StateRandomForce<T, TI, STATE_BASE>>;
-            using OBSERVATION_TYPE = observation::Position<observation::PositionSpecification<T, TI,
-                    observation::OrientationRotationMatrix<observation::OrientationRotationMatrixSpecification<T, TI,
-                            observation::LinearVelocity<observation::LinearVelocitySpecification<T, TI,
-                                    observation::AngularVelocity<observation::AngularVelocitySpecification<T, TI,
-                                            observation::ActionHistory<observation::ActionHistorySpecification<T, TI, ACTION_HISTORY_LENGTH>>>>>>>>>>;
-            using OBSERVATION_TYPE_PRIVILEGED = observation::Position<observation::PositionSpecificationPrivileged<T, TI,
-                    observation::OrientationRotationMatrix<observation::OrientationRotationMatrixSpecificationPrivileged<T, TI,
-                            observation::LinearVelocity<observation::LinearVelocitySpecificationPrivileged<T, TI,
-                                    observation::AngularVelocity<observation::AngularVelocitySpecificationPrivileged<T, TI,
-                                            observation::RandomForce<observation::RandomForceSpecification<T, TI,
-                                                    observation::RotorSpeeds<observation::RotorSpeedsSpecification<T, TI>>
-                                            >
-                                            >
-                                    >>
-                            >>
-                    >>
-            >>;
+            using OBSERVATION_TYPE = typename ENVIRONMENT_FACTORY::ENVIRONMENT_STATIC_PARAMETERS::OBSERVATION_TYPE;
+            using OBSERVATION_TYPE_PRIVILEGED = typename ENVIRONMENT_FACTORY::ENVIRONMENT_STATIC_PARAMETERS::OBSERVATION_TYPE_PRIVILEGED;
             static constexpr bool PRIVILEGED_OBSERVATION_NOISE = false;
             using PARAMETERS = PARAMETERS_TYPE;
             static constexpr auto PARAMETER_VALUES = nominal_parameters;
