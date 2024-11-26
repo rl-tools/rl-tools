859f7b72fe492a304e6aaae244dfb5dfe9fdb080
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-11-26 10:00:16 -0500

    reverting sac to be fast

--- src/rl/zoo/l2f/sac.h
+++ src/rl/zoo/l2f/sac.h
@@ -11,45 +11,43 @@ namespace rl_tools::rl::zoo::l2f::sac{
         using ENVIRONMENT = typename ENVIRONMENT_FACTORY<DEVICE, T, TI>::ENVIRONMENT;
         struct LOOP_CORE_PARAMETERS: rlt::rl::algorithms::sac::loop::core::DefaultParameters<T, TI, ENVIRONMENT>{
             struct SAC_PARAMETERS: rlt::rl::algorithms::sac::DefaultParameters<T, TI>{
-                static constexpr TI ACTOR_BATCH_SIZE = 256;
-                static constexpr TI CRITIC_BATCH_SIZE = 256;
-                static constexpr TI TRAINING_INTERVAL = 16;
+                static constexpr TI ACTOR_BATCH_SIZE = 64;
+                static constexpr TI CRITIC_BATCH_SIZE = 64;
+                static constexpr TI TRAINING_INTERVAL = 5;
                 static constexpr TI CRITIC_TRAINING_INTERVAL = 1 * TRAINING_INTERVAL;
                 static constexpr TI ACTOR_TRAINING_INTERVAL = 2 * TRAINING_INTERVAL;
                 static constexpr TI CRITIC_TARGET_UPDATE_INTERVAL = 1 * TRAINING_INTERVAL;
+                static constexpr T TARGET_NEXT_ACTION_NOISE_CLIP = 0.9;
+                static constexpr T TARGET_NEXT_ACTION_NOISE_STD = 0.3;
                 static constexpr T GAMMA = 0.99;
                 static constexpr bool IGNORE_TERMINATION = false;
                 static constexpr T TARGET_ENTROPY = -((T)4);
                 static constexpr TI SEQUENCE_LENGTH = 1;
-                static constexpr bool ENTROPY_BONUS_NEXT_STEP = true;
             };
-            static constexpr TI STEP_LIMIT = 20000000;
+            static constexpr TI STEP_LIMIT = 200000;
             static constexpr TI REPLAY_BUFFER_CAP = STEP_LIMIT;
             static constexpr TI ACTOR_NUM_LAYERS = 3;
-            static constexpr TI ACTOR_HIDDEN_DIM = 256;
-            static constexpr auto ACTOR_ACTIVATION_FUNCTION = rlt::nn::activation_functions::ActivationFunction::RELU;
+            static constexpr TI ACTOR_HIDDEN_DIM = 32;
+            static constexpr auto ACTOR_ACTIVATION_FUNCTION = rlt::nn::activation_functions::ActivationFunction::FAST_TANH;
             static constexpr TI CRITIC_NUM_LAYERS = 3;
-            static constexpr TI CRITIC_HIDDEN_DIM = 256;
-            static constexpr auto CRITIC_ACTIVATION_FUNCTION = rlt::nn::activation_functions::ActivationFunction::RELU;
+            static constexpr TI CRITIC_HIDDEN_DIM = 32;
+            static constexpr auto CRITIC_ACTIVATION_FUNCTION = rlt::nn::activation_functions::ActivationFunction::FAST_TANH;
             static constexpr TI EPISODE_STEP_LIMIT = 500;
         //            static constexpr bool SHARED_BATCH = false;
-            static constexpr TI N_WARMUP_STEPS = 10000; // Exploration executed with a uniform random policy for N_WARMUP_STEPS steps
-            static constexpr TI N_WARMUP_STEPS_CRITIC = 10000; // Number of steps before critic training starts
-            static constexpr TI N_WARMUP_STEPS_ACTOR = 10000; // Number of steps before actor training starts
             struct OPTIMIZER_PARAMETERS_COMMON: rlt::nn::optimizers::adam::DEFAULT_PARAMETERS_TENSORFLOW<T>{
-                static constexpr bool ENABLE_GRADIENT_CLIPPING = false;
+                static constexpr bool ENABLE_GRADIENT_CLIPPING = true;
                 static constexpr T GRADIENT_CLIP_VALUE = 1;
-                static constexpr bool ENABLE_WEIGHT_DECAY = false;
+                static constexpr bool ENABLE_WEIGHT_DECAY = true;
                 static constexpr T WEIGHT_DECAY = 0.0001;
             };
             struct ACTOR_OPTIMIZER_PARAMETERS: OPTIMIZER_PARAMETERS_COMMON{
-                static constexpr T ALPHA = 3e-4;
+                static constexpr T ALPHA = 1e-3;
             };
             struct CRITIC_OPTIMIZER_PARAMETERS: OPTIMIZER_PARAMETERS_COMMON{
-                static constexpr T ALPHA = 3e-4;
+                static constexpr T ALPHA = 1e-3;
             };
             struct ALPHA_OPTIMIZER_PARAMETERS: OPTIMIZER_PARAMETERS_COMMON{
-                static constexpr T ALPHA = 3e-4;
+                static constexpr T ALPHA = 1e-3;
             };
             static constexpr bool SAMPLE_ENVIRONMENT_PARAMETERS = true;
         };
