18c7423712e35b6f85dfd9217da05d09ceeb5a1d
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-11-26 13:24:17 -0500

    cherry picking out of 20 seeds gives a very good policy

--- src/rl/zoo/l2f/plot_learning_curves.py
+++ src/rl/zoo/l2f/plot_learning_curves.py
@@ -3,7 +3,7 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 
-def load_experiment(experiment, time):
+def load_experiment(experiment, time, lower_percentile=0, upper_percentile=100):
     steps = []
     steps_set = False
     returns = []
@@ -24,28 +24,70 @@ def load_experiment(experiment, time):
     returns = returns.reshape((len(steps), -1))
     steps = np.array(steps)
 
-    mean_returns = np.mean(returns, axis=1)
-    std_returns = np.std(returns, axis=1)
+    # mean_returns = np.mean(returns, axis=1)
+    # std_returns = np.std(returns, axis=1)
     wall_time = steps/(steps.max() / time)
-    return steps, wall_time, mean_returns, std_returns
+    upper_percentile_values = np.percentile(returns, upper_percentile, axis=1)
+    lower_percentile_values = np.percentile(returns, lower_percentile, axis=1)
+    iqm_mask = np.logical_and(returns > lower_percentile_values[:, None], returns < upper_percentile_values[:, None])
+    iqm_data = np.where(iqm_mask, returns, np.nan)
+    iqm_mean_returns = np.nanmean(iqm_data, axis=1)
+    iqm_std_returns = np.nanstd(iqm_data, axis=1)
+    return steps, wall_time, iqm_mean_returns, iqm_std_returns
 
 
 experiments = {
-    "baseline": ("experiments/2024-11-26_11-51-39/f06eafb_zoo_environment_algorithm/l2f_sac", 1.38),
-    "challenger": ("experiments/2024-11-26_12-54-50/f06eafb_zoo_environment_algorithm/l2f_sac", 1.143),
+    "baseline": ("experiments/2024-11-26_12-54-50/f06eafb_zoo_environment_algorithm/l2f_sac", 1.143),
+    "challenger": ("experiments/2024-11-26_12-57-49/f06eafb_zoo_environment_algorithm/l2f_sac", 1.144),
+    "challenger2": ("experiments/2024-11-26_13-01-03/f06eafb_zoo_environment_algorithm/l2f_sac", 1.144),
 }
 
 
+combinations = [
+    (True, (0, 100)),
+    (True, (25, 75)),
+    (True, (90, 100)),
+    (False, (0, 100)),
+    (False, (25, 75)),
+    (False, (90, 100)),
+]
 
-use_wall_time = True
-plt.figure()
-for experiment_name, experiment in experiments.items():
-    steps, wall_time, mean_returns, std_returns = load_experiment(*experiment)
-    x = wall_time if use_wall_time else steps
-    plt.plot(x, mean_returns, label=experiment_name)
-    plt.fill_between(x, mean_returns - std_returns, mean_returns + std_returns, alpha=0.2)
-plt.xlabel("Time [s]") if use_wall_time else plt.xlabel("Step")
-plt.ylabel("Returns")
-plt.title("Learning curve")
-plt.legend()
+cols = 3
+fig, axes = plt.subplots(2, cols, figsize=(12, 10))
+
+for idx, (use_wall_time, (iqm_lower_percentile, iqm_upper_percentile)) in enumerate(combinations):
+    ax = axes[idx // cols, idx % cols]
+    for experiment_name, experiment in experiments.items():
+        steps, wall_time, mean_returns, std_returns = load_experiment(*experiment, lower_percentile=iqm_lower_percentile, upper_percentile=iqm_upper_percentile)
+        x = wall_time if use_wall_time else steps
+        ax.plot(x, mean_returns, label=experiment_name)
+        ax.fill_between(x, mean_returns - std_returns, mean_returns + std_returns, alpha=0.2)
+    
+    xlabel = "Time [s]" if use_wall_time else "Step"
+    ylabel = "Returns"
+    title = f"Learning Curve ({'Wall Time' if use_wall_time else 'Steps'}, {f'IQM({iqm_lower_percentile}%:{iqm_upper_percentile}%)'})"
+    
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+    ax.legend()
+
+# Adjust layout for better spacing
+plt.tight_layout()
 plt.show()
+
+# use_wall_time = True
+# use_iqm = True
+# plt.figure()
+# for experiment_name, experiment in experiments.items():
+#     steps, wall_time, mean_returns, std_returns, iqm_mean_returns, iqm_std_returns = load_experiment(*experiment)
+#     x = wall_time if use_wall_time else steps
+#     mean_y = iqm_mean_returns if use_iqm else mean_returns
+#     std_y = iqm_std_returns if use_iqm else std_returns
+#     plt.plot(x, mean_y, label=experiment_name)
+#     plt.fill_between(x, mean_y - std_y, mean_y + std_y, alpha=0.2)
+# plt.xlabel("Time [s]") if use_wall_time else plt.xlabel("Step")
+# plt.ylabel("Returns")
+# plt.title("Learning curve")
+# plt.legend()
+# plt.show()
