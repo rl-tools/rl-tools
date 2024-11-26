662c3cc67bf0db05b5897bb7ba078a6b0d23ffab
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-11-25 19:46:59 -0500

    td3 competitive

--- src/rl/zoo/l2f/td3.h
+++ src/rl/zoo/l2f/td3.h
@@ -13,27 +13,36 @@ namespace rl_tools::rl::zoo::l2f::td3{
             struct TD3_PARAMETERS: rlt::rl::algorithms::td3::DefaultParameters<T, TI>{
                 static constexpr TI ACTOR_BATCH_SIZE = 256;
                 static constexpr TI CRITIC_BATCH_SIZE = 256;
-                static constexpr TI TRAINING_INTERVAL = 10;
+                static constexpr TI TRAINING_INTERVAL = 16;
                 static constexpr TI CRITIC_TRAINING_INTERVAL = 1 * TRAINING_INTERVAL;
                 static constexpr TI ACTOR_TRAINING_INTERVAL = 2 * TRAINING_INTERVAL;
                 static constexpr TI CRITIC_TARGET_UPDATE_INTERVAL = 1 * TRAINING_INTERVAL;
                 static constexpr TI ACTOR_TARGET_UPDATE_INTERVAL = 2 * TRAINING_INTERVAL;
-                static constexpr T TARGET_NEXT_ACTION_NOISE_CLIP = 0.9;
-                static constexpr T TARGET_NEXT_ACTION_NOISE_STD = 0.3;
+                static constexpr T TARGET_NEXT_ACTION_NOISE_CLIP = 0.5;
+                static constexpr T TARGET_NEXT_ACTION_NOISE_STD = 0.2;
                 static constexpr T GAMMA = 0.99;
                 static constexpr bool IGNORE_TERMINATION = false;
             };
             static constexpr T EXPLORATION_NOISE = 0.3;
-            static constexpr TI STEP_LIMIT = 3000000;
+            static constexpr TI STEP_LIMIT = 4000000;
             static constexpr TI REPLAY_BUFFER_CAP = STEP_LIMIT;
             static constexpr TI ACTOR_NUM_LAYERS = 3;
-            static constexpr TI ACTOR_HIDDEN_DIM = 64;
+            static constexpr TI ACTOR_HIDDEN_DIM = 192;
             static constexpr auto ACTOR_ACTIVATION_FUNCTION = nn::activation_functions::ActivationFunction::FAST_TANH;
             static constexpr TI CRITIC_NUM_LAYERS = 3;
-            static constexpr TI CRITIC_HIDDEN_DIM = 64;
+            static constexpr TI CRITIC_HIDDEN_DIM = 192;
             static constexpr auto CRITIC_ACTIVATION_FUNCTION = nn::activation_functions::ActivationFunction::FAST_TANH;
             static constexpr TI EPISODE_STEP_LIMIT = 500;
+            static constexpr TI N_WARMUP_STEPS = 1000;
 //            static constexpr bool SHARED_BATCH = false;
+            struct OPTIMIZER_PARAMETERS: rlt::nn::optimizers::adam::DEFAULT_PARAMETERS_TENSORFLOW<T>{
+                static constexpr T ALPHA = 3e-4;
+                static constexpr bool ENABLE_GRADIENT_CLIPPING = false;
+                static constexpr T GRADIENT_CLIP_VALUE = 1;
+                static constexpr bool ENABLE_WEIGHT_DECAY = false;
+                static constexpr T WEIGHT_DECAY = 0.0001;
+            };
+
         };
         using LOOP_CORE_CONFIG = rlt::rl::algorithms::td3::loop::core::Config<T, TI, RNG, ENVIRONMENT, LOOP_CORE_PARAMETERS>;
     };
