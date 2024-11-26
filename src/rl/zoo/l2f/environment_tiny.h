fce7307838894ce0fc1bee280e78249781abf154
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-11-26 16:09:59 -0500

    history length = 2 works on drone

--- src/rl/zoo/l2f/environment_tiny.h
+++ src/rl/zoo/l2f/environment_tiny.h
@@ -47,7 +47,7 @@ namespace rl_tools::rl::zoo::l2f{
         };
 
         struct ENVIRONMENT_STATIC_PARAMETERS{
-            static constexpr TI ACTION_HISTORY_LENGTH = 1;
+            static constexpr TI ACTION_HISTORY_LENGTH = 2;
             static constexpr TI CLOSED_FORM = false;
             using STATE_BASE = StateBase<T, TI>;
             using STATE_TYPE = StateRotorsHistory<T, TI, ACTION_HISTORY_LENGTH, CLOSED_FORM, StateRandomForce<T, TI, STATE_BASE>>;
