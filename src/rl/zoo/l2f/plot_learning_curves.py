955b9765f3535954230faf24079442f7c21352a4
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-11-26 11:22:36 -0500

    adding learning curve plotting script

---
+++ src/rl/zoo/l2f/plot_learning_curves.py
@@ -0,0 +1,48 @@
+import os
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+def load_experiment(experiment):
+    steps = []
+    steps_set = False
+    returns = []
+
+    for seed in os.listdir(experiment):
+        run_path = os.path.join(experiment, seed)
+        if os.path.exists(os.path.join(run_path, "return.json.set")):
+            with open(os.path.join(run_path, "return.json")) as f:
+                results = json.load(f)
+                for i, evaluation_step in enumerate(results):
+                    if not steps_set:
+                        steps.append(evaluation_step["step"])
+                        returns.append([])
+                    returns[i].append(evaluation_step["returns"])
+            steps_set = True
+
+    returns = np.array(returns)
+    returns = returns.reshape((len(steps), -1))
+    steps = np.array(steps)
+
+    mean_returns = np.mean(returns, axis=1)
+    std_returns = np.std(returns, axis=1)
+    return steps, mean_returns, std_returns
+
+
+experiments = {
+    "baseline": "experiments/2024-11-26_11-06-39/dd10193_zoo_environment_algorithm/l2f_sac",
+    "challenger": "experiments/2024-11-26_11-04-29/dd10193_zoo_environment_algorithm/l2f_sac"
+}
+
+
+
+plt.figure()
+for experiment_name, experiment in experiments.items():
+    steps, mean_returns, std_returns = load_experiment(experiment)
+    plt.plot(steps, mean_returns, label=experiment_name)
+    plt.fill_between(steps, mean_returns - std_returns, mean_returns + std_returns, alpha=0.2)
+plt.xlabel("Steps")
+plt.ylabel("Returns")
+plt.title("Learning curve")
+plt.legend()
+plt.show()
