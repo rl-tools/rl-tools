5ee46e8cb11adf36c4c969344ca007e7dab1a9fa
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-11-27 16:12:10 -0500

    larger velocity penality leads to higher returns 🤯

--- tools/index_experiments.sh
+++ tools/index_experiments.sh
@@ -8,6 +8,6 @@ if [ -z "$1" ]; then
 fi
 
 cd $1
-find . -type f -not -path '\.\/\.git*' | grep -v index.txt\$ | grep -v index_files.txt\$ | grep -v index_directories.txt\$ | grep -v index_static.txt\$ | grep -v index_static.txt.tmp\$ | grep -v .gitignore\$ | sort > index.txt.tmp
+find . -type f -not -path '\.\/\.git*' | grep -v index.txt\$ | grep -v \.DS_Store\$ |grep -v index_files.txt\$ | grep -v index_directories.txt\$ | grep -v index_static.txt\$ | grep -v index_static.txt.tmp\$ | grep -v .gitignore\$ | sort > index.txt.tmp
 mv index.txt.tmp index.txt
 echo "Wrote $(pwd)/index.txt"
