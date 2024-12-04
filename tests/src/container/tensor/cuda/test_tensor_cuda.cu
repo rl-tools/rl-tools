f6fdce64e3ad0ece77782488bc13a69ed2a8d81f
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-12-04 11:03:25 -0500

    tensor cuda test passing

--- tests/src/container/tensor/cuda/test_tensor_cuda.cu
+++ tests/src/container/tensor/cuda/test_tensor_cuda.cu
@@ -9,16 +9,51 @@ namespace rlt = rl_tools;
 using DEVICE = rlt::devices::DEVICE_FACTORY_CUDA<>;
 using DEVICE_CPU = rlt::devices::DEVICE_FACTORY<>;
 
+
+template <typename T, typename TI, typename SHAPE, typename DEVICE, typename DEVICE_CPU>
+void test(DEVICE& device, DEVICE_CPU& device_cpu, T epsilon) {
+    rlt::Tensor<rlt::tensor::Specification<T, TI, SHAPE, true>> tensor, tensor_cpu, target;
+    rlt::malloc(device, tensor);
+    rlt::malloc(device_cpu, tensor_cpu);
+    rlt::malloc(device_cpu, target);
+    {
+        rlt::set_all(device, tensor, 1337);
+        rlt::set_all(device_cpu, target, 1337);
+        rlt::copy(device, device_cpu, tensor, tensor_cpu);
+        T diff = rlt::abs_diff(device_cpu, tensor_cpu, target);
+        std::cout << "set_all diff: " << diff << std::endl;
+        ASSERT_EQ(diff, 0);
+    }
+    {
+        rlt::set_all(device, tensor, 1.3);
+        rlt::set_all(device_cpu, target, 1.3);
+        rlt::exp(device, tensor);
+        rlt::exp(device_cpu, target);
+        rlt::copy(device, device_cpu, tensor, tensor_cpu);
+        T diff = rlt::abs_diff(device_cpu, tensor_cpu, target);
+        if constexpr(SHAPE::LENGTH == 1 && SHAPE::template GET<0> < 100) {
+            rlt::print(device_cpu, target);
+        }
+        std::cout << "exp diff: " << diff << std::endl;
+        ASSERT_LT(diff, epsilon);
+    }
+}
+
 TEST(RL_TOOLS_CONTAINER_TENSOR_CUDA, MAIN){
     DEVICE device;
     DEVICE_CPU device_cpu;
     rlt::init(device);
     using T = float;
     using TI = typename DEVICE::index_t;
-    rlt::Tensor<rlt::tensor::Specification<T, TI, rlt::tensor::Shape<TI, 10>, true>> tensor, tensor_cpu;
-    rlt::malloc(device, tensor);
-    rlt::malloc(device_cpu, tensor_cpu);
-    rlt::set_all(device, tensor, 1337);
-    rlt::copy(device, device_cpu, tensor, tensor_cpu);
-    rlt::print(device_cpu, tensor_cpu);
+    T epsilon = 1e-6;
+    test<T, TI, rlt::tensor::Shape<TI, 1>>(device, device_cpu, epsilon);
+    test<T, TI, rlt::tensor::Shape<TI, 2>>(device, device_cpu, epsilon);
+    test<T, TI, rlt::tensor::Shape<TI, 10>>(device, device_cpu, epsilon);
+    test<T, TI, rlt::tensor::Shape<TI, 10, 1>>(device, device_cpu, epsilon);
+    test<T, TI, rlt::tensor::Shape<TI, 1, 10>>(device, device_cpu, epsilon);
+    test<T, TI, rlt::tensor::Shape<TI, 10, 10>>(device, device_cpu, epsilon);
+    test<T, TI, rlt::tensor::Shape<TI, 10, 10, 1>>(device, device_cpu, epsilon);
+    test<T, TI, rlt::tensor::Shape<TI, 10, 1, 10>>(device, device_cpu, epsilon);
+    test<T, TI, rlt::tensor::Shape<TI, 1, 10, 10>>(device, device_cpu, epsilon);
+    test<T, TI, rlt::tensor::Shape<TI, 10, 10, 10>>(device, device_cpu, epsilon);
 }
