a5cfd7c3a78053ce6ac37453aff041bb9ce0aa80
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-12-04 18:13:30 -0500

    stand-alone CUDA pendulum SAC is clean

--- include/rl_tools/rl/components/off_policy_runner/operations_generic_per_env.h
+++ include/rl_tools/rl/components/off_policy_runner/operations_generic_per_env.h
@@ -18,11 +18,11 @@ namespace rl_tools::rl::components::off_policy_runner{
         static_assert(!SPEC::PARAMETERS::COLLECT_EPISODE_STATS || SPEC::PARAMETERS::EPISODE_STATS_BUFFER_SIZE > 1);
         if (get(runner.truncated, 0, env_i)){
             T episode_return = get(runner.episode_return, 0, env_i);
-#ifdef __CUDA_ARCH__
-            printf("GPU: Episode return: %f\n", episode_return);
-#else
-            std::cout << "CPU: Episode return: " << episode_return << std::endl;
-#endif
+// #ifdef __CUDA_ARCH__
+//             printf("GPU: Episode return: %f\n", episode_return);
+// #else
+//             std::cout << "CPU: Episode return: " << episode_return << std::endl;
+// #endif
             if constexpr(SPEC::PARAMETERS::COLLECT_EPISODE_STATS){
                 // todo: the first episode is always zero steps and zero return because the initialization is done by setting truncated to true
                 auto& episode_stats = get(runner.episode_stats, 0, env_i);
