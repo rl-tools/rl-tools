efea9fd1a0b45429968559517acfae14c2d3a449
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-12-20 12:41:06 +0100

    malloc counter

--- tools/run.sh
+++ tools/run.sh
@@ -4,6 +4,6 @@ set -e
 export MKL_NUM_THREADS=1
 export RL_TOOLS_EXTRACK_EXPERIMENT=$(date '+%Y-%m-%d_%H-%M-%S')
 echo "RL_TOOLS_EXTRACK_EXPERIMENT=$RL_TOOLS_EXTRACK_EXPERIMENT"
-$1 -s 0 --extrack-experiment $RL_TOOLS_EXTRACK_EXPERIMENT
-seq 1 $2 | xargs -n 1 -P 4 -I {} $1 -s {} --extrack-experiment $RL_TOOLS_EXTRACK_EXPERIMENT
+timeout 10s $1 -s 0 --extrack-experiment $RL_TOOLS_EXTRACK_EXPERIMENT || true
+seq 0 $2 | xargs -n 1 -P 16 -I {} $1 -s {} --extrack-experiment $RL_TOOLS_EXTRACK_EXPERIMENT
 
