d176cd62a791a33bff16669222a728712e3d302b
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-01-08 10:44:04 +0400

    adding description, cleaning up flag-memory

--- src/rl/zoo/flag/sac.h
+++ src/rl/zoo/flag/sac.h
@@ -10,28 +10,46 @@ namespace rl_tools::rl::zoo::flag::sac{
         using ENVIRONMENT = typename ENVIRONMENT_FACTORY<DEVICE, T, TI>::ENVIRONMENT;
         struct LOOP_CORE_PARAMETERS: rlt::rl::algorithms::sac::loop::core::DefaultParameters<T, TI, ENVIRONMENT>{
             struct SAC_PARAMETERS: rl::algorithms::sac::DefaultParameters<T, TI, ENVIRONMENT::ACTION_DIM>{
-                static constexpr TI ACTOR_BATCH_SIZE = 256;
-                static constexpr TI CRITIC_BATCH_SIZE = 256;
-                static constexpr T GAMMA = 0.995;
-                static constexpr TI CRITIC_TRAINING_INTERVAL = 10;
-                static constexpr TI ACTOR_TRAINING_INTERVAL = 20;
-                static constexpr TI CRITIC_TARGET_UPDATE_INTERVAL = 20;
+                static constexpr TI ACTOR_BATCH_SIZE = 64;
+                static constexpr TI CRITIC_BATCH_SIZE = 64;
+                static constexpr T GAMMA = 0.9;
+                static constexpr TI CRITIC_TRAINING_INTERVAL = 1;
+                static constexpr TI ACTOR_TRAINING_INTERVAL = 2;
+                static constexpr TI CRITIC_TARGET_UPDATE_INTERVAL = 2;
+                static constexpr TI SEQUENCE_LENGTH = ENVIRONMENT::EPISODE_STEP_LIMIT;
+                static constexpr bool ENTROPY_BONUS = true;
+                static constexpr bool ENTROPY_BONUS_NEXT_STEP = false;
+                static constexpr T TARGET_ENTROPY = -2;
+                static constexpr T ALPHA = 1;
+                static constexpr bool ADAPTIVE_ALPHA = true;
+                static constexpr bool INCLUDE_FIRST_STEP_IN_TARGETS = true;
             };
             static constexpr TI STEP_LIMIT = 200000;
             static constexpr TI N_ENVIRONMENTS = 32;
-            static constexpr TI REPLAY_BUFFER_CAP = STEP_LIMIT * N_ENVIRONMENTS;
+            static constexpr TI REPLAY_BUFFER_CAP = STEP_LIMIT;
             static constexpr TI N_WARMUP_STEPS = 100;
             static constexpr TI N_WARMUP_STEPS_CRITIC = 100;
             static constexpr TI N_WARMUP_STEPS_ACTOR = 100;
             static constexpr TI ACTOR_NUM_LAYERS = 3;
-            static constexpr TI ACTOR_HIDDEN_DIM = 16;
+            static constexpr TI ACTOR_HIDDEN_DIM = 32;
             static constexpr TI CRITIC_NUM_LAYERS = 3;
-            static constexpr TI CRITIC_HIDDEN_DIM = 16;
+            static constexpr TI CRITIC_HIDDEN_DIM = 32;
             static constexpr auto ACTOR_ACTIVATION_FUNCTION = nn::activation_functions::ActivationFunction::FAST_TANH;
             static constexpr auto CRITIC_ACTIVATION_FUNCTION = nn::activation_functions::ActivationFunction::FAST_TANH;
             static constexpr T ALPHA = 1.0;
+            static constexpr bool ALWAYS_SAMPLE_FROM_INITIAL_STATE = true;
+
+            struct ACTOR_OPTIMIZER_PARAMETERS: rlt::nn::optimizers::adam::DEFAULT_PARAMETERS_TENSORFLOW<T>{
+                static constexpr T ALPHA = 1e-4;
+            };
+            struct CRITIC_OPTIMIZER_PARAMETERS: rlt::nn::optimizers::adam::DEFAULT_PARAMETERS_TENSORFLOW<T>{
+                static constexpr T ALPHA = 1e-3;
+            };
+            struct ALPHA_OPTIMIZER_PARAMETERS: rlt::nn::optimizers::adam::DEFAULT_PARAMETERS_TENSORFLOW<T>{
+                static constexpr T ALPHA = 1e-3;
+            };
         };
-        using LOOP_CORE_CONFIG = rlt::rl::algorithms::sac::loop::core::Config<T, TI, RNG, ENVIRONMENT, LOOP_CORE_PARAMETERS, rlt::rl::algorithms::sac::loop::core::ConfigApproximatorsMLP>;
+        using LOOP_CORE_CONFIG = rlt::rl::algorithms::sac::loop::core::Config<T, TI, RNG, ENVIRONMENT, LOOP_CORE_PARAMETERS, rlt::rl::algorithms::sac::loop::core::ConfigApproximatorsGRU>;
     };
 }
 RL_TOOLS_NAMESPACE_WRAPPER_END
