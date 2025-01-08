3312e80c5b5c47f1abdcd67874161b816abb6d40
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-01-08 11:30:52 +0400

    adding measurement

--- src/rl/environments/pendulum/sac/cpu/training.cpp
+++ src/rl/environments/pendulum/sac/cpu/training.cpp
@@ -4,3 +4,4 @@ int main(){
 }
 
 // Should take ~ 1.6s on M3 Pro in BECHMARK mode (tested @ 1118e19f904a26a9619fac7b1680643a0afcb695)
+// Should take ~ 1.68s on M3 Pro in BECHMARK mode (tested @ 3ad4292ccab4e66f6c5487e19ef82de2fa88eacb)
