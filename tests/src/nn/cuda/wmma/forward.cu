4d1806699d42febd2417d16efb4a7042df45baa8
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-01-13 19:10:29 +0400

    fused kernel only 30x slower than mkl

--- tests/src/nn/cuda/wmma/forward.cu
+++ tests/src/nn/cuda/wmma/forward.cu
@@ -1,7 +1,7 @@
 
 #define RL_TOOLS_FUNCTION_PLACEMENT __device__ __host__
 #define RL_TOOLS_DEVICES_DISABLE_REDEFINITION_DETECTION
-#include <rl_tools/operations/cpu.h>
+#include <rl_tools/operations/cpu_mux.h>
 #include <rl_tools/operations/cuda.h>
 #include <rl_tools/nn/layers/operations_cuda.h>
 #include <rl_tools/nn_models/mlp/operations_generic.h>
@@ -48,6 +48,7 @@ __global__ void wmma_kernel(__nv_bfloat16* global_a, __nv_bfloat16* global_b, fl
 }
 using DEVICE_CUDA = rlt::devices::DefaultCUDA;
 using DEVICE_CPU = rlt::devices::DefaultCPU;
+using DEVICE_BLAS = rlt::devices::DEVICE_FACTORY<>;
 using T = float;
 using TI = DEVICE_CUDA::index_t;
 constexpr bool DYNAMIC_ALLOCATION = true;
@@ -58,10 +59,10 @@ using RNG_CUDA = DEVICE_CUDA::SPEC::RANDOM::ENGINE<>;
 using RNG_CPU = DEVICE_CPU::SPEC::RANDOM::ENGINE<>;
 
 constexpr TI SEQUENCE_LENGTH = 1;
-constexpr TI BATCH_SIZE = 16;
-constexpr TI INPUT_DIM = 16;
-constexpr TI OUTPUT_DIM = 16;
-constexpr TI HIDDEN_DIM = 16;
+constexpr TI BATCH_SIZE = 32;
+constexpr TI INPUT_DIM = 32;
+constexpr TI OUTPUT_DIM = 32;
+constexpr TI HIDDEN_DIM = 32;
 constexpr TI NUM_LAYERS = 3;
 
 using CAPABILITY = rlt::nn::capability::Forward<true>;
@@ -91,13 +92,18 @@ struct NORNG{};
 template <typename INPUT, typename OUTPUT, typename BUFFER>
 __global__ void evaluate_fused(NETWORK nn, INPUT input, OUTPUT output, BUFFER buffer){
     __shared__ NETWORK_STATIC nn_shared;
+    __shared__ rlt::Tensor<rlt::tensor::Specification<T, TI, NETWORK::INPUT_SHAPE, false>> input_shared;
+    __shared__ rlt::Tensor<rlt::tensor::Specification<T, TI, NETWORK::OUTPUT_SHAPE, false>> output_shared;
     if(threadIdx.x == 0){
         CUDA_FUSED device;
         NORNG rng;
         rlt::copy(device, device, nn, nn_shared);
+        rlt::copy(device, device, input, input_shared);
+        rlt::copy(device, device, output, output_shared);
         for (TI i = 0; i < 1000; i++){
-            rlt::evaluate(device, nn_shared, input, output, buffer, rng);
+            rlt::evaluate(device, nn_shared, input_shared, output_shared, buffer, rng);
         }
+        rlt::copy(device, device, output_shared, output);
     }
 }
 
@@ -114,6 +120,8 @@ int main() {
     rlt::init(device_cpu);
     rlt::malloc(device_cpu, rng_cpu);
     rlt::init(device_cpu, rng_cpu, 0);
+
+    DEVICE_BLAS device_blas;
 
     NETWORK nn_cuda, nn_cpu;
     NETWORK::Buffer<> buffer_cuda, buffer_cpu;
@@ -150,10 +158,12 @@ int main() {
         fprintf(stderr, "ERROR: %s\n", cudaGetErrorString(error));
     }
     auto start_cpu = std::chrono::high_resolution_clock::now();
-    rlt::evaluate(device_cpu, nn_cpu, input_cpu, output_cpu, buffer_cpu, rng_cpu);
+    for (TI i=0; i < 1000; i++){
+        rlt::evaluate(device_blas, nn_cpu, input_cpu, output_cpu, buffer_cpu, rng_cpu);
+    }
     auto end_cpu = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed_seconds = end_cpu - start_cpu;
-    printf("Time: %f ms\n", elapsed_seconds.count() * 1000);
+    printf("Time: %f ms\n", elapsed_seconds.count() * 1000.0 / 1000.0);
 
 
     rlt::copy(device_cuda, device_cpu, output_cuda, output_cuda_cpu);
