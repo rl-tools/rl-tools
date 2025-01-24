ba058d5bcb92a93da9d8c1267b3b815b9840086f
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-01-24 16:08:56 -0500

    gru cuda nan; wip

--- tests/src/nn/layers/gru/cuda/gru_cuda.cu
+++ tests/src/nn/layers/gru/cuda/gru_cuda.cu
@@ -22,7 +22,8 @@ using DEVICE_CPU = rlt::devices::DefaultCPU;
 using DEVICE_GPU = rlt::devices::DefaultCUDA;
 using TI = typename DEVICE_GPU::index_t;
 
-constexpr TI SEQUENCE_LENGTH = 10;
+constexpr T EPSILON = 1e-10;
+constexpr TI SEQUENCE_LENGTH = 1;
 constexpr TI BATCH_SIZE = 3;
 constexpr TI INPUT_DIM = 4;
 constexpr TI HIDDEN_DIM = 5;
@@ -33,7 +34,7 @@ using CAPABILITY = rlt::nn::capability::Gradient<rlt::nn::parameters::Adam>;
 using INPUT_SHAPE = rlt::tensor::Shape<TI, SEQUENCE_LENGTH, BATCH_SIZE, INPUT_DIM>;
 using GRU = GRU_TEMPLATE::Layer<CAPABILITY, INPUT_SHAPE>;
 
-#define DEV
+// #define DEV
 
 #ifndef DEV
 TEST(RL_TOOLS_NN_LAYERS_GRU, GRU_HELPERS_CUBLAS_SGEMM){
@@ -227,7 +228,7 @@ TEST(RL_TOOLS_NN_LAYERS_GRU, GRU_CUDA){
     GRU gru_cpu, gru_gpu;
     GRU::Buffer<> gru_buffer_cpu, gru_buffer_gpu;
     rlt::Tensor<rlt::tensor::Specification<T, TI, GRU::INPUT_SHAPE>> input_cpu, input_gpu;
-    rlt::Tensor<rlt::tensor::Specification<T, TI, GRU::OUTPUT_SHAPE>> output_cpu, output_gpu;
+    rlt::Tensor<rlt::tensor::Specification<T, TI, GRU::OUTPUT_SHAPE>> output_gpu_cpu;
 
     rlt::init(device_cpu);
     rlt::init(device_gpu);
@@ -240,8 +241,7 @@ TEST(RL_TOOLS_NN_LAYERS_GRU, GRU_CUDA){
     rlt::malloc(device_gpu, gru_buffer_gpu);
     rlt::malloc(device_cpu, input_cpu);
     rlt::malloc(device_gpu, input_gpu);
-    rlt::malloc(device_cpu, output_cpu);
-    rlt::malloc(device_gpu, output_gpu);
+    rlt::malloc(device_cpu, output_gpu_cpu);
 
     rlt::init(device_cpu, rng_cpu, 0);
     rlt::init(device_gpu, rng_gpu, 0);
@@ -251,10 +251,15 @@ TEST(RL_TOOLS_NN_LAYERS_GRU, GRU_CUDA){
     rlt::randn(device_gpu, input_gpu, rng_gpu);
     rlt::copy(device_gpu, device_cpu, input_gpu, input_cpu);
 
-    // rlt::evaluate(device_cpu, gru_cpu, input_cpu, output_cpu, gru_buffer_cpu, rng_cpu);
-    rlt::evaluate(device_gpu, gru_gpu, input_gpu, output_gpu, gru_buffer_gpu, rng_gpu);
+    rlt::forward(device_cpu, gru_cpu, input_cpu, gru_buffer_cpu, rng_cpu);
+    rlt::forward(device_gpu, gru_gpu, input_gpu, gru_buffer_gpu, rng_gpu);
+    rlt::copy(device_gpu, device_cpu, gru_gpu.output, output_gpu_cpu);
 
-    rlt::print(device_cpu, output_cpu);
+    rlt::print(device_cpu, gru_cpu.output);
+    rlt::print(device_cpu, output_gpu_cpu);
+    T abs_diff = rlt::abs_diff(device_cpu, gru_cpu.output, output_gpu_cpu);
+    std::cout << "abs_diff: " << abs_diff << std::endl;
+    ASSERT_LT(abs_diff, EPSILON);
 }
 
 #endif
