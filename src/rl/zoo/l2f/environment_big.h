96748653601ca14ca17d11d23b3bd631fda159e5
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-01-28 14:01:49 -0500

    addendum; all tests passing [ubuntu]

--- src/rl/zoo/l2f/environment_big.h
+++ src/rl/zoo/l2f/environment_big.h
@@ -125,7 +125,7 @@ namespace rl_tools::rl::zoo::l2f{
 
         using ENVIRONMENT_SPEC = rl_tools::rl::environments::l2f::Specification<T, TI, ENVIRONMENT_STATIC_PARAMETERS>;
         using ENVIRONMENT = rl_tools::rl::environments::MultirotorMultiTask<ENVIRONMENT_SPEC>;
-        static_assert(rl::environments::PREVENT_DEFAULT(ENVIRONMENT{}));
+        static_assert(rl::environments::PREVENT_DEFAULT_GET_UI<ENVIRONMENT>::value);
     };
 }
 RL_TOOLS_NAMESPACE_WRAPPER_END
