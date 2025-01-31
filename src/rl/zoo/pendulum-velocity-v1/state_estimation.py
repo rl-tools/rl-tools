237182fbf6b7827a3f31f538f346aaca716d5f70
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-01-31 12:43:07 -0500

    fixing replay buffer handling of warmup => actual actor switch

--- src/rl/zoo/pendulum-velocity-v1/state_estimation.py
+++ src/rl/zoo/pendulum-velocity-v1/state_estimation.py
@@ -2,10 +2,15 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 
+def load(filename):
+    with h5py.File(filename, 'r') as f:
+        print(list(f["0"].keys()))
+        return {k:f["0"][k][:] for k in ['actions', 'full', 'next_observations', 'observations', 'position', 'rewards', 'terminated', 'truncated', 'episode_start']}
 
-with h5py.File('replay_buffer.h5', 'r') as f:
-    print(list(f["0"].keys()))
-    actions, full, next_observations, observations, position, rewards, terminated, truncated, episode_start = [f["0"][k][:] for k in ['actions', 'full', 'next_observations', 'observations', 'position', 'rewards', 'terminated', 'truncated', 'episode_start']]
+old = load("replay_buffer_old.h5")
+new = load("replay_buffer.h5")
 
+assert (old["observations"] == new["observations"]).all()
+assert (old["episode_start"] == new["episode_start"]).all()
 
-observations_normalized = observations / observations.std(axis=0)
+print("end")
