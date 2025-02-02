e41d3122744dbae9bb067beb8961844bb9938898
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-02-02 12:38:56 -0500

    importing numeric type policy directly

--- include/rl_tools/operations/wasm32/group_1.h
+++ include/rl_tools/operations/wasm32/group_1.h
@@ -5,7 +5,7 @@
 #if defined(RL_TOOLS_DISABLE_INCLUDE_GUARDS) || !defined(RL_TOOLS_OPERATIONS_WASM32_GROUP_1)
     #define RL_TOOLS_OPERATIONS_WASM32_GROUP_1
     #include "../../devices/wasm32.h"
-    #include "../../utils/generic/typing.h"
+    #include "../../numeric_types/policy.h"
     #include "../../math/operations_wasm32.h"
     #include "../../logging/operations_wasm32.h"
 #else
