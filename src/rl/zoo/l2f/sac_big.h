93401619cfac18551a031e395ad4174ab066a221
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-02-07 16:47:45 -0500

    td3 very good

--- src/rl/zoo/l2f/sac_big.h
+++ src/rl/zoo/l2f/sac_big.h
@@ -16,7 +16,7 @@ namespace rl_tools::rl::zoo::l2f::sac{
             struct SAC_PARAMETERS: algorithms::sac::DefaultParameters<T, TI>{
                 static constexpr TI ACTOR_BATCH_SIZE = SEQUENTIAL_MODEL ? 64 : 256;
                 static constexpr TI CRITIC_BATCH_SIZE = SEQUENTIAL_MODEL ? 64 : 256;
-                static constexpr TI TRAINING_INTERVAL = SEQUENTIAL_MODEL ? 1 : 16;
+                static constexpr TI TRAINING_INTERVAL = SEQUENTIAL_MODEL ? 1 : 1;
                 static constexpr TI CRITIC_TRAINING_INTERVAL = 1 * TRAINING_INTERVAL;
                 static constexpr TI ACTOR_TRAINING_INTERVAL = 2 * TRAINING_INTERVAL;
                 static constexpr TI CRITIC_TARGET_UPDATE_INTERVAL = 1 * TRAINING_INTERVAL;
@@ -24,10 +24,10 @@ namespace rl_tools::rl::zoo::l2f::sac{
                 static constexpr bool IGNORE_TERMINATION = false;
                 static constexpr T TARGET_ENTROPY = -((T)4);
                 static constexpr TI SEQUENCE_LENGTH = SEQUENTIAL_MODEL ? 16 : 1;
-                static constexpr bool ENTROPY_BONUS_NEXT_STEP = true;
+                static constexpr bool ENTROPY_BONUS_NEXT_STEP = false;
             };
             static constexpr TI N_ENVIRONMENTS = SEQUENTIAL_MODEL ? SAC_PARAMETERS::SEQUENCE_LENGTH : 1;
-            static constexpr TI STEP_LIMIT = SEQUENTIAL_MODEL ? 2000000 : 2000000;
+            static constexpr TI STEP_LIMIT = SEQUENTIAL_MODEL ? 2000000 : 200000;
             static constexpr TI REPLAY_BUFFER_CAP = STEP_LIMIT;
             static constexpr TI ACTOR_NUM_LAYERS = SEQUENTIAL_MODEL ? 4 : 3;
             static constexpr TI ACTOR_HIDDEN_DIM = SEQUENTIAL_MODEL ? 32: 32;
@@ -37,7 +37,7 @@ namespace rl_tools::rl::zoo::l2f::sac{
             static constexpr auto CRITIC_ACTIVATION_FUNCTION = nn::activation_functions::ActivationFunction::FAST_TANH;
             static constexpr TI EPISODE_STEP_LIMIT = 500;
         //            static constexpr bool SHARED_BATCH = false;
-            static constexpr TI N_WARMUP_STEPS = 10000; // Exploration executed with a uniform random policy for N_WARMUP_STEPS steps
+            static constexpr TI N_WARMUP_STEPS = 0; // Exploration executed with a uniform random policy for N_WARMUP_STEPS steps
             static constexpr TI N_WARMUP_STEPS_CRITIC = 10000; // Number of steps before critic training starts
             static constexpr TI N_WARMUP_STEPS_ACTOR = 10000; // Number of steps before actor training starts
             struct OPTIMIZER_PARAMETERS_COMMON: nn::optimizers::adam::DEFAULT_PARAMETERS_TENSORFLOW<T>{
--- src/rl/zoo/l2f/td3.h
+++ src/rl/zoo/l2f/td3.h
@@ -8,7 +8,11 @@ namespace rl_tools::rl::zoo::l2f::td3{
     namespace rlt = rl_tools;
     template <typename DEVICE, typename T, typename TI, typename RNG, bool DYNAMIC_ALLOCATION>
     struct FACTORY{
-        using ENVIRONMENT = typename ENVIRONMENT_FACTORY<DEVICE, T, TI>::ENVIRONMENT;
+        static constexpr bool SEQUETIAL_MODEL = false;
+        static constexpr bool MOTOR_DELAY = false;
+        static constexpr bool THRASH_MARKOV = true;
+        static constexpr bool OBSERVE_THRASH_MARKOV = true;
+        using ENVIRONMENT = typename ENVIRONMENT_BIG_FACTORY<T, T, TI, SEQUETIAL_MODEL, MOTOR_DELAY, THRASH_MARKOV, OBSERVE_THRASH_MARKOV>::ENVIRONMENT;
         struct LOOP_CORE_PARAMETERS: rlt::rl::algorithms::td3::loop::core::DefaultParameters<T, TI, ENVIRONMENT>{
             struct TD3_PARAMETERS: rlt::rl::algorithms::td3::DefaultParameters<T, TI>{
                 static constexpr TI ACTOR_BATCH_SIZE = 256;
