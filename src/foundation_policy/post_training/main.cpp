0b1098cb33515d103ba8f8ebeac69a398b6cc9d6
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-02-13 12:06:11 -0500

    pre-training with many seeds => post training with particular seed works

--- src/foundation_policy/post_training/main.cpp
+++ src/foundation_policy/post_training/main.cpp
@@ -52,6 +52,8 @@ namespace rlt = rl_tools;
 
 using DEVICE = rlt::devices::DEVICE_FACTORY<>;
 using RNG = typename DEVICE::SPEC::RANDOM::ENGINE<>;
+using RNG_PARAMS_DEVICE = rlt::devices::random::Generic<DEVICE::SPEC::MATH>;
+using RNG_PARAMS = RNG_PARAMS_DEVICE::ENGINE<>;
 using TI = typename DEVICE::index_t;
 using T = float;
 constexpr bool DYNAMIC_ALLOCATION = true;
@@ -72,22 +74,55 @@ struct ADAM_PARAMETERS: rlt::nn::optimizers::adam::DEFAULT_PARAMETERS_TENSORFLOW
     static constexpr T ALPHA = 0.001;
 };
 
+
+
+// note: make sure that the rng_params is invoked in the exact same way in pre- as in post-training, to make sure the params used to sample parameters to generate data from the trained policy are matching the ones seen by the particular policy for the seed during pretraining
+
 int main(int argc, char** argv){
+    // std::vector<std::filesystem::path> checkpoint_paths;
+    // for (TI seed = 0; seed < 50; seed++){
+    //     std::stringstream ss;
+    //     ss << "experiments/2025-02-11_17-03-35/f98ad54_default_default/default/" << std::setfill('0') << std::setw(4) << seed << "/steps/000000002000000/checkpoint.h5";
+    //     checkpoint_paths.push_back(ss.str());
+    // }
+
     DEVICE device;
     RNG rng;
+    RNG_PARAMS rng_params;
     rlt::init(device);
+
+    TI seed = argc >= 2 ? std::stoi(argv[1]) : 0;
+
+    rlt::utils::extrack::Path checkpoint_path;
+    checkpoint_path.experiment = "2025-02-13_11-30-07";
+    checkpoint_path.name = "foundation-policy-pre-training";
+    checkpoint_path.seed = std::to_string(seed);
+    rlt::find_latest_run(device, "experiments", checkpoint_path);
+    if (!rlt::find_latest_checkpoint(device, checkpoint_path)){
+        std::cerr << "No checkpoint found for " << checkpoint_path.experiment << std::endl;
+        return 1;
+    }
+    rlt::malloc(device, rng_params);
+    rlt::init(device, rng_params, seed);
+    // warmup
+    for(TI i=0; i < (TI)std::stoi(checkpoint_path.attributes["rng-warmup"]); i++){
+        rlt::random::uniform_int_distribution(RNG_PARAMS_DEVICE{}, 0, 1, rng_params);
+    }
+
+
+    std::cout << "Found checkpoint: " << checkpoint_path.checkpoint_path << std::endl;
+
 #ifdef RL_TOOLS_ENABLE_TENSORBOARD
     auto timestamp_string = rlt::utils::extrack::get_timestamp_string();
     rlt::init(device, device.logger, "logs/" + timestamp_string);
 #endif
     rlt::malloc(device, rng);
-    TI seed = argc >= 2 ? std::stoi(argv[1]) : 0;
     rlt::init(device, rng, seed);
     typename LOOP_CORE_CONFIG_PRE_TRAINING::template State <LOOP_CORE_CONFIG_PRE_TRAINING> ts;
     rlt::malloc(device, ts);
     rlt::init(device, ts, seed);
     auto& base_env = rlt::get(ts.off_policy_runner.envs, 0, 0);
-    rlt::sample_initial_parameters<DEVICE, LOOP_CORE_CONFIG_PRE_TRAINING::ENVIRONMENT::SPEC, RNG, false>(device, base_env, base_env.parameters, rng);
+    rlt::sample_initial_parameters<DEVICE, LOOP_CORE_CONFIG_PRE_TRAINING::ENVIRONMENT::SPEC, RNG_PARAMS, true>(device, base_env, base_env.parameters, rng_params);
     for (TI env_i = 1; env_i < LOOP_CORE_CONFIG_PRE_TRAINING::CORE_PARAMETERS::N_ENVIRONMENTS; env_i++){
         auto& env = rlt::get(ts.off_policy_runner.envs, 0, env_i);
         env.parameters = base_env.parameters;
@@ -96,8 +131,8 @@ int main(int argc, char** argv){
     rlt::nn::optimizers::Adam<rlt::nn::optimizers::adam::Specification<T, TI, ADAM_PARAMETERS>> actor_optimizer;
     rlt::malloc(device, actor_optimizer);
 
-    std::filesystem::path checkpoint_path = "experiments/2025-02-11_17-03-35/f98ad54_default_default/default/0000/steps/000000002000000/checkpoint.h5";
-    auto actor_file = HighFive::File(checkpoint_path.string(), HighFive::File::ReadOnly);
+    // std::filesystem::path checkpoint_path = "experiments/2025-02-11_17-03-35/f98ad54_default_default/default/0000/steps/000000002000000/checkpoint.h5";
+    auto actor_file = HighFive::File(checkpoint_path.checkpoint_path.string(), HighFive::File::ReadOnly);
 
     constexpr TI NUM_EPISODES = 100;
     rlt::rl::utils::evaluation::Result<rlt::rl::utils::evaluation::Specification<T, TI, LOOP_CORE_CONFIG_PRE_TRAINING::ENVIRONMENT, NUM_EPISODES, LOOP_CORE_CONFIG_PRE_TRAINING::CORE_PARAMETERS::EPISODE_STEP_LIMIT>> result;
@@ -114,12 +149,13 @@ int main(int argc, char** argv){
     typename LOOP_CORE_CONFIG_PRE_TRAINING::ENVIRONMENT_EVALUATION env_eval;
     typename LOOP_CORE_CONFIG_PRE_TRAINING::ENVIRONMENT_EVALUATION::Parameters env_eval_parameters;
     rlt::init(device, env_eval);
+    env_eval.parameters = base_env.parameters;
     rlt::initial_parameters(device, env_eval, env_eval_parameters);
 
     rlt::init(device, rng, seed);
     rlt::Mode<rlt::mode::Evaluation<rlt::nn::layers::sample_and_squash::mode::DisableEntropy<rlt::mode::Final>>> evaluation_mode;
     rlt::evaluate(device, env_eval, env_eval_parameters, ui, evaluation_actor, result, *data, eval_buffer, rng, evaluation_mode, false, true);
-    rlt::log(device, device.logger, "Checkpoint ", checkpoint_path.string(), ": Mean return: ", result.returns_mean, " Mean episode length: ", result.episode_length_mean);
+    rlt::log(device, device.logger, "Checkpoint ", checkpoint_path.checkpoint_path.string(), ": Mean return: ", result.returns_mean, " Mean episode length: ", result.episode_length_mean);
 
     using INPUT_SHAPE = decltype(ts.actor_critic.actor)::INPUT_SHAPE;
     using OUTPUT_SHAPE = decltype(ts.actor_critic.actor)::OUTPUT_SHAPE;
@@ -216,7 +252,7 @@ int main(int argc, char** argv){
             rlt::add_scalar(device, device.logger, "evaluation/return/std", result.returns_std);
             rlt::add_scalar(device, device.logger, "evaluation/episode_length/mean", result.episode_length_mean);
             rlt::add_scalar(device, device.logger, "evaluation/episode_length/std", result.episode_length_std);
-            rlt::log(device, device.logger, "Checkpoint ", checkpoint_path.string(), ": Mean return: ", result.returns_mean, " Mean episode length: ", result.episode_length_mean);
+            rlt::log(device, device.logger, "Checkpoint ", checkpoint_path.checkpoint_path.string(), ": Mean return: ", result.returns_mean, " Mean episode length: ", result.episode_length_mean);
             rlt::free(device, evaluation_actor);
             rlt::free(device, eval_buffer);
         }
