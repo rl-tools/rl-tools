6427cabad35a409107ddddd3e9319f71e67272b5
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-02-14 15:49:45 -0500

    automated git diff for every target

--- src/foundation_policy/pre_training/options.h
+++ src/foundation_policy/pre_training/options.h
@@ -1,6 +1,6 @@
 struct OPTIONS_PRE_TRAINING{
     static constexpr bool SEQUENTIAL_MODEL = false;
-    static constexpr bool MOTOR_DELAY = true;
+    static constexpr bool MOTOR_DELAY = false;
     static constexpr bool RANDOMIZE_MOTOR_MAPPING = true;
     static constexpr bool RANDOMIZE_THRUST_CURVES = false;
     static constexpr bool OBSERVE_THRASH_MARKOV = false;
