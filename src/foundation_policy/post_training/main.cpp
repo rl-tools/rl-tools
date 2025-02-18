7f0c12ad3100e8b1cb48e04d17956f5dd1902dbf
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-02-18 09:54:05 -0500

    pre-training ident i believe

--- src/foundation_policy/post_training/main.cpp
+++ src/foundation_policy/post_training/main.cpp
@@ -48,8 +48,10 @@
 
 #include "../pre_training/config.h"
 #include "../pre_training/options.h"
-
 namespace rlt = rl_tools;
+
+#include "generate_data.h"
+
 
 using DEVICE = rlt::devices::DEVICE_FACTORY<>;
 using RNG = typename DEVICE::SPEC::RANDOM::ENGINE<>;
@@ -104,50 +106,6 @@ constexpr TI DATASET_SIZE = N_PRE_TRAINING_SEEDS * NUM_EPISODES * ENVIRONMENT::E
 // typedefs derived
 using INPUT_SHAPE_DATASET = rlt::tensor::Replace<INPUT_SHAPE, DATASET_SIZE, 1>;
 using OUTPUT_SHAPE_DATASET = rlt::tensor::Replace<OUTPUT_SHAPE, DATASET_SIZE, 1>;
-
-template <TI NUM_EPISODES, typename RESULT, typename DATA>
-auto generate_data(DEVICE& device, rlt::utils::extrack::Path checkpoint_path, typename DEVICE::index_t seed, RESULT& result, DATA& data){
-
-    RNG_PARAMS rng_params;
-    {
-        rlt::malloc(device, rng_params);
-        rlt::init(device, rng_params, seed);
-        // warmup
-        for(TI i=0; i < (TI)std::stoi(checkpoint_path.attributes["rng-warmup"]); i++){
-            rlt::random::uniform_int_distribution(RNG_PARAMS_DEVICE{}, 0, 1, rng_params);
-        }
-        rlt::free(device, rng_params);
-    }
-    auto actor_file = HighFive::File(checkpoint_path.checkpoint_path.string(), HighFive::File::ReadOnly);
-    ENVIRONMENT_PT base_env;
-    rlt::sample_initial_parameters<DEVICE, ENVIRONMENT_PT::SPEC, RNG_PARAMS, true>(device, base_env, base_env.parameters, rng_params);
-    using LOOP_CORE_CONFIG_PRE_TRAINING = builder::FACTORY<DEVICE, T, TI, RNG, OPTIONS_PRE_TRAINING, DYNAMIC_ALLOCATION>::LOOP_CORE_CONFIG;
-    using EVALUATION_ACTOR_TYPE_BATCH_SIZE = typename LOOP_CORE_CONFIG_PRE_TRAINING::NN::ACTOR_TYPE::template CHANGE_BATCH_SIZE<TI, NUM_EPISODES>;
-    using EVALUATION_ACTOR_TYPE = typename EVALUATION_ACTOR_TYPE_BATCH_SIZE::template CHANGE_CAPABILITY<rlt::nn::capability::Forward<LOOP_CORE_CONFIG_PRE_TRAINING::DYNAMIC_ALLOCATION>>;
-    rlt::rl::environments::DummyUI ui;
-    EVALUATION_ACTOR_TYPE evaluation_actor;
-    typename EVALUATION_ACTOR_TYPE::template Buffer<LOOP_CORE_CONFIG_PRE_TRAINING::DYNAMIC_ALLOCATION> eval_buffer;
-    rlt::malloc(device, evaluation_actor);
-    rlt::malloc(device, eval_buffer);
-    rlt::load(device, evaluation_actor, actor_file.getGroup("actor"));
-
-    ENVIRONMENT_PT env_eval;
-    ENVIRONMENT_PT::Parameters env_eval_parameters;
-    rlt::init(device, env_eval);
-    env_eval.parameters = base_env.parameters;
-    rlt::initial_parameters(device, env_eval, env_eval_parameters);
-
-    RNG rng;
-    rlt::malloc(device, rng);
-    rlt::init(device, rng, seed);
-    rlt::Mode<rlt::mode::Evaluation<rlt::nn::layers::sample_and_squash::mode::DisableEntropy<rlt::mode::Final>>> evaluation_mode;
-    rlt::evaluate(device, env_eval, env_eval_parameters, ui, evaluation_actor, result, *data, eval_buffer, rng, evaluation_mode, false, true);
-
-    rlt::free(device, evaluation_actor);
-    rlt::free(device, eval_buffer);
-    rlt::free(device, rng);
-    return base_env.parameters;
-}
 
 
 template <typename DATA, typename INPUT_SPEC, typename OUTPUT_SPEC, typename PARAMS, typename RNG>
