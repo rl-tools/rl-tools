7cfb0a902a16f816f405d0f3806d96e7e2f59aee
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-02-24 18:24:32 -0500

    all required comps

--- static/l2f/l2f.cpp
+++ static/l2f/l2f.cpp
@@ -24,9 +24,12 @@ int main(){
   rlt::sample_initial_parameters(device, env, parameters, rng);
   ENVIRONMENT::State state, next_state;
   rlt::sample_initial_state(device, env, parameters, state, rng);
-  rlt::Matrix<rlt::matrix::Specification<T, TI, 1, 4, true>> action;
+  rlt::Matrix<rlt::matrix::Specification<T, TI, 1, 4, false>> action;
+  rlt::Matrix<rlt::matrix::Specification<T, TI, 1, ENVIRONMENT::Observation::DIM, false>> observation;
+  rlt::observe(device, env, parameters, state, ENVIRONMENT::Observation{}, observation, rng);
   rlt::set_all(device, action, 0.0);
   rlt::step(device, env, parameters, state, action, next_state, rng);
+
 
   return 0;
 }
