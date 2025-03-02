907ae295c2a29459bbc69ececb50fbb83c0250d9
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-03-02 11:44:26 -0500

    adding symlink to rl_tools such that the -gsource-map of emcc and the chrome dev tools can find the sources

--- static/l2f-studio/build_emscripten.sh
+++ static/l2f-studio/build_emscripten.sh
@@ -4,4 +4,6 @@ docker run -it --rm \
 --mount type=bind,source=$(cd ../../external/json && pwd),target=/json,readonly \
 -w /mnt \
 emscripten/emsdk:4.0.4 \
-emcc -std=c++17 -O3 -I /rl_tools/include -I /json/include -DEMSCRIPTEN -DRL_TOOLS_ENABLE_JSON -s WASM=1 --bind -s EXPORTED_RUNTIME_METHODS='["cwrap", "ccall"]' -s EXPORT_ES6=1 -s MODULARIZE=1 -o l2f-interface.js l2f.cpp
+emcc -std=c++17 -I /rl_tools/include -I /json/include -DEMSCRIPTEN -DRL_TOOLS_ENABLE_JSON \
+-gsource-map -O0 -sSAFE_HEAP=1 -sASSERTIONS=1 \
+-s WASM=1 --bind -s EXPORTED_RUNTIME_METHODS='["cwrap", "ccall"]' -s EXPORT_ES6=1 -s MODULARIZE=1 -o l2f-interface.js l2f.cpp
