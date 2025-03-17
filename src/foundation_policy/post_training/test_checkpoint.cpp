a98277680c249446aaaa1cec288a772d17ce05d2
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-03-17 11:23:04 -0400

    post training to config

---
+++ src/foundation_policy/post_training/test_checkpoint.cpp
@@ -0,0 +1,100 @@
+#include <rl_tools/operations/cpu.h>
+
+#include <rl_tools/nn/layers/dense/operations_cpu.h>
+#include <rl_tools/nn/layers/sample_and_squash/operations_generic.h>
+#include <rl_tools/nn/layers/gru/operations_generic.h>
+#include <rl_tools/nn/layers/sample_and_squash/operations_generic.h>
+#include <rl_tools/nn_models/mlp/operations_generic.h>
+#include <rl_tools/nn_models/sequential/operations_generic.h>
+#include <rl_tools/rl/environments/l2f/operations_cpu.h>
+#include <rl_tools/rl/algorithms/sac/loop/core/config.h>
+#include <rl_tools/rl/loop/steps/extrack/config.h>
+#include <rl_tools/rl/loop/steps/evaluation/config.h>
+#include <rl_tools/rl/loop/steps/checkpoint/config.h>
+#include <rl_tools/rl/loop/steps/save_trajectories/config.h>
+#include <rl_tools/rl/loop/steps/timing/config.h>
+
+#include "../../../logs/2025-03-17_10-54-33/checkpoint.h"
+
+namespace rlt = rl_tools;
+
+using DEVICE = rlt::devices::DefaultCPU;
+using RNG = DEVICE::SPEC::RANDOM::ENGINE<>;
+using T = float;
+using TI = typename DEVICE::index_t;
+static constexpr bool DYNAMIC_ALLOCATION = true;
+
+using CAPABILITY = rlt::nn::capability::Gradient<rlt::nn::parameters::Adam, DYNAMIC_ALLOCATION>;
+using INPUT_SHAPE = rlt::tensor::Shape<TI, SEQUENCE_LENGTH, BATCH_SIZE, ENVIRONMENT::Observation::DIM>;
+using ACTOR = rlt::nn_models::sequential::Build<CAPABILITY, MODULE_CHAIN, INPUT_SHAPE>;
+using OPTIMIZER = rlt::nn::optimizers::Adam<rlt::nn::optimizers::adam::Specification<T, TI, ADAM_PARAMETERS>>;
+using OUTPUT_SHAPE = ACTOR::OUTPUT_SHAPE;
+using RESULT = rlt::rl::utils::evaluation::Result<rlt::rl::utils::evaluation::Specification<T, TI, ENVIRONMENT, NUM_EPISODES, ENVIRONMENT::EPISODE_STEP_LIMIT>>;
+using RESULT_EVAL = rlt::rl::utils::evaluation::Result<rlt::rl::utils::evaluation::Specification<T, TI, ENVIRONMENT, NUM_EPISODES_EVAL, ENVIRONMENT::EPISODE_STEP_LIMIT>>;
+using DATA = rlt::rl::utils::evaluation::Data<RESULT::SPEC>;
+using DATA_EVAL = rlt::rl::utils::evaluation::NoData<RESULT_EVAL::SPEC>;
+
+int test(){
+    DEVICE device;
+    RNG rng;
+    rlt::init(device, rng, 0);
+
+    using ACTOR_TYPE = rl_tools::checkpoint::actor::TYPE::CHANGE_BATCH_SIZE<TI, 1>;
+    ACTOR_TYPE::Buffer<false> buffer;
+    // test
+    rl_tools::checkpoint::actor::TYPE::Buffer<> test_buffer;
+    rlt::Tensor<rlt::tensor::Specification<T, TI, rl_tools::checkpoint::example::output::SHAPE>> output;
+    rlt::malloc(device, test_buffer);
+    rlt::malloc(device, output);
+    rlt::Mode<rlt::mode::Evaluation<>> mode;
+    rlt::evaluate(device, rl_tools::checkpoint::actor::module, rl_tools::checkpoint::example::input::container, output, test_buffer, rng, mode);
+    T abs_diff = rlt::abs_diff(device, rl_tools::checkpoint::example::output::container, output) / rl_tools::checkpoint::example::output::SPEC::SIZE;
+    auto last_step_output = rlt::view(device, output, rlt::get<0>(rl_tools::checkpoint::example::output::SHAPE{}) - 1);
+    auto last_step_expected = rlt::view(device, rl_tools::checkpoint::example::output::container, rlt::get<0>(rl_tools::checkpoint::example::output::SHAPE{}) - 1);
+    std::cout << "last_step_output: " << std::endl;
+    rlt::print(device, last_step_output);
+    std::cout << "last_step_expected: " << std::endl;
+    rlt::print(device, last_step_expected);
+
+    std::cout << "abs_diff to checkpoint example: " << abs_diff << std::endl;
+    rlt::free(device, test_buffer);
+    rlt::free(device, output);
+    return abs_diff < 1e-5 ? 0 : 1;
+}
+
+
+int main(){
+    using EVALUATION_ACTOR_TYPE_BATCH_SIZE = typename ACTOR::template CHANGE_BATCH_SIZE<TI, NUM_EPISODES>;
+    using EVALUATION_ACTOR_TYPE = typename EVALUATION_ACTOR_TYPE_BATCH_SIZE::template CHANGE_CAPABILITY<rlt::nn::capability::Forward<DYNAMIC_ALLOCATION>>;
+    rlt::rl::environments::DummyUI ui;
+    EVALUATION_ACTOR_TYPE evaluation_actor;
+    EVALUATION_ACTOR_TYPE::Buffer<DYNAMIC_ALLOCATION> eval_buffer;
+    rlt::malloc(device, evaluation_actor);
+    rlt::malloc(device, eval_buffer);
+    rlt::copy(device, device, actor, evaluation_actor);
+
+    ENVIRONMENT env_eval;
+    ENVIRONMENT::Parameters env_eval_parameters;
+    rlt::init(device, env_eval);
+    rlt::sample_initial_parameters(device, env_eval, env_eval_parameters, rng);
+    rlt::Mode<rlt::mode::Default<>> mode;
+    RESULT_EVAL result_eval;
+    DATA_EVAL data_eval;
+    rlt::evaluate(device, env_eval, env_eval_parameters, ui, evaluation_actor, result_eval, data_eval, eval_buffer, rng, mode, false, true);
+    rlt::add_scalar(device, device.logger, "evaluation/return/mean", result_eval.returns_mean);
+    rlt::add_scalar(device, device.logger, "evaluation/return/std", result_eval.returns_std);
+    rlt::add_scalar(device, device.logger, "evaluation/episode_length/mean", result_eval.episode_length_mean);
+    rlt::add_scalar(device, device.logger, "evaluation/episode_length/std", result_eval.episode_length_std);
+    rlt::add_scalar(device, device.logger, "evaluation/share_terminated", result_eval.share_terminated);
+    rlt::log(device, device.logger, "Mean return: ", result_eval.returns_mean, " Mean episode length: ", result_eval.episode_length_mean, " Share terminated: ", result_eval.share_terminated * 100, "%");
+
+    if (!best_return_set || result_eval.returns_mean > best_return){
+        best_return = result_eval.returns_mean;
+        best_return_set = true;
+        rlt::copy(device, device, evaluation_actor, best_actor);
+        std::cout << "Best return: " << best_return << std::endl;
+    }
+
+    rlt::free(device, evaluation_actor);
+    rlt::free(device, eval_buffer);
+}
