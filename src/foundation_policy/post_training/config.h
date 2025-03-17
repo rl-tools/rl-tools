da9a3ec68c5f3f6db1942d61b679219436f7f4ce
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-03-17 11:23:04 -0400

    post training to config

---
+++ src/foundation_policy/post_training/config.h
@@ -0,0 +1,55 @@
+// constants parameters
+constexpr TI NUM_EPISODES = 1000;
+constexpr TI NUM_EPISODES_EVAL = 100;
+constexpr TI N_EPOCH = 100;
+constexpr TI N_PRE_TRAINING_SEEDS = 1;
+constexpr TI SEQUENCE_LENGTH = 32;
+constexpr TI BATCH_SIZE = 32;
+constexpr T SOLVED_RETURN = 550;
+constexpr TI HIDDEN_DIM = 16;
+
+// typedefs
+using ENVIRONMENT = typename builder::ENVIRONMENT_FACTORY_POST_TRAINING<DEVICE, T, TI, OPTIONS_POST_TRAINING>::ENVIRONMENT;
+struct ENVIRONMENT_PT_STATIC_PARAMETERS: ENVIRONMENT::SPEC::STATIC_PARAMETERS{
+    using LOOP_CORE_CONFIG_PRE_TRAINING = builder::FACTORY<DEVICE, T, TI, RNG, OPTIONS_PRE_TRAINING, DYNAMIC_ALLOCATION>::LOOP_CORE_CONFIG;
+    using ENV = LOOP_CORE_CONFIG_PRE_TRAINING::ENVIRONMENT;
+    using OBSERVATION_TYPE = ENV::Observation;
+    using OBSERVATION_TYPE_PRIVILEGED = OBSERVATION_TYPE;
+    static constexpr auto PARAMETER_VALUES = [](){
+        auto params = ENVIRONMENT::SPEC::STATIC_PARAMETERS::PARAMETER_VALUES;
+        params.mdp.observation_noise.position = 0;
+        params.mdp.observation_noise.orientation = 0;
+        params.mdp.observation_noise.linear_velocity = 0;
+        params.mdp.observation_noise.angular_velocity = 0;
+        params.mdp.observation_noise.imu_acceleration = 0;
+        return params;
+    }();
+};
+using ENVIRONMENT_PT_SPEC = rl_tools::rl::environments::l2f::MultiTaskSpecification<T, TI, ENVIRONMENT_PT_STATIC_PARAMETERS, OPTIONS_POST_TRAINING::SAMPLE_INITIAL_PARAMETERS>;
+using ENVIRONMENT_PT = rl_tools::rl::environments::MultirotorMultiTask<ENVIRONMENT_PT_SPEC>;
+
+template <typename T_CONTENT, typename T_NEXT_MODULE = rlt::nn_models::sequential::OutputModule>
+using Module = typename rlt::nn_models::sequential::Module<T_CONTENT, T_NEXT_MODULE>;
+
+// using MLP_CONFIG = rlt::nn_models::mlp::Configuration<T, TI, ENVIRONMENT::ACTION_DIM, 3, HIDDEN_DIM, rlt::nn::activation_functions::ActivationFunction::FAST_TANH, rlt::nn::activation_functions::ActivationFunction::IDENTITY>;
+// using MLP = rlt::nn_models::mlp::BindConfiguration<MLP_CONFIG>;
+// using MODULE_CHAIN = Module<MLP>;
+
+using INPUT_LAYER_CONFIG = rlt::nn::layers::dense::Configuration<T, TI, HIDDEN_DIM, rlt::nn::activation_functions::ActivationFunction::RELU>;
+using INPUT_LAYER = rlt::nn::layers::dense::BindConfiguration<INPUT_LAYER_CONFIG>;
+using GRU_CONFIG = rlt::nn::layers::gru::Configuration<T, TI, HIDDEN_DIM>;
+using GRU = rlt::nn::layers::gru::BindConfiguration<GRU_CONFIG>;
+using OUTPUT_LAYER_CONFIG = rlt::nn::layers::dense::Configuration<T, TI, ENVIRONMENT::ACTION_DIM, rlt::nn::activation_functions::ActivationFunction::IDENTITY>;
+using OUTPUT_LAYER = rlt::nn::layers::dense::BindConfiguration<OUTPUT_LAYER_CONFIG>;
+using MODULE_CHAIN = Module<INPUT_LAYER, Module<GRU, Module<OUTPUT_LAYER>>>;
+
+
+using CAPABILITY = rlt::nn::capability::Gradient<rlt::nn::parameters::Adam, DYNAMIC_ALLOCATION>;
+using INPUT_SHAPE = rlt::tensor::Shape<TI, SEQUENCE_LENGTH, BATCH_SIZE, ENVIRONMENT::Observation::DIM>;
+using ACTOR = rlt::nn_models::sequential::Build<CAPABILITY, MODULE_CHAIN, INPUT_SHAPE>;
+using OPTIMIZER = rlt::nn::optimizers::Adam<rlt::nn::optimizers::adam::Specification<T, TI, ADAM_PARAMETERS>>;
+using OUTPUT_SHAPE = ACTOR::OUTPUT_SHAPE;
+using RESULT = rlt::rl::utils::evaluation::Result<rlt::rl::utils::evaluation::Specification<T, TI, ENVIRONMENT, NUM_EPISODES, ENVIRONMENT::EPISODE_STEP_LIMIT>>;
+using RESULT_EVAL = rlt::rl::utils::evaluation::Result<rlt::rl::utils::evaluation::Specification<T, TI, ENVIRONMENT, NUM_EPISODES_EVAL, ENVIRONMENT::EPISODE_STEP_LIMIT>>;
+using DATA = rlt::rl::utils::evaluation::Data<RESULT::SPEC>;
+using DATA_EVAL = rlt::rl::utils::evaluation::NoData<RESULT_EVAL::SPEC>;
--- src/foundation_policy/post_training/main.cpp
+++ src/foundation_policy/post_training/main.cpp
@@ -72,61 +72,8 @@ struct OPTIONS_POST_TRAINING: OPTIONS_PRE_TRAINING{
 struct ADAM_PARAMETERS: rlt::nn::optimizers::adam::DEFAULT_PARAMETERS_TENSORFLOW<T>{
     static constexpr T ALPHA = 0.0001;
 };
-// constants parameters
-constexpr TI NUM_EPISODES = 1000;
-constexpr TI NUM_EPISODES_EVAL = 100;
-constexpr TI N_EPOCH = 100;
-constexpr TI N_PRE_TRAINING_SEEDS = 1;
-constexpr TI SEQUENCE_LENGTH = 32;
-constexpr TI BATCH_SIZE = 32;
-constexpr T SOLVED_RETURN = 550;
-constexpr TI HIDDEN_DIM = 16;
 
-// typedefs
-using ENVIRONMENT = typename builder::ENVIRONMENT_FACTORY_POST_TRAINING<DEVICE, T, TI, OPTIONS_POST_TRAINING>::ENVIRONMENT;
-struct ENVIRONMENT_PT_STATIC_PARAMETERS: ENVIRONMENT::SPEC::STATIC_PARAMETERS{
-    using LOOP_CORE_CONFIG_PRE_TRAINING = builder::FACTORY<DEVICE, T, TI, RNG, OPTIONS_PRE_TRAINING, DYNAMIC_ALLOCATION>::LOOP_CORE_CONFIG;
-    using ENV = LOOP_CORE_CONFIG_PRE_TRAINING::ENVIRONMENT;
-    using OBSERVATION_TYPE = ENV::Observation;
-    using OBSERVATION_TYPE_PRIVILEGED = OBSERVATION_TYPE;
-    static constexpr auto PARAMETER_VALUES = [](){
-        auto params = ENVIRONMENT::SPEC::STATIC_PARAMETERS::PARAMETER_VALUES;
-        params.mdp.observation_noise.position = 0;
-        params.mdp.observation_noise.orientation = 0;
-        params.mdp.observation_noise.linear_velocity = 0;
-        params.mdp.observation_noise.angular_velocity = 0;
-        params.mdp.observation_noise.imu_acceleration = 0;
-        return params;
-    }();
-};
-using ENVIRONMENT_PT_SPEC = rl_tools::rl::environments::l2f::MultiTaskSpecification<T, TI, ENVIRONMENT_PT_STATIC_PARAMETERS, OPTIONS_POST_TRAINING::SAMPLE_INITIAL_PARAMETERS>;
-using ENVIRONMENT_PT = rl_tools::rl::environments::MultirotorMultiTask<ENVIRONMENT_PT_SPEC>;
-
-template <typename T_CONTENT, typename T_NEXT_MODULE = rlt::nn_models::sequential::OutputModule>
-using Module = typename rlt::nn_models::sequential::Module<T_CONTENT, T_NEXT_MODULE>;
-
-// using MLP_CONFIG = rlt::nn_models::mlp::Configuration<T, TI, ENVIRONMENT::ACTION_DIM, 3, HIDDEN_DIM, rlt::nn::activation_functions::ActivationFunction::FAST_TANH, rlt::nn::activation_functions::ActivationFunction::IDENTITY>;
-// using MLP = rlt::nn_models::mlp::BindConfiguration<MLP_CONFIG>;
-// using MODULE_CHAIN = Module<MLP>;
-
-using INPUT_LAYER_CONFIG = rlt::nn::layers::dense::Configuration<T, TI, HIDDEN_DIM, rlt::nn::activation_functions::ActivationFunction::RELU>;
-using INPUT_LAYER = rlt::nn::layers::dense::BindConfiguration<INPUT_LAYER_CONFIG>;
-using GRU_CONFIG = rlt::nn::layers::gru::Configuration<T, TI, HIDDEN_DIM>;
-using GRU = rlt::nn::layers::gru::BindConfiguration<GRU_CONFIG>;
-using OUTPUT_LAYER_CONFIG = rlt::nn::layers::dense::Configuration<T, TI, ENVIRONMENT::ACTION_DIM, rlt::nn::activation_functions::ActivationFunction::IDENTITY>;
-using OUTPUT_LAYER = rlt::nn::layers::dense::BindConfiguration<OUTPUT_LAYER_CONFIG>;
-using MODULE_CHAIN = Module<INPUT_LAYER, Module<GRU, Module<OUTPUT_LAYER>>>;
-
-
-using CAPABILITY = rlt::nn::capability::Gradient<rlt::nn::parameters::Adam, DYNAMIC_ALLOCATION>;
-using INPUT_SHAPE = rlt::tensor::Shape<TI, SEQUENCE_LENGTH, BATCH_SIZE, ENVIRONMENT::Observation::DIM>;
-using ACTOR = rlt::nn_models::sequential::Build<CAPABILITY, MODULE_CHAIN, INPUT_SHAPE>;
-using OPTIMIZER = rlt::nn::optimizers::Adam<rlt::nn::optimizers::adam::Specification<T, TI, ADAM_PARAMETERS>>;
-using OUTPUT_SHAPE = ACTOR::OUTPUT_SHAPE;
-using RESULT = rlt::rl::utils::evaluation::Result<rlt::rl::utils::evaluation::Specification<T, TI, ENVIRONMENT, NUM_EPISODES, ENVIRONMENT::EPISODE_STEP_LIMIT>>;
-using RESULT_EVAL = rlt::rl::utils::evaluation::Result<rlt::rl::utils::evaluation::Specification<T, TI, ENVIRONMENT, NUM_EPISODES_EVAL, ENVIRONMENT::EPISODE_STEP_LIMIT>>;
-using DATA = rlt::rl::utils::evaluation::Data<RESULT::SPEC>;
-using DATA_EVAL = rlt::rl::utils::evaluation::NoData<RESULT_EVAL::SPEC>;
+#include "config.h"
 
 // constants derived
 constexpr TI DATASET_SIZE = N_PRE_TRAINING_SEEDS * NUM_EPISODES * ENVIRONMENT::EPISODE_STEP_LIMIT;
@@ -323,7 +270,7 @@ int main(int argc, char** argv){
         }
         std::cout << "Epoch: " << epoch_i << " Loss: " << epoch_loss/epoch_loss_count << std::endl;
         {
-            using EVALUATION_ACTOR_TYPE_BATCH_SIZE = typename ACTOR::template CHANGE_BATCH_SIZE<TI, NUM_EPISODES>;
+            using EVALUATION_ACTOR_TYPE_BATCH_SIZE = typename ACTOR::template CHANGE_BATCH_SIZE<TI, NUM_EPISODES_EVAL>;
             using EVALUATION_ACTOR_TYPE = typename EVALUATION_ACTOR_TYPE_BATCH_SIZE::template CHANGE_CAPABILITY<rlt::nn::capability::Forward<DYNAMIC_ALLOCATION>>;
             rlt::rl::environments::DummyUI ui;
             EVALUATION_ACTOR_TYPE evaluation_actor;
