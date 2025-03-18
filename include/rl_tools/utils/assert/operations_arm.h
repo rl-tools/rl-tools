8a3f0727fa4e29aed5e564288a103ce5980b13f0
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-03-18 13:56:29 -0400

    guarding arm assert to allow compilation for uc

--- include/rl_tools/utils/assert/operations_arm.h
+++ include/rl_tools/utils/assert/operations_arm.h
@@ -3,15 +3,19 @@
 #pragma once
 #define RL_TOOLS_UTILS_ASSERT_OPERATIONS_ARM_H
 
+#ifdef RL_TOOLS_ARM_TEST
 #include <cstdlib>
 #include <stdexcept>
+#endif
 RL_TOOLS_NAMESPACE_WRAPPER_START
 namespace rl_tools::utils{
     template <typename DEV_SPEC, typename T>
     void assert_exit(devices::ARM<DEV_SPEC>& device, bool condition, T message){
         if(!condition){
             log(device, device.logger, message);
+#ifdef RL_TOOLS_ARM_TEST
             throw std::runtime_error(message);
+#endif
         }
     }
 }
