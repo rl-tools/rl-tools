9162b13bc2ce9373a678da0c897a972317b4aa34
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-03-18 12:09:56 -0400

    fixing crazyflie fw crash/"kalman rate low"

--- include/rl_tools/math/operations_arm.h
+++ include/rl_tools/math/operations_arm.h
@@ -7,7 +7,7 @@
 
 #include "../devices/arm.h"
 
-// #include <cmath>
+#include <cmath>
 //#include <algorithm>
 
 
@@ -16,39 +16,39 @@ namespace rl_tools::math {
 
     template<typename T>
     T sqrt(const devices::math::ARM&, const T x) {
-        return __builtin_sqrt(x);
+        return std::sqrt(x);
     }
     template<typename T>
     T tanh(const devices::math::ARM&, const T x) {
-        return __builtin_tanh(x);
+        return std::tanh(x);
     }
     template<typename T>
     T exp(const devices::math::ARM&, const T x) {
-        return __builtin_exp(x);
+        return std::exp(x);
     }
     template<typename T>
     T sin(const devices::math::ARM&, const T x) {
-        return __builtin_sin(x);
+        return std::sin(x);
     }
     template<typename T>
     T cos(const devices::math::ARM&, const T x) {
-        return __builtin_cos(x);
+        return std::cos(x);
     }
     template<typename T>
     T acos(const devices::math::ARM&, const T x) {
-        return __builtin_acos(x);
+        return std::acos(x);
     }
     template<typename TX, typename TY>
     auto pow(const devices::math::ARM&, const TX x, const TY y) {
-        return __builtin_pow(x, y);
+        return std::pow(x, y);
     }
     template<typename T>
     auto log(const devices::math::ARM&, const T x) {
-        return __builtin_log(x);
+        return std::log(x);
     }
     template<typename T>
     auto floor(const devices::math::ARM&, const T x) {
-        return __builtin_floor(x);
+        return std::floor(x);
     }
     template<typename T>
     auto is_nan(const devices::math::ARM&, const T x) {
@@ -56,26 +56,26 @@ namespace rl_tools::math {
     }
     template<typename T>
     auto is_finite(const devices::math::ARM&, const T x) {
-        return __builtin_isfinite(x);
+        return std::isfinite(x);
     }
     template<typename T>
     T clamp(const devices::math::ARM&, T x, T min, T max){
-//        return __builtin_clamp(x, min, max);
+//        return std::clamp(x, min, max);
         return x < min ? min : (x > max ? max : x);
     }
     template<typename T>
     T min(const devices::math::ARM&, T x, T y){
-//        return __builtin_min(x, y);
+//        return std::min(x, y);
         return x < y ? x : y;
     }
     template<typename T>
     T max(const devices::math::ARM&, T x, T y){
-//        return __builtin_max(x, y);
+//        return std::max(x, y);
         return x > y ? x : y;
     }
     template<typename T>
     T abs(const devices::math::ARM&, T x){
-        return __builtin_abs(x);
+        return std::abs(x);
     }
     template<typename T>
     T nan(const devices::math::ARM&){
@@ -83,7 +83,7 @@ namespace rl_tools::math {
     }
     template<typename T>
     T atan2(const devices::math::ARM& dev, T a, T b) {
-        return __builtin_atan2(a, b);
+        return std::atan2(a, b);
     }
     template<typename T>
     T fast_sigmoid(const devices::math::ARM& dev, T x) {
