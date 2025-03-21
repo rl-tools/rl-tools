312bbf1b9b3be764bccf8ff9de42f1c8606861a1
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-03-21 17:10:38 -0400

    splitting out state algebra; ident

--- include/rl_tools/rl/environments/l2f/operations_generic.h
+++ include/rl_tools/rl/environments/l2f/operations_generic.h
@@ -18,141 +18,19 @@
 
 RL_TOOLS_NAMESPACE_WRAPPER_START
 namespace rl_tools{
-    // State arithmetic for RK4 integration
-    // scalar multiply
-    template<typename DEVICE, typename STATE_SPEC, typename T>
-    RL_TOOLS_FUNCTION_PLACEMENT static void scalar_multiply(DEVICE& device, const typename rl::environments::l2f::StateBase<STATE_SPEC>& state, T scalar, rl::environments::l2f::StateBase<STATE_SPEC>& out){
-        for(int i = 0; i < 3; ++i){
-            out.position[i]         = scalar * state.position[i]        ;
-            out.orientation[i]      = scalar * state.orientation[i]     ;
-            out.linear_velocity[i]  = scalar * state.linear_velocity[i] ;
-            out.angular_velocity[i] = scalar * state.angular_velocity[i];
-        }
-        out.orientation[3] = scalar * state.orientation[3];
-    }
-    template<typename DEVICE, typename STATE_SPEC, typename T>
-    RL_TOOLS_FUNCTION_PLACEMENT static void scalar_multiply(DEVICE& device, const typename rl::environments::l2f::StatePoseErrorIntegral<STATE_SPEC>& state, T scalar, rl::environments::l2f::StatePoseErrorIntegral<STATE_SPEC>& out){
-        scalar_multiply(device, static_cast<const typename STATE_SPEC::NEXT_COMPONENT&>(state), scalar, static_cast<typename STATE_SPEC::NEXT_COMPONENT&>(out));
-        out.position_integral = scalar * out.position_integral;
-        out.orientation_integral = scalar * out.orientation_integral;
-    }
-    template<typename DEVICE, typename STATE_SPEC, typename T>
-    RL_TOOLS_FUNCTION_PLACEMENT static void scalar_multiply(DEVICE& device, const typename rl::environments::l2f::StateRotors<STATE_SPEC>& state, T scalar, rl::environments::l2f::StateRotors<STATE_SPEC>& out){
-        scalar_multiply(device, static_cast<const typename STATE_SPEC::NEXT_COMPONENT&>(state), scalar, static_cast<typename STATE_SPEC::NEXT_COMPONENT&>(out));
-        if constexpr(!STATE_SPEC::CLOSED_FORM){
-            for(int i = 0; i < 4; ++i){
-                out.rpm[i] = scalar * state.rpm[i];
-            }
-        }
-    }
-    template<typename DEVICE, typename STATE, typename T>
-    RL_TOOLS_FUNCTION_PLACEMENT static void scalar_multiply(DEVICE& device, const STATE& state, T scalar, STATE& out, utils::typing::enable_if_t<!STATE::REQUIRES_INTEGRATION, bool> disable = false){
-        static_assert(!STATE::REQUIRES_INTEGRATION);
-        scalar_multiply(device, static_cast<const typename STATE::NEXT_COMPONENT&>(state), scalar, static_cast<typename STATE::NEXT_COMPONENT&>(out));
-    }
-    // scalar multiply in place
-    template<typename DEVICE, typename STATE_SPEC, typename T>
-    RL_TOOLS_FUNCTION_PLACEMENT static void scalar_multiply(DEVICE& device, typename rl::environments::l2f::StateBase<STATE_SPEC>& state, T scalar){
-        scalar_multiply(device, state, scalar, state);
-    }
-    template<typename DEVICE, typename STATE_SPEC, typename T>
-    RL_TOOLS_FUNCTION_PLACEMENT static void scalar_multiply(DEVICE& device, typename rl::environments::l2f::StatePoseErrorIntegral<STATE_SPEC>& state, T scalar){
-        scalar_multiply(device, state, scalar, state);
-    }
-    template<typename DEVICE, typename STATE_SPEC, typename T>
-    RL_TOOLS_FUNCTION_PLACEMENT static void scalar_multiply(DEVICE& device, typename rl::environments::l2f::StateRotors<STATE_SPEC>& state, T scalar){
-        scalar_multiply(device, state, scalar, state);
-    }
-    template<typename DEVICE, typename STATE, typename T>
-    RL_TOOLS_FUNCTION_PLACEMENT static void scalar_multiply(DEVICE& device, STATE& state, T scalar, utils::typing::enable_if_t<!STATE::REQUIRES_INTEGRATION, bool> disable = false){
-        static_assert(!STATE::REQUIRES_INTEGRATION);
-        scalar_multiply(device, static_cast<typename STATE::NEXT_COMPONENT&>(state), scalar);
-    }
-
-    template<typename DEVICE, typename STATE_SPEC, typename T>
-    RL_TOOLS_FUNCTION_PLACEMENT static void scalar_multiply_accumulate(DEVICE& device, const typename rl::environments::l2f::StateBase<STATE_SPEC>& state, T scalar, rl::environments::l2f::StateBase<STATE_SPEC>& out){
-        for(int i = 0; i < 3; ++i){
-            out.position[i]         += scalar * state.position[i]        ;
-            out.orientation[i]      += scalar * state.orientation[i]     ;
-            out.linear_velocity[i]  += scalar * state.linear_velocity[i] ;
-            out.angular_velocity[i] += scalar * state.angular_velocity[i];
-        }
-        out.orientation[3] += scalar * state.orientation[3];
-    }
-    template<typename DEVICE, typename STATE_SPEC, typename T>
-    RL_TOOLS_FUNCTION_PLACEMENT static void scalar_multiply_accumulate(DEVICE& device, const typename rl::environments::l2f::StatePoseErrorIntegral<STATE_SPEC>& state, T scalar, rl::environments::l2f::StatePoseErrorIntegral<STATE_SPEC>& out){
-        scalar_multiply_accumulate(device, static_cast<const typename STATE_SPEC::NEXT_COMPONENT&>(state), scalar, static_cast<typename STATE_SPEC::NEXT_COMPONENT&>(out));
-        out.position_integral += scalar * out.position_integral;
-        out.orientation_integral += scalar * out.orientation_integral;
-    }
-    template<typename DEVICE, typename STATE_SPEC, typename T>
-    RL_TOOLS_FUNCTION_PLACEMENT static void scalar_multiply_accumulate(DEVICE& device, const typename rl::environments::l2f::StateRotors<STATE_SPEC>& state, T scalar, rl::environments::l2f::StateRotors<STATE_SPEC>& out){
-        scalar_multiply_accumulate(device, static_cast<const typename STATE_SPEC::NEXT_COMPONENT&>(state), scalar, static_cast<typename STATE_SPEC::NEXT_COMPONENT&>(out));
-        if constexpr(!STATE_SPEC::CLOSED_FORM) {
-            for(int i = 0; i < 4; ++i){
-                out.rpm[i] += scalar * state.rpm[i];
-            }
-        }
-    }
-    template<typename DEVICE, typename STATE, typename T>
-    RL_TOOLS_FUNCTION_PLACEMENT static void scalar_multiply_accumulate(DEVICE& device, const STATE& state, T scalar, STATE& out, utils::typing::enable_if_t<!STATE::REQUIRES_INTEGRATION, bool> disable = false){
-        static_assert(!STATE::REQUIRES_INTEGRATION);
-        scalar_multiply_accumulate(device, static_cast<const typename STATE::NEXT_COMPONENT&>(state), scalar, static_cast<typename STATE::NEXT_COMPONENT&>(out));
-    }
-
-    template<typename DEVICE, typename STATE_SPEC>
-    RL_TOOLS_FUNCTION_PLACEMENT static void add_accumulate(DEVICE& device, const typename rl::environments::l2f::StateBase<STATE_SPEC>& s1, const rl::environments::l2f::StateBase<STATE_SPEC>& s2, rl::environments::l2f::StateBase<STATE_SPEC>& out){
-        for(int i = 0; i < 3; ++i){
-            out.position[i]         = s1.position[i] + s2.position[i];
-            out.orientation[i]      = s1.orientation[i] + s2.orientation[i];
-            out.linear_velocity[i]  = s1.linear_velocity[i] + s2.linear_velocity[i];
-            out.angular_velocity[i] = s1.angular_velocity[i] + s2.angular_velocity[i];
-        }
-        out.orientation[3] = s1.orientation[3] + s2.orientation[3];
-    }
-    template<typename DEVICE, typename STATE_SPEC>
-    RL_TOOLS_FUNCTION_PLACEMENT static void add_accumulate(DEVICE& device, const typename rl::environments::l2f::StatePoseErrorIntegral<STATE_SPEC>& s1, const rl::environments::l2f::StatePoseErrorIntegral<STATE_SPEC>& s2, rl::environments::l2f::StatePoseErrorIntegral<STATE_SPEC>& out){
-        add_accumulate(device, static_cast<const typename STATE_SPEC::NEXT_COMPONENT&>(s1), static_cast<const typename STATE_SPEC::NEXT_COMPONENT&>(s2), static_cast<typename STATE_SPEC::NEXT_COMPONENT&>(out));
-        out.position_integral = s1.position_integral + s2.position_integral;
-        out.orientation_integral = s1.orientation_integral + s2.orientation_integral;
-    }
-    template<typename DEVICE, typename STATE_SPEC>
-    RL_TOOLS_FUNCTION_PLACEMENT static void add_accumulate(DEVICE& device, const typename rl::environments::l2f::StateRotors<STATE_SPEC>& s1, const rl::environments::l2f::StateRotors<STATE_SPEC>& s2, rl::environments::l2f::StateRotors<STATE_SPEC>& out){
-        add_accumulate(device, static_cast<const typename STATE_SPEC::NEXT_COMPONENT&>(s1), static_cast<const typename STATE_SPEC::NEXT_COMPONENT&>(s2), static_cast<typename STATE_SPEC::NEXT_COMPONENT&>(out));
-        if constexpr(!STATE_SPEC::CLOSED_FORM) {
-            for(int i = 0; i < 4; ++i){
-                out.rpm[i] = s1.rpm[i] + s2.rpm[i];
-            }
-        }
-    }
-    template<typename DEVICE, typename STATE>
-    RL_TOOLS_FUNCTION_PLACEMENT static void add_accumulate(DEVICE& device, const STATE& s1, const STATE& s2, STATE& out, utils::typing::enable_if_t<!STATE::REQUIRES_INTEGRATION, bool> disable = false){
-        static_assert(!STATE::REQUIRES_INTEGRATION);
-        add_accumulate(device, static_cast<const typename STATE::NEXT_COMPONENT&>(s1), static_cast<const typename STATE::NEXT_COMPONENT&>(s2), static_cast<typename STATE::NEXT_COMPONENT&>(out));
-    }
-    template<typename DEVICE, typename STATE_SPEC>
-    RL_TOOLS_FUNCTION_PLACEMENT static void add_accumulate(DEVICE& device, const rl::environments::l2f::StateBase<STATE_SPEC>& s, rl::environments::l2f::StateBase<STATE_SPEC>& out){
-        add_accumulate(device, s, out, out);
-    }
-    template<typename DEVICE, typename STATE_SPEC>
-    RL_TOOLS_FUNCTION_PLACEMENT static void add_accumulate(DEVICE& device, const rl::environments::l2f::StatePoseErrorIntegral<STATE_SPEC>& s, rl::environments::l2f::StatePoseErrorIntegral<STATE_SPEC>& out){
-        add_accumulate(device, s, out, out);
-    }
-    template<typename DEVICE, typename STATE_SPEC>
-    RL_TOOLS_FUNCTION_PLACEMENT static void add_accumulate(DEVICE& device, const rl::environments::l2f::StateRotors<STATE_SPEC>& s, rl::environments::l2f::StateRotors<STATE_SPEC>& out){
-        add_accumulate(device, s, out, out);
-    }
-    template<typename DEVICE, typename STATE>
-    RL_TOOLS_FUNCTION_PLACEMENT static void add_accumulate(DEVICE& device, const STATE& s, STATE& out, utils::typing::enable_if_t<!STATE::REQUIRES_INTEGRATION, bool> disable = false){
-        static_assert(!STATE::REQUIRES_INTEGRATION);
-        add_accumulate(device, static_cast<const typename STATE::NEXT_COMPONENT&>(s), static_cast<const typename STATE::NEXT_COMPONENT&>(out), static_cast<typename STATE::NEXT_COMPONENT&>(out));
-    }
 }
 RL_TOOLS_NAMESPACE_WRAPPER_END
 
 #include <rl_tools/utils/generic/integrators.h>
 
 
+// Since L2F is quite flexible in the way states and observations are composed, the operations might need to call each other in arbitrary order (depending on the definition). Hence we implement a dispatch scheme, were a dispatch function is forward declared such that it can be called from all specialized functions (_xxx). This dispatch function serves as the public interface xxx as well.
+#include "operations_generic/05_state_is_nan.h"
+#include "operations_generic/10_sample_initial_parameters.h"
+#include "operations_generic/20_initial_state.h"
+#include "operations_generic/30_sample_initial_state.h"
+#include "operations_generic/40_observe.h"
+#include "operations_generic/50_state_algebra.h"
 RL_TOOLS_NAMESPACE_WRAPPER_START
 namespace rl_tools::rl::environments::l2f{
     template<typename DEVICE, typename STATE_SPEC, typename PARAMETERS, typename T>
@@ -263,12 +141,6 @@ namespace rl_tools::rl::environments::l2f{
 RL_TOOLS_NAMESPACE_WRAPPER_END
 
 
-// Since L2F is quite flexible in the way states and observations are composed, the operations might need to call each other in arbitrary order (depending on the definition). Hence we implement a dispatch scheme, were a dispatch function is forward declared such that it can be called from all specialized functions (_xxx). This dispatch function serves as the public interface xxx as well.
-#include "operations_generic/05_state_is_nan.h"
-#include "operations_generic/10_sample_initial_parameters.h"
-#include "operations_generic/20_initial_state.h"
-#include "operations_generic/30_sample_initial_state.h"
-#include "operations_generic/40_observe.h"
 RL_TOOLS_NAMESPACE_WRAPPER_START
 namespace rl_tools
 {
