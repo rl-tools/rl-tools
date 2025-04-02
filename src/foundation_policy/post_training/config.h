caa9607082404190eec6e3ca1517ecde66f6d7e2
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-04-02 18:46:32 -0400

    adding importance sampling

--- src/foundation_policy/post_training/config.h
+++ src/foundation_policy/post_training/config.h
@@ -9,15 +9,16 @@ struct OPTIONS_POST_TRAINING: OPTIONS_PRE_TRAINING{
 
 static_assert(sizeof(TI) == 8);
 // constants parameters
-constexpr TI NUM_EPISODES = 1000;
+constexpr TI NUM_EPISODES = 10;
 constexpr TI NUM_EPISODES_EVAL = 100;
 constexpr TI N_EPOCH = 100;
 constexpr TI N_PRE_TRAINING_SEEDS = 1;
 constexpr TI SEQUENCE_LENGTH = 500;
 constexpr TI BATCH_SIZE = 64;
 constexpr T SOLVED_RETURN = 300;
-constexpr TI HIDDEN_DIM = 16;
-constexpr TI NUM_TEACHERS = 1;
+constexpr TI HIDDEN_DIM = 32;
+constexpr TI NUM_TEACHERS = 280;
+constexpr TI NUM_ACTIVE_TEACHERS = 100;
 constexpr TI EPOCH_DAGGER = 5;
 constexpr bool DYNAMIC_ALLOCATION = true;
 constexpr bool SHUFFLE = true;
