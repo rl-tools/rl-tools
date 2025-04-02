b4a1057fbad29bfbcc1abce4fc6b50bc71bb7295
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-04-02 17:01:29 -0400

    permute rotors; checking more known dynamics

--- src/foundation_policy/post_training/check_checkpoints.cpp
+++ src/foundation_policy/post_training/check_checkpoints.cpp
@@ -48,41 +48,6 @@ using TI = typename DEVICE::index_t;
 #include "environment.h"
 #include "../pre_training/config.h"
 #include "../pre_training/options.h"
-// #include "config.h"
-
-
-// int test(){
-//     DEVICE device;
-//     RNG rng;
-//     rlt::init(device, rng, 100);
-//
-//     // using ACTOR_TYPE = rl_tools::checkpoint::actor::TYPE::CHANGE_BATCH_SIZE<TI, 1>;
-//     using FACTORY = builder::FACTORY<DEVICE, T, TI, RNG, OPTIONS_PRE_TRAINING, DYNAMIC_ALLOCATION>;
-//     using LOOP_CORE_CONFIG = FACTORY::LOOP_CORE_CONFIG;
-//     using LOOP_CONFIG = builder::LOOP_ASSEMBLY<LOOP_CORE_CONFIG>::LOOP_CONFIG;
-//     using ACTOR_ORIG = LOOP_CONFIG::ACTOR_CRITIC_TYPE::SPEC::ACTOR_NETWORK_TYPE;
-//     using ACTOR = ACTOR_ORIG::CHANGE_BATCH_SIZE<TI, 32>::CHANGE_CAPABILITY<rlt::nn::capability::Forward<>>;
-//     ACTOR::Buffer<false> buffer;
-//     // test
-//     // rl_tools::checkpoint::actor::TYPE::Buffer<> test_buffer;
-//     // rlt::Tensor<rlt::tensor::Specification<T, TI, rl_tools::checkpoint::example::output::SHAPE>> output;
-//     // rlt::malloc(device, test_buffer);
-//     // rlt::malloc(device, output);
-//     // rlt::Mode<rlt::mode::Evaluation<>> mode;
-//     // rlt::evaluate(device, rl_tools::checkpoint::actor::module, rl_tools::checkpoint::example::input::container, output, test_buffer, rng, mode);
-//     // T abs_diff = rlt::abs_diff(device, rl_tools::checkpoint::example::output::container, output) / rl_tools::checkpoint::example::output::SPEC::SIZE;
-//     // auto last_step_output = rlt::view(device, output, rlt::get<0>(rl_tools::checkpoint::example::output::SHAPE{}) - 1);
-//     // auto last_step_expected = rlt::view(device, rl_tools::checkpoint::example::output::container, rlt::get<0>(rl_tools::checkpoint::example::output::SHAPE{}) - 1);
-//     // std::cout << "last_step_output: " << std::endl;
-//     // rlt::print(device, last_step_output);
-//     // std::cout << "last_step_expected: " << std::endl;
-//     // rlt::print(device, last_step_expected);
-//     //
-//     // std::cout << "abs_diff to checkpoint example: " << abs_diff << std::endl;
-//     // rlt::free(device, test_buffer);
-//     // rlt::free(device, output);
-//     // return abs_diff < 1e-5 ? 0 : 1;
-// }
 
 static constexpr bool DYNAMIC_ALLOCATION = true;
 static constexpr TI NUM_EPISODES_EVAL = 100;
@@ -108,19 +73,32 @@ int main(){
     EVALUATION_ACTOR_TYPE::Buffer<DYNAMIC_ALLOCATION> eval_buffer;
     rlt::malloc(device, evaluation_actor);
     rlt::malloc(device, eval_buffer);
+    ENVIRONMENT env;
 
     rlt::utils::extrack::Path checkpoint_path;
-    checkpoint_path.experiment = "2025-03-31_21-06-47"; // fails
-    // checkpoint_path.experiment = "2025-04-01_13-43-13"; // good
+    checkpoint_path.experiment = "2025-03-31_21-06-47";
+    // checkpoint_path.experiment = "2025-04-01_13-43-13";
     checkpoint_path.name = "foundation-policy-pre-training";
 
-    std::filesystem::path dynamics_parameters_path = "./src/foundation_policy/dynamics_parameters/";
+
+    // cf like: 203; 139; 334; 31;
+
+    std::filesystem::path dynamics_parameters_path = "./src/foundation_policy/dynamics_parameters_2025-03-31_21-06-47/";
     std::filesystem::path dynamics_parameter_index = "./src/foundation_policy/checkpoints_2025-03-31_21-06-47.txt";
 
 
     std::vector<std::tuple<std::string, ENVIRONMENT::Parameters::Dynamics>> query_dynamics;
 
+    auto permute_rotors_px4_to_cf = [&device, &env](const auto& dynamics){
+        auto copy = dynamics;
+        rlt::permute_rotors(device, env, copy, 0, 3, 1, 2);
+        return copy;
+    };
+
     query_dynamics.emplace_back("crazyflie", rlt::rl::environments::l2f::parameters::dynamics::crazyflie<ENVIRONMENT::SPEC::T, ENVIRONMENT::SPEC::TI>);
+    query_dynamics.emplace_back("x500", permute_rotors_px4_to_cf(rlt::rl::environments::l2f::parameters::dynamics::x500::real<ENVIRONMENT::SPEC::T, ENVIRONMENT::SPEC::TI>));
+    query_dynamics.emplace_back("mrs", permute_rotors_px4_to_cf(rlt::rl::environments::l2f::parameters::dynamics::mrs<ENVIRONMENT::SPEC::T, ENVIRONMENT::SPEC::TI>));
+    query_dynamics.emplace_back("fs", permute_rotors_px4_to_cf(rlt::rl::environments::l2f::parameters::dynamics::fs::base<ENVIRONMENT::SPEC::T, ENVIRONMENT::SPEC::TI>));
 
     std::ifstream dynamics_parameter_index_file(dynamics_parameter_index);
     if (!dynamics_parameter_index_file){
@@ -149,14 +127,20 @@ int main(){
         std::ifstream dynamics_parameter_file = std::ifstream(dynamics_parameters_path / (cpp_copy.attributes["dynamics-id"] + ".json"));
         std::string dynamics_parameter_json((std::istreambuf_iterator<char>(dynamics_parameter_file)), std::istreambuf_iterator<char>());
         dynamics_parameter_file.close();
-        ENVIRONMENT env;
         rlt::from_json(device, env, dynamics_parameter_json, env.parameters);
 
-        rlt::rl::utils::evaluation::Result<rlt::rl::utils::evaluation::Specification<T, TI, ENVIRONMENT, NUM_EPISODES_EVAL, ENVIRONMENT::EPISODE_STEP_LIMIT>> result;
-        rlt::rl::utils::evaluation::NoData<rlt::rl::utils::evaluation::DataSpecification<decltype(result)::SPEC>> no_data;
+        using RESULT_SPEC = rlt::rl::utils::evaluation::Specification<T, TI, ENVIRONMENT, NUM_EPISODES_EVAL, ENVIRONMENT::EPISODE_STEP_LIMIT>;
+        using RESULT = rlt::rl::utils::evaluation::Result<RESULT_SPEC>;
+        RESULT base_result, query_result;
+        rlt::rl::utils::evaluation::NoData<rlt::rl::utils::evaluation::DataSpecification<RESULT_SPEC>> no_data;
         rlt::Mode<rlt::mode::Evaluation<>> mode;
-        rlt::evaluate(device, env, ui, evaluation_actor, result, no_data, rng, mode);
-        std::cout << "Teacher policy " << cpp_copy.checkpoint_path.string() << " mean return: " << result.returns_mean << " episode length: " << result.episode_length_mean << " share terminated: " << result.share_terminated << std::endl;
+        rlt::evaluate(device, env, ui, evaluation_actor, base_result, no_data, rng, mode);
+        std::cout << "Teacher env " << cpp_copy.checkpoint_path.string() << " mean return: " << base_result.returns_mean << " episode length: " << base_result.episode_length_mean << " share terminated: " << base_result.share_terminated << std::endl;
+        for (const auto& [name, dynamics] : query_dynamics){
+            env.parameters.dynamics = dynamics;
+            rlt::evaluate(device, env, ui, evaluation_actor, query_result, no_data, rng, mode);
+            std::cout << "    " << name << " mean return: " << query_result.returns_mean << " episode length: " << query_result.episode_length_mean << " share terminated: " << query_result.share_terminated << std::endl;
+        }
     }
     rlt::free(device, evaluation_actor);
     rlt::free(device, eval_buffer);
