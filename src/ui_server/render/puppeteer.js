ed11421245f88a89b29c543e396a62fbbe774a86
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-05-25 16:25:38 -0400

    rebasing to remove blob

--- src/ui_server/render/puppeteer.js
+++ src/ui_server/render/puppeteer.js
@@ -3,17 +3,18 @@ const fs = require('fs');
 const path = require('path');
 const http = require('http');
 const { parse } = require('url');
+const busboy = require('busboy');
 
 class Renderer{
-    constructor(UI){
-        if(!UI) {
-            this.ui = fs.readFileSync(path.join(__dirname, "ui.js"), 'utf8');
-        }
+    constructor(ui, width, height){
+        this.ui = ui
+        this.width = width
+        this.height = height
     }
     async init(){
         this.browser = await puppeteer.launch({headless: "new", args: ['--no-sandbox', '--disable-setuid-sandbox'], timeout: 0});
         this.page = await this.browser.newPage();
-        await this.page.setViewport({ width: WIDTH, height: HEIGHT });
+        await this.page.setViewport({ width: this.width, height: this.height });
         await this.page.goto(`http://localhost:${PORT}/`);
     }
     async render(parameters, state){
@@ -38,7 +39,8 @@ class Renderer{
         const mean_dt = trajectory.reduce((a, c) => a + c.dt, 0) / trajectory.length;
         const fps = Math.round(1 / mean_dt);
         console.log(`FPS: ${fps}`);
-        return frames
+        const buffers = frames.map(f => Buffer.from(f, 'base64'));
+        return buffers
     }
     async close(){
         await this.browser.close();
@@ -83,26 +85,75 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, { 'Content-Type': 'application/javascript' });
         const UI = fs.readFileSync(path.join(__dirname, "ui.js"), 'utf8');
         res.end(UI);
-    } else if (req.method === 'POST' && req.url === '/render') {
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString();
+    } else if (req.method === 'POST' && (req.url === '/render_trajectory' || req.url === '/render')) {
+        // input: {parameters, trajectory: [{state, action, dt, ...}]}, ui: ui.js
+        const bb = busboy({ headers: req.headers });
+        const files = {};
+        const fields = {};
+        bb.on('file', (name, file, info) => {
+            const { filename, encoding, mimeType } = info;
+            const combined_data = [];
+            console.log(
+                `File [${name}]: filename: %j, encoding: %j, mimeType: %j`,
+                filename,
+                encoding,
+                mimeType
+            );
+            file.on('data', (data) => {
+                console.log(`File [${name}] got ${data.length} bytes`);
+                combined_data.push(data);
+            }).on('close', () => {
+                console.log(`File [${name}] done`);
+                files[name] = Buffer.concat(combined_data);
+            });
         });
-
-        req.on('end', async () => {
+        bb.on('field', (name, val, info) => {
+            console.log(`Field [${name}]: value: %j`, val);
+            fields[name] = val;
+        });
+        bb.on('close', async () => {
+            console.log('Done parsing form!');
             try {
-                const payload = JSON.parse(body);
-                res.writeHead(200, {
-                    'Content-Type': 'image/png',
-                    'Content-Length': buffer.length
-                });
-                res.end(buffer);
+                const payload = JSON.parse(files["data"]);
+                const ui = files["ui"].toString('utf8');
+                const renderer = new Renderer(ui, parseInt(fields["width"]), parseInt(fields["height"]));
+                await renderer.init();
+                let frames = null;
+                if(req.url === '/render'){
+                    const frame = await renderer.render(payload.parameters, payload.step, {frame_counter: false})
+                    await renderer.close();
+
+                    res.writeHead(200, {
+                        'Content-Type': 'image/png',
+                        'Content-Length': frame.length,
+                    });
+                    res.end(frame);
+                }
+                else{
+                    const yazl = require('yazl');
+                    frames = await renderer.render_trajectory(payload.parameters, payload.trajectory, {frame_counter: false})
+                    await renderer.close();
+
+                    const zipfile = new yazl.ZipFile();
+                    frames.forEach((frame, i) => {
+                        const name = `frame_${i.toString().padStart(5, '0')}.png`;
+                        zipfile.addBuffer(frame, name);
+                    });
+
+                    res.writeHead(200, {
+                        'Content-Type': 'application/zip',
+                        'Content-Disposition': 'attachment; filename="frames.zip"'
+                    });
+                    zipfile.outputStream.pipe(res);
+                    zipfile.end();
+                }
             } catch (err) {
                 console.error('Render error:', err);
                 res.writeHead(500);
                 res.end('Failed to render canvas.');
             }
         });
+        req.pipe(bb);
     } else {
         res.writeHead(404);
         res.end('Not Found');
@@ -114,28 +165,25 @@ const server = http.createServer((req, res) => {
 const DATA_CONTENT = fs.readFileSync(path.join(__dirname, "data.json"), 'utf8')
 const DATA = JSON.parse(DATA_CONTENT)
 
-const WIDTH = 2000
-const HEIGHT = 2000
 const PORT = 3010;
 async function main(){
     await new Promise(resolve => server.listen(PORT, resolve));
-    const renderer = new Renderer();
-    await renderer.init();
-    // const buffer = await renderer.render(DATA[0].parameters, DATA[0].trajectory[0])
-    frames = await renderer.render_trajectory(DATA[0].parameters, DATA[0].trajectory, {frame_counter: false})
-    await renderer.close();
+    // const renderer = new Renderer();
+    // await renderer.init();
+    // // const buffer = await renderer.render(DATA[0].parameters, DATA[0].trajectory[0])
+    // frames = await renderer.render_trajectory(DATA[0].parameters, DATA[0].trajectory, {frame_counter: false})
+    // await renderer.close();
 
-    const outputDir = path.join(__dirname, 'canvas_frames');
-    if (!fs.existsSync(outputDir)) {
-        fs.mkdirSync(outputDir, { recursive: true });
-    }
-    const file_promises = frames.map((frame, i) => {
-        const buffer = Buffer.from(frame, 'base64');
-        const outputPath = path.join(outputDir, `frame_${i.toString().padStart(5, '0')}.png`);
-        console.log(`Saved frame ${i} to ${outputPath}`);
-        return fs.promises.writeFile(outputPath, buffer);
-    })
-    await Promise.all(file_promises);
+    // const outputDir = path.join(__dirname, 'canvas_frames');
+    // if (!fs.existsSync(outputDir)) {
+    //     fs.mkdirSync(outputDir, { recursive: true });
+    // }
+    // const file_promises = frames.map((frame, i) => {
+    //     const outputPath = path.join(outputDir, `frame_${i.toString().padStart(5, '0')}.png`);
+    //     console.log(`Saved frame ${i} to ${outputPath}`);
+    //     return fs.promises.writeFile(outputPath, frame);
+    // })
+    // await Promise.all(file_promises);
     // const outputPath = path.join(__dirname, 'output.png');
     // fs.writeFileSync(outputPath, buffer);
     // console.log(`Screenshot saved to ${outputPath}`);
