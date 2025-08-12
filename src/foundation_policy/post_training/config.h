113ce3dbf4ad445ec240a642ef205df98d99d9ec
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-08-12 19:49:00 -0400

    deepsweep client and setting up for dmodel sweep

--- src/foundation_policy/post_training/config.h
+++ src/foundation_policy/post_training/config.h
@@ -16,7 +16,12 @@ constexpr TI N_PRE_TRAINING_SEEDS = 1;
 constexpr TI SEQUENCE_LENGTH = 500;
 constexpr TI BATCH_SIZE = 64;
 constexpr T SOLVED_RETURN = 300;
+#ifdef DMODEL
+#warning "Using DMODEL for hidden dimension"
+constexpr TI HIDDEN_DIM = DMODEL;
+#else
 constexpr TI HIDDEN_DIM = 16;
+#endif
 constexpr TI NUM_TEACHERS = 1000;
 constexpr TI NUM_ACTIVE_TEACHERS = NUM_TEACHERS;
 constexpr TI EPOCH_TEACHER_FORCING = 10;
---
+++ tools/deepsweep/deepsweep-client.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+SEED=$(jq -e ".seed" <<< "$DEEPSWEEP_SPEC")
+DMODEL=$(jq -e ".dmodel" <<< "$DEEPSWEEP_SPEC")
+echo DModel $DMODEL, Seed $SEED
+
+EXPERIMENT=deepsweep_runs/$DEEPSWEEP_JOB/$DMODEL/$SEED
+mkdir -p $EXPERIMENT
+g++ -std=c++17 -O3 -ffast-math -I include -I external/highfive/include -I /opt/homebrew/include -L /opt/homebrew/lib -lhdf5 -framework Accelerate -DRL_TOOLS_ENABLE_JSON -DRL_TOOLS_ENABLE_HDF5 -DRL_TOOLS_BACKEND_ENABLE_ACCELERATE -DDMODEL=$DMODEL src/foundation_policy/post_training/main.cpp -o $EXPERIMENT/a.out
+CMD="MKL_NUM_THREADS=1 OPENBLAS_NUM_THREADS=1 RL_TOOLS_EXTRACK_EXPERIMENT=$JOB_ID RL_TOOLS_RUN_PATH=$EXPERIMENT ./$EXPERIMENT/a.out $SEED"
+echo "Executing: $CMD"
+eval $CMD
