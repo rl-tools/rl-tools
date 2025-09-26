6fdc10a317f5344ede5777d691a1a53e073b8e0d
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-09-26 16:38:27 -0700

    autoconfig

---
+++ cmake/tier3-tensorboard.cmake
@@ -0,0 +1,10 @@
+if(RL_TOOLS_ENABLE_TENSORBOARD)
+    FetchContent_Declare(tensorboard
+            GIT_REPOSITORY https://github.com/rl-tools/tensorboard_logger.git
+            GIT_TAG   9761224e07c34f672523808c3f688651e2822cbc
+            GIT_SHALLOW    TRUE
+    )
+    FetchContent_MakeAvailable(tensorboard)
+    target_link_libraries(rl_tools_full INTERFACE tensorboard_logger)
+    target_compile_definitions(rl_tools_full INTERFACE RL_TOOLS_ENABLE_TENSORBOARD)
+endif()
