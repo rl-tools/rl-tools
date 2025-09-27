f2b332292a7caae5cb202fb5e8c93c55dcc91a2d
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-09-26 18:36:08 -0700

    adaptive hdf5 handling

--- cmake/tier2-json-hdf5.cmake
+++ cmake/tier2-json-hdf5.cmake
@@ -19,20 +19,21 @@ endif()
 
 if(NOT RL_TOOLS_DISABLE_HDF5)
     find_package(HDF5)
-    if(NOT HDF5_FOUND)
-        message(FATAL_ERROR "HDF5 not found. Please install HDF5 or disable HDF5 support by setting RL_TOOLS_DISABLE_HDF5=ON")
+    if(HDF5_FOUND)
+        message(STATUS "Found existing/system HDF5 ${HDF5_VERSION} at ${HDF5_INCLUDE_DIRS}")
+        target_link_libraries(rl_tools_full INTERFACE HDF5::HDF5)
+        message(STATUS "Using FetchContent to get HighFive")
+        FetchContent_Declare(highfive
+                GIT_REPOSITORY https://github.com/rl-tools/highfive.git
+                GIT_TAG   be68bd0efcef338a016fba448d6444089fd196d5
+        )
+        set(HIGHFIVE_USE_BOOST OFF CACHE BOOL "Disable Boost usage in HighFive" FORCE)
+        set(HIGHFIVE_EXAMPLES OFF CACHE BOOL "Disable HighFive examples" FORCE)
+        set(HIGHFIVE_UNIT_TESTS OFF CACHE BOOL "Disable HighFive tests" FORCE)
+        FetchContent_MakeAvailable(highfive)
+        target_include_directories(rl_tools_full INTERFACE ${highfive_SOURCE_DIR}/include)
+        target_compile_definitions(rl_tools_full INTERFACE RL_TOOLS_ENABLE_HDF5)
+    else()
+        message(WARNING "HDF5 not found. HDF5 is disabled. Suppress this warning by setting RL_TOOLS_DISABLE_HDF5=ON")
     endif()
-    message(STATUS "Found existing/system HDF5 ${HDF5_VERSION} at ${HDF5_INCLUDE_DIRS}")
-    target_link_libraries(rl_tools_full INTERFACE HDF5::HDF5)
-    message(STATUS "Using FetchContent to get HighFive")
-    FetchContent_Declare(highfive
-            GIT_REPOSITORY https://github.com/rl-tools/highfive.git
-            GIT_TAG   be68bd0efcef338a016fba448d6444089fd196d5
-    )
-    set(HIGHFIVE_USE_BOOST OFF CACHE BOOL "Disable Boost usage in HighFive" FORCE)
-    set(HIGHFIVE_EXAMPLES OFF CACHE BOOL "Disable HighFive examples" FORCE)
-    set(HIGHFIVE_UNIT_TESTS OFF CACHE BOOL "Disable HighFive tests" FORCE)
-    FetchContent_MakeAvailable(highfive)
-    target_include_directories(rl_tools_full INTERFACE ${highfive_SOURCE_DIR}/include)
-    target_compile_definitions(rl_tools_full INTERFACE RL_TOOLS_ENABLE_HDF5)
 endif()
