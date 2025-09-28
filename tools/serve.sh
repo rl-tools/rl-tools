d003cac2179405b5b734cb1131697f1e6ce96e00
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-09-27 21:16:13 -0700

    index html and upgrading extrack

--- tools/serve.sh
+++ tools/serve.sh
@@ -1,2 +1,5 @@
 # serve the main folder
-python3 -m http.server -d ..
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PARENT_DIR="$(dirname "$SCRIPT_DIR")"
+bash -c "while true; do $SCRIPT_DIR/index_experiments.sh $PARENT_DIR/experiments; sleep 10; done" &
+python3 -m http.server -d "$PARENT_DIR"
