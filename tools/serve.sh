8cc5b8bdeab95e92f979d20ef158614e83baa4f4
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-09-29 13:07:39 -0700

    fix js deps in serve.sh

--- tools/serve.sh
+++ tools/serve.sh
@@ -3,5 +3,6 @@ set -e
 (cd )
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PARENT_DIR="$(dirname "$SCRIPT_DIR")"
+bash $PARENT_DIR/static/extrack_ui/download_dependencies.sh
 bash -c "while true; do $SCRIPT_DIR/index_experiments.sh $PARENT_DIR/experiments; sleep 10; done" &
 python3 -m http.server -d "$PARENT_DIR"
