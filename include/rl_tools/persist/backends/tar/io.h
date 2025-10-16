0113d3660d12acad5f5ca03bc8ea33e3d0a7591a
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-10-16 15:00:22 -0700

    improving

--- include/rl_tools/persist/backends/tar/io.h
+++ include/rl_tools/persist/backends/tar/io.h
@@ -8,6 +8,31 @@ namespace rl_tools::persist::backends::tar {
     struct Writer{
         std::vector<char> buffer;
     };
+    template <typename T_TI, typename T_WRITER>
+    struct WriterGroupSpecification{
+        using TI = T_TI;
+        using WRITER = T_WRITER;
+        static constexpr TI MAX_PATH_LENGTH = 100;
+    };
+    template <typename SPEC>
+    struct WriterGroup{
+        using TI = typename SPEC::TI;
+        using WRITER = typename SPEC::WRITER;
+        char path[SPEC::MAX_PATH_LENGTH] = "";
+        WRITER writer;
+    };
+    template <typename T_TI>
+    struct ReaderGroupSpecification{
+        using TI = T_TI;
+        static constexpr TI MAX_PATH_LENGTH = 100;
+    };
+    template <typename SPEC>
+    struct ReaderGroup{
+        using TI = typename SPEC::TI;
+        char path[SPEC::MAX_PATH_LENGTH] = "";
+        const char* data = nullptr;
+        TI size = 0;
+    };
     struct Reader{
 
     };
--- include/rl_tools/persist/backends/tar/operations_generic.h
+++ include/rl_tools/persist/backends/tar/operations_generic.h
@@ -34,12 +34,15 @@ namespace rl_tools{
             return len;
         }
         template <typename TI>
-        void strncpy(char* dest, const char* src, TI n) {
+        TI strncpy(char* dest, const char* src, TI n) {
             for (TI i = 0; i < n; i++){
                 dest[i] = src[i];
-                if (src[i] == '\0') return;
+                if (src[i] == '\0'){
+                    return i;
+                }
             }
             dest[n - 1] = '\0';
+            return n-1;
         }
         template <typename TI>
         bool strcmp(const char* a, const char* b, TI n) {
@@ -111,6 +114,23 @@ namespace rl_tools{
             
             return result;
         }
+        template <typename TI>
+        bool seek_in_metadata(const char* metadata, TI metadata_size, const char* key, TI& position, TI& value_len) {
+            TI key_len = strlen<TI, 100>(key);
+            bool previous_was_newline = true;
+            for (position = 0; position < metadata_size; position++){
+                if (previous_was_newline && strcmp<TI>(metadata + position, key, key_len) && metadata[position + key_len] == ':'){
+                    position += key_len + 2;
+                    value_len = 0;
+                    while (position + value_len < metadata_size && metadata[position+value_len] != '\n'){
+                        value_len++;
+                    }
+                    return true;
+                }
+                previous_was_newline = (metadata[position] == '\n');
+            }
+            return -1; // Key not found
+        }
         template <typename DEVICE, typename WRITER, typename TI>
         void write_entry(DEVICE& device, WRITER& writer, const char* entry_name, const char* data, TI data_size) {
             constexpr TI MAX_LEN = 100;
@@ -156,7 +176,7 @@ namespace rl_tools{
         }
 
         template <typename DEVICE>
-        bool get(DEVICE& device, const char* tar_data, typename DEVICE::index_t length, const char* entry_name, char* output_data, typename DEVICE::index_t output_size) {
+        bool get(DEVICE& device, const char* tar_data, typename DEVICE::index_t length, const char* entry_name, char* output_data, typename DEVICE::index_t output_size, typename DEVICE::index_t& read_size) {
             using TI = typename DEVICE::index_t;
             char* ptr = const_cast<char*>(tar_data);
             while (ptr <= tar_data + length - BLOCK_SIZE<TI>) {
@@ -170,15 +190,15 @@ namespace rl_tools{
 
                 utils::assert_exit(device, strcmp(h->magic, "ustar", 5), "Warning: Not a UStar format archive or header is corrupted.");
 
-                TI size = parse_octal<TI>(h->size, 12);
+                read_size = parse_octal<TI>(h->size, 12);
                 if (strcmp(h->name, entry_name, 100)){
-                    utils::assert_exit(device, size <= output_size, "persist::backends::tar: Output buffer is too small for the requested entry");
-                    memcpy<TI>(output_data, ptr, size);
+                    utils::assert_exit(device, read_size <= output_size, "persist::backends::tar: Output buffer is too small for the requested entry");
+                    memcpy<TI>(output_data, ptr, read_size);
                     return true;
                 }
-                ptr += size;
+                ptr += read_size;
 
-                size_t padding_size = (BLOCK_SIZE<TI> - (size % BLOCK_SIZE<TI>)) % BLOCK_SIZE<TI>;
+                size_t padding_size = (BLOCK_SIZE<TI> - (read_size % BLOCK_SIZE<TI>)) % BLOCK_SIZE<TI>;
                 if (padding_size > 0) {
                     ptr += padding_size;
                 }
@@ -187,6 +207,79 @@ namespace rl_tools{
         }
     }
 
+    namespace containers::tensor{
+        template <typename SPEC, typename TI = typename SPEC::TI, TI METADATA_SIZE, TI DIM = 0>
+        void dim_helper(char* metadata, TI& metadata_position){
+            if constexpr(DIM < SPEC::SHAPE::LENGTH){
+                std::string dim_key = "dim_" + std::to_string(DIM) + ": ";
+                metadata_position += persist::backends::tar::strncpy(metadata + metadata_position, dim_key.c_str(), METADATA_SIZE - metadata_position);
+                metadata_position += persist::backends::tar::strncpy<TI>(metadata + metadata_position, std::to_string(SPEC::SHAPE::template GET<DIM>).c_str(), METADATA_SIZE - metadata_position);
+                metadata_position += persist::backends::tar::strncpy(metadata + metadata_position, "\n", METADATA_SIZE - metadata_position);
+                dim_helper<SPEC, TI, METADATA_SIZE, DIM + 1>(metadata, metadata_position);
+            }
+        }
+
+    }
+
+    template<typename DEVICE, typename SPEC, typename GROUP_SPEC>
+    void load(DEVICE& device, Tensor<SPEC>& tensor, persist::backends::tar::ReaderGroup<GROUP_SPEC>& group, const char* name) {
+        using TI = typename DEVICE::index_t;
+        char group_path[GROUP_SPEC::MAX_PATH_LENGTH];
+        persist::backends::tar::strncpy<TI>(group_path, group.path, GROUP_SPEC::MAX_PATH_LENGTH);
+        TI group_path_length = persist::backends::tar::strlen<TI, GROUP_SPEC::MAX_PATH_LENGTH+1>(group_path);
+        TI name_length = persist::backends::tar::strlen<TI, GROUP_SPEC::MAX_PATH_LENGTH+1>(name);
+        utils::assert_exit(device, group_path_length + 1 + name_length < GROUP_SPEC::MAX_PATH_LENGTH, "persist::backends::tar: Group path and name exceed maximum length");
+        TI current_position = group_path_length;
+        if (group_path_length > 0){
+            group_path[group_path_length] = '/';
+            current_position += 1;
+        }
+        persist::backends::tar::strncpy<TI>(group_path + current_position, name, GROUP_SPEC::MAX_PATH_LENGTH - group_path_length - 1);
+        std::cout << "Reading tensor from tar entry: " << group_path << std::endl;
+        constexpr TI METADATA_SIZE = 100;
+        char metadata[METADATA_SIZE];
+        TI read_size = 0;
+        utils::assert_exit(device, persist::backends::tar::get(device, group.data, group.size, "meta", metadata, METADATA_SIZE, read_size), "persist::backends::tar: Failed to read metadata entry from tar archive");
+        metadata[read_size] = '\0';
+        TI type_position = 0;
+        TI type_value_length = 0;
+        utils::assert_exit(device, rl_tools::persist::backends::tar::seek_in_metadata(metadata, METADATA_SIZE, "type", type_position, type_value_length), "persist::backends::tar: 'type' not found in metadata");
+        char value[20];
+        persist::backends::tar::strncpy<TI>(value, metadata + type_position, std::min<TI>(type_value_length + 1, 20));
+        std::cout << "type: " << value << std::endl;
+    }
+
+    template<typename DEVICE, typename SPEC, typename GROUP_SPEC>
+    void save(DEVICE& device, Tensor<SPEC>& tensor, persist::backends::tar::WriterGroup<GROUP_SPEC>& group, const char* name) {
+        using TI = typename DEVICE::index_t;
+        char group_path[GROUP_SPEC::MAX_PATH_LENGTH];
+        persist::backends::tar::strncpy<TI>(group_path, group.path, GROUP_SPEC::MAX_PATH_LENGTH);
+        TI group_path_length = persist::backends::tar::strlen<TI, GROUP_SPEC::MAX_PATH_LENGTH+1>(group_path);
+        TI name_length = persist::backends::tar::strlen<TI, GROUP_SPEC::MAX_PATH_LENGTH+1>(name);
+        utils::assert_exit(device, group_path_length + 1 + name_length < GROUP_SPEC::MAX_PATH_LENGTH, "persist::backends::tar: Group path and name exceed maximum length");
+        TI current_position = group_path_length;
+        if (group_path_length > 0){
+            group_path[group_path_length] = '/';
+            current_position += 1;
+        }
+        persist::backends::tar::strncpy<TI>(group_path + current_position, name, GROUP_SPEC::MAX_PATH_LENGTH - group_path_length - 1);
+        std::cout << "Saving tensor to tar entry: " << group_path << std::endl;
+        constexpr TI METADATA_SIZE = 100;
+        char metadata[METADATA_SIZE];
+        TI metadata_position = 0;
+        metadata_position += persist::backends::tar::strncpy<TI>(metadata, "type: tensor\n", METADATA_SIZE - metadata_position);
+        metadata_position += persist::backends::tar::strncpy(metadata + metadata_position, "num_dims: ", METADATA_SIZE - metadata_position);
+        metadata_position += persist::backends::tar::strncpy<TI>(metadata + metadata_position, std::to_string(SPEC::SHAPE::LENGTH).c_str(), METADATA_SIZE - metadata_position);
+        metadata_position += persist::backends::tar::strncpy(metadata + metadata_position, "\n", METADATA_SIZE - metadata_position);
+        containers::tensor::dim_helper<SPEC, TI, METADATA_SIZE>(metadata, metadata_position);
+        std::cout << "metadata: \n" << metadata << std::endl;
+        write_entry(device, group.writer, "meta", metadata, metadata_position);
+        Tensor<tensor::Specification<typename SPEC::T, typename SPEC::TI, typename SPEC::SHAPE>> tensor_dense;
+        malloc(device, tensor_dense);
+        copy(device, device, tensor, tensor_dense);
+        write_entry(device, group.writer, "data", reinterpret_cast<const char*>(data(tensor_dense)), SPEC::SIZE_BYTES);
+        free(device, tensor_dense);
+    }
 
 }
 RL_TOOLS_NAMESPACE_WRAPPER_END
