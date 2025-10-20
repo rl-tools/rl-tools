976a9294b28a6552c8a6438c271ef5f0f96ab20b
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-10-20 15:22:12 -0700

    td3 l2f does something

--- src/rl/zoo/l2f/td3.h
+++ src/rl/zoo/l2f/td3.h
@@ -6,18 +6,12 @@
 
 namespace rl_tools::rl::zoo::l2f::td3{
     namespace rlt = rl_tools;
-    template <typename DEVICE, typename T, typename TI, typename RNG, bool DYNAMIC_ALLOCATION>
+    template <typename DEVICE, typename TYPE_POLICY, typename TI, typename RNG, bool DYNAMIC_ALLOCATION>
     struct FACTORY{
-        struct OPTIONS{
-            static constexpr bool SEQUETIAL_MODEL = false;
-            static constexpr bool MOTOR_DELAY = false;
-            static constexpr bool RANDOMIZE_MOTOR_MAPPING = true;
-            static constexpr bool RANDOMIZE_THRUST_CURVES = true;
-            static constexpr bool OBSERVE_THRASH_MARKOV = true;
-        };
-        using ENVIRONMENT = typename ENVIRONMENT_FACTORY<T, T, TI>::ENVIRONMENT;
-        struct LOOP_CORE_PARAMETERS: rlt::rl::algorithms::td3::loop::core::DefaultParameters<T, TI, ENVIRONMENT>{
-            struct TD3_PARAMETERS: rlt::rl::algorithms::td3::DefaultParameters<T, TI>{
+        using T = typename TYPE_POLICY::DEFAULT;
+        using ENVIRONMENT = typename ENVIRONMENT_TINY_FACTORY<DEVICE, TYPE_POLICY, TI>::ENVIRONMENT;
+        struct LOOP_CORE_PARAMETERS: rlt::rl::algorithms::td3::loop::core::DefaultParameters<TYPE_POLICY, TI, ENVIRONMENT>{
+            struct TD3_PARAMETERS: rlt::rl::algorithms::td3::DefaultParameters<TYPE_POLICY, TI>{
                 static constexpr TI ACTOR_BATCH_SIZE = 256;
                 static constexpr TI CRITIC_BATCH_SIZE = 256;
                 static constexpr TI TRAINING_INTERVAL = 16;
@@ -31,18 +25,18 @@ namespace rl_tools::rl::zoo::l2f::td3{
                 static constexpr bool IGNORE_TERMINATION = false;
             };
             static constexpr T EXPLORATION_NOISE = 0.3;
-            static constexpr TI STEP_LIMIT = 4000000;
+            static constexpr TI STEP_LIMIT = 400000;
             static constexpr TI REPLAY_BUFFER_CAP = STEP_LIMIT;
             static constexpr TI ACTOR_NUM_LAYERS = 3;
-            static constexpr TI ACTOR_HIDDEN_DIM = 256;
+            static constexpr TI ACTOR_HIDDEN_DIM = 32;
             static constexpr auto ACTOR_ACTIVATION_FUNCTION = nn::activation_functions::ActivationFunction::RELU;
             static constexpr TI CRITIC_NUM_LAYERS = 3;
-            static constexpr TI CRITIC_HIDDEN_DIM = 256;
+            static constexpr TI CRITIC_HIDDEN_DIM = 32;
             static constexpr auto CRITIC_ACTIVATION_FUNCTION = nn::activation_functions::ActivationFunction::RELU;
             static constexpr TI EPISODE_STEP_LIMIT = 500;
             static constexpr TI N_WARMUP_STEPS = 10000;
 //            static constexpr bool SHARED_BATCH = false;
-            struct OPTIMIZER_PARAMETERS: rlt::nn::optimizers::adam::DEFAULT_PARAMETERS_TENSORFLOW<T>{
+            struct OPTIMIZER_PARAMETERS: rlt::nn::optimizers::adam::DEFAULT_PARAMETERS_TENSORFLOW<TYPE_POLICY>{
                 static constexpr T ALPHA = 3e-4;
                 static constexpr bool ENABLE_GRADIENT_CLIPPING = false;
                 static constexpr T GRADIENT_CLIP_VALUE = 1;
@@ -51,6 +45,6 @@ namespace rl_tools::rl::zoo::l2f::td3{
             };
 
         };
-        using LOOP_CORE_CONFIG = rlt::rl::algorithms::td3::loop::core::Config<T, TI, RNG, ENVIRONMENT, LOOP_CORE_PARAMETERS, rlt::rl::algorithms::td3::loop::core::ConfigApproximatorsMLP, DYNAMIC_ALLOCATION>;
+        using LOOP_CORE_CONFIG = rlt::rl::algorithms::td3::loop::core::Config<TYPE_POLICY, TI, RNG, ENVIRONMENT, LOOP_CORE_PARAMETERS, rlt::rl::algorithms::td3::loop::core::ConfigApproximatorsMLP, DYNAMIC_ALLOCATION>;
     };
 }
