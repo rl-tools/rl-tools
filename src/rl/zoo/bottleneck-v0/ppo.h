27e929f63cd6279a18d5c44e267fecf36b384f06
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-10-20 15:33:39 -0700

    zoo bottleneck compiles

--- src/rl/zoo/bottleneck-v0/ppo.h
+++ src/rl/zoo/bottleneck-v0/ppo.h
@@ -6,10 +6,11 @@
 RL_TOOLS_NAMESPACE_WRAPPER_START
 namespace rl_tools::rl::zoo::bottleneck_v0::ppo{
     namespace rlt = rl_tools;
-    template <typename DEVICE, typename T, typename TI, typename RNG, bool DYNAMIC_ALLOCATION>
+    template <typename DEVICE, typename TYPE_POLICY, typename TI, typename RNG, bool DYNAMIC_ALLOCATION>
     struct FACTORY{
-        using ENVIRONMENT = typename ENVIRONMENT_FACTORY<DEVICE, T, TI>::ENVIRONMENT;
-        struct LOOP_CORE_PARAMETERS: rlt::rl::algorithms::ppo::loop::core::DefaultParameters<T, TI, ENVIRONMENT>{
+        using T = typename TYPE_POLICY::DEFAULT;
+        using ENVIRONMENT = typename ENVIRONMENT_FACTORY<DEVICE, TYPE_POLICY, TI>::ENVIRONMENT;
+        struct LOOP_CORE_PARAMETERS: rlt::rl::algorithms::ppo::loop::core::DefaultParameters<TYPE_POLICY, TI, ENVIRONMENT>{
             static constexpr TI STEP_LIMIT = 5000; // 1024 * 4 * 74 ~ 300k steps
 
             static constexpr TI ACTOR_HIDDEN_DIM = 64;
@@ -22,11 +23,11 @@ namespace rl_tools::rl::zoo::bottleneck_v0::ppo{
             static constexpr TI N_ENVIRONMENTS = 128;
             static constexpr TI ON_POLICY_RUNNER_STEPS_PER_ENV = 128;
             static constexpr TI BATCH_SIZE = 1024;
-            struct OPTIMIZER_PARAMETERS: nn::optimizers::adam::DEFAULT_PARAMETERS_TENSORFLOW<T>{
+            struct OPTIMIZER_PARAMETERS: nn::optimizers::adam::DEFAULT_PARAMETERS_TENSORFLOW<TYPE_POLICY>{
                 static constexpr T ALPHA = 1e-3;
             };
             static constexpr bool NORMALIZE_OBSERVATIONS = true;
-            struct PPO_PARAMETERS: rl::algorithms::ppo::DefaultParameters<T, TI, BATCH_SIZE>{
+            struct PPO_PARAMETERS: rl::algorithms::ppo::DefaultParameters<TYPE_POLICY, TI, BATCH_SIZE>{
                 static constexpr T GAMMA = 0.98;
                 static constexpr T ACTION_ENTROPY_COEFFICIENT = 0.01;
                 static constexpr TI N_EPOCHS = 1;
@@ -34,7 +35,7 @@ namespace rl_tools::rl::zoo::bottleneck_v0::ppo{
 //                static constexpr bool ADAPTIVE_LEARNING_RATE = true;
             };
         };
-        using LOOP_CORE_CONFIG = rlt::rl::algorithms::ppo::loop::core::Config<T, TI, RNG, ENVIRONMENT, LOOP_CORE_PARAMETERS, rlt::rl::algorithms::ppo::loop::core::ConfigApproximatorsSequentialMultiAgent, DYNAMIC_ALLOCATION>;
+        using LOOP_CORE_CONFIG = rlt::rl::algorithms::ppo::loop::core::Config<TYPE_POLICY, TI, RNG, ENVIRONMENT, LOOP_CORE_PARAMETERS, rlt::rl::algorithms::ppo::loop::core::ConfigApproximatorsSequentialMultiAgent, DYNAMIC_ALLOCATION>;
         template <typename BASE>
         struct LOOP_EVALUATION_PARAMETER_OVERWRITES: BASE{
 //            static constexpr TI EPISODE_STEP_LIMIT = 20 / ENVIRONMENT_PARAMETERS::DT;
