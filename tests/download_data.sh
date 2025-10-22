3f441b0027e9480c6875d013e4505a62f910a96e
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-10-22 13:21:42 -0700

    bump data reference (partially fixing CI?)

--- tests/download_data.sh
+++ tests/download_data.sh
@@ -1,4 +1,4 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 git clone -b master https://huggingface.co/datasets/rl-tools/test-data $SCRIPT_DIR/data
 cd $SCRIPT_DIR/data
-git checkout ba81c1f298c68c8c5bfe6b61a7017a0b482f1c45
+git checkout ea997d044860f67e852ed92217be95fde00be165
