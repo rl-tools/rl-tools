581cd8cd91e975fe1eab43ad39a7194e22bc61e6
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-11-12 13:26:04 -0800

    port checkpoint: both namespaces work

--- src/port_checkpoint/main.cpp
+++ src/port_checkpoint/main.cpp
@@ -7,34 +7,34 @@ constexpr unsigned ACTION_DIM = 4;
 constexpr unsigned SEED = 0;
 constexpr bool DYNAMIC_ALLOCATION = true;
 
-// #define RL_TOOLS_NAMESPACE_WRAPPER base
-// #include "rl_tools/base/include/rl_tools/operations/cpu.h"
-// #include "rl_tools/base/include/rl_tools/nn/layers/gru/operations_generic.h"
-// #include "rl_tools/base/include/rl_tools/nn/layers/dense/operations_generic.h"
-// #include "rl_tools/base/include/rl_tools/nn_models/sequential/operations_generic.h"
-//
-// #include "rl_tools/base/include/rl_tools/nn/layers/gru/persist.h"
-// #include "rl_tools/base/include/rl_tools/nn/layers/dense/persist.h"
-// #include "rl_tools/base/include/rl_tools/nn_models/sequential/persist.h"
-//
-// namespace base{
-//     namespace rlt = rl_tools;
-//     using DEVICE = rl_tools::devices::DefaultCPU;
-//     using TI = typename DEVICE::index_t;
-//     template <typename T_CONTENT, typename T_NEXT_MODULE = rlt::nn_models::sequential::OutputModule>
-//     using Module = typename rlt::nn_models::sequential::Module<T_CONTENT, T_NEXT_MODULE>;
-//
-//     using INPUT_LAYER_CONFIG = rlt::nn::layers::dense::Configuration<T, TI, HIDDEN_DIM, rlt::nn::activation_functions::ActivationFunction::RELU, rlt::nn::layers::dense::DefaultInitializer<T, TI>, rlt::nn::parameters::groups::Input>;
-//     using INPUT_LAYER = rlt::nn::layers::dense::BindConfiguration<INPUT_LAYER_CONFIG>;
-//     using GRU_CONFIG = rlt::nn::layers::gru::Configuration<T, TI, HIDDEN_DIM, rlt::nn::parameters::groups::Normal>;
-//     using GRU = rlt::nn::layers::gru::BindConfiguration<GRU_CONFIG>;
-//     using OUTPUT_LAYER_CONFIG = rlt::nn::layers::dense::Configuration<T, TI, ACTION_DIM, rlt::nn::activation_functions::ActivationFunction::IDENTITY, rlt::nn::layers::dense::DefaultInitializer<T, TI>, rlt::nn::parameters::groups::Output>;
-//     using OUTPUT_LAYER = rlt::nn::layers::dense::BindConfiguration<OUTPUT_LAYER_CONFIG>;
-//     using MODULE_CHAIN = Module<INPUT_LAYER, Module<GRU, Module<OUTPUT_LAYER>>>;
-//     using CAPABILITY = rlt::nn::capability::Forward<DYNAMIC_ALLOCATION>;
-//     using INPUT_SHAPE = rlt::tensor::Shape<TI, SEQUENCE_LENGTH, BATCH_SIZE, INPUT_DIM>;
-//     using POLICY = rlt::nn_models::sequential::Build<CAPABILITY, MODULE_CHAIN, INPUT_SHAPE>;
-// }
+#define RL_TOOLS_NAMESPACE_WRAPPER base
+#include "rl_tools/base/include/rl_tools/operations/cpu.h"
+#include "rl_tools/base/include/rl_tools/nn/layers/gru/operations_generic.h"
+#include "rl_tools/base/include/rl_tools/nn/layers/dense/operations_generic.h"
+#include "rl_tools/base/include/rl_tools/nn_models/sequential/operations_generic.h"
+
+#include "rl_tools/base/include/rl_tools/nn/layers/gru/persist.h"
+#include "rl_tools/base/include/rl_tools/nn/layers/dense/persist.h"
+#include "rl_tools/base/include/rl_tools/nn_models/sequential/persist.h"
+
+namespace base{
+    namespace rlt = rl_tools;
+    using DEVICE = rl_tools::devices::DefaultCPU;
+    using TI = typename DEVICE::index_t;
+    template <typename T_CONTENT, typename T_NEXT_MODULE = rlt::nn_models::sequential::OutputModule>
+    using Module = typename rlt::nn_models::sequential::Module<T_CONTENT, T_NEXT_MODULE>;
+
+    using INPUT_LAYER_CONFIG = rlt::nn::layers::dense::Configuration<T, TI, HIDDEN_DIM, rlt::nn::activation_functions::ActivationFunction::RELU, rlt::nn::layers::dense::DefaultInitializer<T, TI>, rlt::nn::parameters::groups::Input>;
+    using INPUT_LAYER = rlt::nn::layers::dense::BindConfiguration<INPUT_LAYER_CONFIG>;
+    using GRU_CONFIG = rlt::nn::layers::gru::Configuration<T, TI, HIDDEN_DIM, rlt::nn::parameters::groups::Normal>;
+    using GRU = rlt::nn::layers::gru::BindConfiguration<GRU_CONFIG>;
+    using OUTPUT_LAYER_CONFIG = rlt::nn::layers::dense::Configuration<T, TI, ACTION_DIM, rlt::nn::activation_functions::ActivationFunction::IDENTITY, rlt::nn::layers::dense::DefaultInitializer<T, TI>, rlt::nn::parameters::groups::Output>;
+    using OUTPUT_LAYER = rlt::nn::layers::dense::BindConfiguration<OUTPUT_LAYER_CONFIG>;
+    using MODULE_CHAIN = Module<INPUT_LAYER, Module<GRU, Module<OUTPUT_LAYER>>>;
+    using CAPABILITY = rlt::nn::capability::Forward<DYNAMIC_ALLOCATION>;
+    using INPUT_SHAPE = rlt::tensor::Shape<TI, SEQUENCE_LENGTH, BATCH_SIZE, INPUT_DIM>;
+    using POLICY = rlt::nn_models::sequential::Build<CAPABILITY, MODULE_CHAIN, INPUT_SHAPE>;
+}
 
 #undef RL_TOOLS_NAMESPACE_WRAPPER
 #define RL_TOOLS_NAMESPACE_WRAPPER target
@@ -76,24 +76,24 @@ namespace target{
 
 
 int main(){
-    // base::DEVICE base_device;
-    // base::DEVICE::SPEC::RANDOM::ENGINE<> base_rng;
-    // base::POLICY base_policy;
-    // base::POLICY::Buffer<DYNAMIC_ALLOCATION> base_policy_buffer;
-    // base::rlt::Tensor<base::rlt::tensor::Specification<T, base::TI, typename base::POLICY::INPUT_SHAPE>> base_input;
-    // base::rlt::Tensor<base::rlt::tensor::Specification<T, base::TI, typename base::POLICY::OUTPUT_SHAPE>> base_output;
-    // base::rlt::malloc(base_device);
-    // base::rlt::malloc(base_device, base_rng);
-    // base::rlt::malloc(base_device, base_policy);
-    // base::rlt::malloc(base_device, base_policy_buffer);
-    // base::rlt::malloc(base_device, base_input);
-    // base::rlt::malloc(base_device, base_output);
-    // base::rlt::init(base_device);
-    // base::rlt::init(base_device, base_rng, SEED);
-    // base::rlt::init_weights(base_device, base_policy, base_rng);
-    // base::rlt::randn(base_device, base_input, base_rng);
-    // base::rlt::evaluate(base_device, base_policy, base_input, base_output, base_policy_buffer, base_rng);
-    // base::rlt::print(base_device, base_output);
+    base::DEVICE base_device;
+    base::DEVICE::SPEC::RANDOM::ENGINE<> base_rng;
+    base::POLICY base_policy;
+    base::POLICY::Buffer<DYNAMIC_ALLOCATION> base_policy_buffer;
+    base::rlt::Tensor<base::rlt::tensor::Specification<T, base::TI, typename base::POLICY::INPUT_SHAPE>> base_input;
+    base::rlt::Tensor<base::rlt::tensor::Specification<T, base::TI, typename base::POLICY::OUTPUT_SHAPE>> base_output;
+    base::rlt::malloc(base_device);
+    base::rlt::malloc(base_device, base_rng);
+    base::rlt::malloc(base_device, base_policy);
+    base::rlt::malloc(base_device, base_policy_buffer);
+    base::rlt::malloc(base_device, base_input);
+    base::rlt::malloc(base_device, base_output);
+    base::rlt::init(base_device);
+    base::rlt::init(base_device, base_rng, SEED);
+    base::rlt::init_weights(base_device, base_policy, base_rng);
+    base::rlt::randn(base_device, base_input, base_rng);
+    base::rlt::evaluate(base_device, base_policy, base_input, base_output, base_policy_buffer, base_rng);
+    base::rlt::print(base_device, base_output);
 
     target::DEVICE target_device;
     target::DEVICE::SPEC::RANDOM::ENGINE<> target_rng;
