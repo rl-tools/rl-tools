c0a98a6c52335bc3058d6367c0b4321029121f90
Jonas Eschmann <jonas.eschmann@gmail.com>
2026-01-20 01:00:50 +0000

    using protbuf config mode

--- cmake/autodetect/tier3-tensorboard.cmake
+++ cmake/autodetect/tier3-tensorboard.cmake
@@ -1,10 +1,10 @@
 if(NOT RL_TOOLS_DISABLE_TENSORBOARD)
-    find_package(Protobuf QUIET)
+    find_package(Protobuf CONFIG QUIET)
     find_package(Git QUIET)
     if(Protobuf_FOUND AND Protobuf_PROTOC_EXECUTABLE AND GIT_FOUND)
         FetchContent_Declare(tensorboard
                 GIT_REPOSITORY https://github.com/rl-tools/tensorboard_logger.git
-                GIT_TAG   d57f9887d2df19db6923b76f73fa6c06f2ecb3b3
+                GIT_TAG   412af82de07ddc9cdb9df77fed90263adac93067
         )
         FetchContent_MakeAvailable(tensorboard)
         target_link_libraries(rl_tools_full INTERFACE tensorboard_logger)
