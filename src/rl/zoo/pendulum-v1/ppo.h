a0aef476e20f54cfbde9b75beda90bb8b7f671bf
Jonas Eschmann <jonas.eschmann@gmail.com>
2026-01-21 17:17:05 -0800

    ppo pendulum params

--- src/rl/zoo/pendulum-v1/ppo.h
+++ src/rl/zoo/pendulum-v1/ppo.h
@@ -10,10 +10,10 @@ namespace rl_tools::rl::zoo::pendulum_v1::ppo{
         using ENVIRONMENT = typename ENVIRONMENT_FACTORY<DEVICE, TYPE_POLICY, TI>::ENVIRONMENT;
         struct LOOP_CORE_PARAMETERS: rlt::rl::algorithms::ppo::loop::core::DefaultParameters<TYPE_POLICY, TI, ENVIRONMENT>{
             using T = typename TYPE_POLICY::DEFAULT;
-            static constexpr TI N_ENVIRONMENTS = 8;
+            static constexpr TI N_ENVIRONMENTS = 64;
             static constexpr TI ON_POLICY_RUNNER_STEPS_PER_ENV = 128;
-            static constexpr TI BATCH_SIZE = 128;
-            static constexpr TI TOTAL_STEP_LIMIT = 1000000;
+            static constexpr TI BATCH_SIZE = 1024;
+            static constexpr TI TOTAL_STEP_LIMIT = 10000000;
             static constexpr TI ACTOR_HIDDEN_DIM = 32;
             static constexpr TI CRITIC_HIDDEN_DIM = 32;
             static constexpr auto ACTOR_ACTIVATION_FUNCTION = rlt::nn::activation_functions::ActivationFunction::FAST_TANH;
@@ -25,7 +25,7 @@ namespace rl_tools::rl::zoo::pendulum_v1::ppo{
             };
             static constexpr bool NORMALIZE_OBSERVATIONS = true;
             struct PPO_PARAMETERS: rlt::rl::algorithms::ppo::DefaultParameters<TYPE_POLICY, TI, BATCH_SIZE>{
-                static constexpr T ACTION_ENTROPY_COEFFICIENT = 0.0;
+                static constexpr T ACTION_ENTROPY_COEFFICIENT = 0.1;
                 static constexpr TI N_EPOCHS = 1;
                 static constexpr T GAMMA = 0.9;
                 static constexpr T INITIAL_ACTION_STD = 2.0;
