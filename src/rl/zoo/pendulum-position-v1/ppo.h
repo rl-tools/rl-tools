893759858cbf49996a09caec1ec8fe7ab2f4b38e
Jonas Eschmann <jonas.eschmann@gmail.com>
2026-01-27 16:28:18 -0800

    reliable training

--- src/rl/zoo/pendulum-position-v1/ppo.h
+++ src/rl/zoo/pendulum-position-v1/ppo.h
@@ -10,7 +10,7 @@ namespace rl_tools::rl::zoo::pendulum_position_v1::ppo{
         using ENVIRONMENT = typename ENVIRONMENT_FACTORY<DEVICE, TYPE_POLICY, TI>::ENVIRONMENT;
         struct LOOP_CORE_PARAMETERS: rlt::rl::algorithms::ppo::loop::core::DefaultParameters<TYPE_POLICY, TI, ENVIRONMENT>{
             using T = typename TYPE_POLICY::DEFAULT;
-            static constexpr TI N_ENVIRONMENTS = 16;
+            static constexpr TI N_ENVIRONMENTS = 256;
             static constexpr TI ON_POLICY_RUNNER_STEPS_PER_ENV = ENVIRONMENT::EPISODE_STEP_LIMIT;
             static constexpr TI BATCH_SIZE = N_ENVIRONMENTS * ON_POLICY_RUNNER_STEPS_PER_ENV;
             static constexpr TI TOTAL_STEP_LIMIT = 1000*1000*1000;
@@ -21,11 +21,11 @@ namespace rl_tools::rl::zoo::pendulum_position_v1::ppo{
             static constexpr TI STEP_LIMIT = TOTAL_STEP_LIMIT/(ON_POLICY_RUNNER_STEPS_PER_ENV * N_ENVIRONMENTS) + 1;
             static constexpr TI EPISODE_STEP_LIMIT = ENVIRONMENT::EPISODE_STEP_LIMIT;
             struct OPTIMIZER_PARAMETERS: rlt::nn::optimizers::adam::DEFAULT_PARAMETERS_TENSORFLOW<TYPE_POLICY>{
-                static constexpr T ALPHA = 0.001;
+                static constexpr T ALPHA = 0.0003;
             };
             static constexpr bool NORMALIZE_OBSERVATIONS = true;
             struct PPO_PARAMETERS: rlt::rl::algorithms::ppo::DefaultParameters<TYPE_POLICY, TI, BATCH_SIZE>{
-                static constexpr T ACTION_ENTROPY_COEFFICIENT = 0.1;
+                static constexpr T ACTION_ENTROPY_COEFFICIENT = 0.0;
                 static constexpr TI N_EPOCHS = 1;
                 static constexpr T GAMMA = 0.99;
                 static constexpr T INITIAL_ACTION_STD = 0.3;
