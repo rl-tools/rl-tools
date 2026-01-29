1be780e0efb618634e83e64a5490900ab96a0530
Jonas Eschmann <jonas.eschmann@gmail.com>
2026-01-29 10:43:23 -0800

    very good ppo mlp behavior

--- src/rl/zoo/flag/ppo_gru.h
+++ src/rl/zoo/flag/ppo_gru.h
@@ -11,19 +11,19 @@ namespace rl_tools::rl::zoo::flag::ppo_gru{
         using ENVIRONMENT = typename ENVIRONMENT_FACTORY<DEVICE, TYPE_POLICY, TI>::ENVIRONMENT;
         struct LOOP_CORE_PARAMETERS: rlt::rl::algorithms::ppo::loop::core::DefaultParameters<TYPE_POLICY, TI, ENVIRONMENT>{
             static constexpr TI N_ENVIRONMENTS = 64;
-            static constexpr TI ON_POLICY_RUNNER_STEPS_PER_ENV = 64;
+            static constexpr TI ON_POLICY_RUNNER_STEPS_PER_ENV = ENVIRONMENT::EPISODE_STEP_LIMIT;
             static constexpr TI BATCH_SIZE = N_ENVIRONMENTS*ON_POLICY_RUNNER_STEPS_PER_ENV;
-            static constexpr TI TOTAL_STEP_LIMIT = 10 * ((TI)1000 * 1000 * 1000);
-            static constexpr TI ACTOR_HIDDEN_DIM = 32;
+            static constexpr TI TOTAL_STEP_LIMIT = 1 * ((TI)100 * 1000 * 1000);
+            static constexpr TI ACTOR_HIDDEN_DIM = 64;
             static constexpr TI ACTOR_NUM_LAYERS = 2;
-            static constexpr TI CRITIC_HIDDEN_DIM = 32;
+            static constexpr TI CRITIC_HIDDEN_DIM = 64;
             static constexpr TI CRITIC_NUM_LAYERS = 2;
             static constexpr auto ACTOR_ACTIVATION_FUNCTION = rlt::nn::activation_functions::ActivationFunction::RELU;
             static constexpr auto CRITIC_ACTIVATION_FUNCTION = rlt::nn::activation_functions::ActivationFunction::RELU;
             static constexpr TI STEP_LIMIT = TOTAL_STEP_LIMIT/(ON_POLICY_RUNNER_STEPS_PER_ENV * N_ENVIRONMENTS) + 1;
             static constexpr TI EPISODE_STEP_LIMIT = ENVIRONMENT::EPISODE_STEP_LIMIT;
             struct ACTOR_OPTIMIZER_PARAMETERS: rlt::nn::optimizers::adam::DEFAULT_PARAMETERS_TENSORFLOW<TYPE_POLICY>{
-                static constexpr T ALPHA = 0.0003;
+                static constexpr T ALPHA = 0.001;
             };
             struct CRITIC_OPTIMIZER_PARAMETERS: rlt::nn::optimizers::adam::DEFAULT_PARAMETERS_TENSORFLOW<TYPE_POLICY>{
                 static constexpr T ALPHA = 0.001;
@@ -32,7 +32,7 @@ namespace rl_tools::rl::zoo::flag::ppo_gru{
             struct PPO_PARAMETERS: rlt::rl::algorithms::ppo::DefaultParameters<TYPE_POLICY, TI, BATCH_SIZE>{
                 static constexpr T ACTION_ENTROPY_COEFFICIENT = 0.0;
                 static constexpr TI N_EPOCHS = 1;
-                static constexpr T GAMMA = 0.995;
+                static constexpr T GAMMA = 0.98;
                 static constexpr T LAMBDA = 0.95;
                 static constexpr T INITIAL_ACTION_STD = 0.2;
                 static constexpr bool LEARN_ACTION_STD = false;
@@ -40,7 +40,7 @@ namespace rl_tools::rl::zoo::flag::ppo_gru{
                 static constexpr bool STATEFUL_ACTOR_AND_CRITIC = true;
             };
         };
-        using LOOP_CORE_CONFIG = rlt::rl::algorithms::ppo::loop::core::Config<TYPE_POLICY, TI, RNG, ENVIRONMENT, LOOP_CORE_PARAMETERS, rlt::rl::algorithms::ppo::loop::core::ConfigApproximatorsGRU, DYNAMIC_ALLOCATION>;
+        using LOOP_CORE_CONFIG = rlt::rl::algorithms::ppo::loop::core::Config<TYPE_POLICY, TI, RNG, ENVIRONMENT, LOOP_CORE_PARAMETERS, rlt::rl::algorithms::ppo::loop::core::ConfigApproximatorsSequential, DYNAMIC_ALLOCATION>;
     };
 }
 RL_TOOLS_NAMESPACE_WRAPPER_END
