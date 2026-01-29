25fab88d34389fb59dfd797b8226bcb6c9ff8d01
Jonas Eschmann <jonas.eschmann@gmail.com>
2026-01-29 11:09:41 -0800

    ppo flag good

--- src/rl/zoo/flag/ppo.h
+++ src/rl/zoo/flag/ppo.h
@@ -13,13 +13,13 @@ namespace rl_tools::rl::zoo::flag::ppo{
         static constexpr bool CRITIC_PRIVILEGED_OBSERVATION = false;
         using ENVIRONMENT = typename ENVIRONMENT_FACTORY<DEVICE, TYPE_POLICY, TI, MAX_EPISODE_LENGTH, ACTOR_PRIVILEGED_OBSERVATION, CRITIC_PRIVILEGED_OBSERVATION>::ENVIRONMENT;
         struct LOOP_CORE_PARAMETERS: rlt::rl::algorithms::ppo::loop::core::DefaultParameters<TYPE_POLICY, TI, ENVIRONMENT>{
-            static constexpr TI N_ENVIRONMENTS = 64;
+            static constexpr TI N_ENVIRONMENTS = 128;
             static constexpr TI ON_POLICY_RUNNER_STEPS_PER_ENV = ENVIRONMENT::EPISODE_STEP_LIMIT;
             static constexpr TI BATCH_SIZE = N_ENVIRONMENTS*ON_POLICY_RUNNER_STEPS_PER_ENV;
-            static constexpr TI TOTAL_STEP_LIMIT = 1 * ((TI)100 * 1000 * 1000);
-            static constexpr TI ACTOR_HIDDEN_DIM = 64;
+            static constexpr TI TOTAL_STEP_LIMIT = 10 * ((TI)100 * 1000 * 1000);
+            static constexpr TI ACTOR_HIDDEN_DIM = 16;
             static constexpr TI ACTOR_NUM_LAYERS = 2;
-            static constexpr TI CRITIC_HIDDEN_DIM = 64;
+            static constexpr TI CRITIC_HIDDEN_DIM = 16;
             static constexpr TI CRITIC_NUM_LAYERS = 2;
             static constexpr auto ACTOR_ACTIVATION_FUNCTION = rlt::nn::activation_functions::ActivationFunction::RELU;
             static constexpr auto CRITIC_ACTIVATION_FUNCTION = rlt::nn::activation_functions::ActivationFunction::RELU;
@@ -33,12 +33,12 @@ namespace rl_tools::rl::zoo::flag::ppo{
             };
             static constexpr bool NORMALIZE_OBSERVATIONS = true;
             struct PPO_PARAMETERS: rlt::rl::algorithms::ppo::DefaultParameters<TYPE_POLICY, TI, BATCH_SIZE>{
-                static constexpr T ACTION_ENTROPY_COEFFICIENT = 0.0;
+                static constexpr T ACTION_ENTROPY_COEFFICIENT = 0.001;
                 static constexpr TI N_EPOCHS = 1;
-                static constexpr T GAMMA = 0.98;
+                static constexpr T GAMMA = 0.99;
                 static constexpr T LAMBDA = 0.95;
                 static constexpr T INITIAL_ACTION_STD = 0.2;
-                static constexpr bool LEARN_ACTION_STD = false;
+                static constexpr bool LEARN_ACTION_STD = true;
             };
         };
         using LOOP_CORE_CONFIG = rlt::rl::algorithms::ppo::loop::core::Config<TYPE_POLICY, TI, RNG, ENVIRONMENT, LOOP_CORE_PARAMETERS, rlt::rl::algorithms::ppo::loop::core::ConfigApproximatorsSequential, DYNAMIC_ALLOCATION>;
