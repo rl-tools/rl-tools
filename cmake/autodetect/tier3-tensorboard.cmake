c66a08d7c0705504c72351c92a0149ef9bcc7534
Jonas Eschmann <jonas.eschmann@gmail.com>
2026-01-29 19:01:19 -0800

    Protobuf_PROTOC_EXECUTABLE not required

--- cmake/autodetect/tier3-tensorboard.cmake
+++ cmake/autodetect/tier3-tensorboard.cmake
@@ -1,27 +1,23 @@
 if(NOT RL_TOOLS_DISABLE_TENSORBOARD)
     find_package(Protobuf CONFIG REQUIRED)
     if(NOT Protobuf_FOUND)
+        message(STATUS "Protobuf not found in config mode, trying to find it with CMake config.")
         find_package(Protobuf QUIET)
         set(RL_TOOLS_PROTOBUF_NO_CONFIG_PACKAGE ON)
     endif()
     find_package(Git QUIET)
     if(Protobuf_FOUND)
-        if(Protobuf_PROTOC_EXECUTABLE)
-            if(GIT_FOUND)
-                FetchContent_Declare(tensorboard
-                        GIT_REPOSITORY https://github.com/rl-tools/tensorboard_logger.git
-                        GIT_TAG   e1c368611062ec358af8e9e29bda8b76fd4f4910
-                )
-                FetchContent_MakeAvailable(tensorboard)
-                target_link_libraries(rl_tools_full INTERFACE tensorboard_logger)
-                target_compile_definitions(rl_tools_full INTERFACE RL_TOOLS_ENABLE_TENSORBOARD)
-                set(RL_TOOLS_ENABLE_TENSORBOARD ON)
-            else()
-                message(STATUS "Git not found, TensorBoard disabled.")
-                return()
-            endif()
+        if(GIT_FOUND)
+            FetchContent_Declare(tensorboard
+                    GIT_REPOSITORY https://github.com/rl-tools/tensorboard_logger.git
+                    GIT_TAG   e1c368611062ec358af8e9e29bda8b76fd4f4910
+            )
+            FetchContent_MakeAvailable(tensorboard)
+            target_link_libraries(rl_tools_full INTERFACE tensorboard_logger)
+            target_compile_definitions(rl_tools_full INTERFACE RL_TOOLS_ENABLE_TENSORBOARD)
+            set(RL_TOOLS_ENABLE_TENSORBOARD ON)
         else()
-            message(STATUS "Protobuf_PROTOC_EXECUTABLE not found, TensorBoard disabled.")
+            message(STATUS "Git not found, TensorBoard disabled.")
             return()
         endif()
     else()
