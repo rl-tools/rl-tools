0c3b519c554386d2a476cf1c87c1a827212de3fb
Jonas Eschmann <jonas.eschmann@gmail.com>
2026-02-01 21:04:28 -0800

    fixed optimizer persist

--- include/rl_tools/nn/optimizers/adam/persist.h
+++ include/rl_tools/nn/optimizers/adam/persist.h
@@ -7,13 +7,15 @@ RL_TOOLS_NAMESPACE_WRAPPER_START
 namespace rl_tools{
     template<typename DEVICE, typename SPEC, typename GROUP>
     void save(DEVICE& device, nn::optimizers::Adam<SPEC>& optimizer, GROUP& group) {
+        save_binary(device, optimizer.parameters._data, 1, group, "parameters");
         save(device, optimizer.age, group, "age");
         save(device, optimizer.first_order_moment_bias_correction, group, "first_order_moment_bias_correction");
         save(device, optimizer.second_order_moment_bias_correction, group, "second_order_moment_bias_correction");
     }
     template<typename DEVICE, typename SPEC, typename GROUP>
     bool load(DEVICE& device, nn::optimizers::Adam<SPEC>& optimizer, GROUP& group) {
-        bool success = load(device, optimizer.age, group, "age");
+        bool success = load_binary(device, optimizer.parameters._data, 1, group, "parameters");
+        success &= load(device, optimizer.age, group, "age");
         success &= load(device, optimizer.first_order_moment_bias_correction, group, "first_order_moment_bias_correction");
         success &= load(device, optimizer.second_order_moment_bias_correction, group, "second_order_moment_bias_correction");
         return success;
