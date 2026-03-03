a804b7044b63e63775be6ad6c777f9372394093d
Jonas Eschmann <jonas.eschmann@gmail.com>
2026-03-02 18:39:07 -0800

    cleaning

--- include/rl_tools/rendering/raytracing/backends/optix/device.h
+++ include/rl_tools/rendering/raytracing/backends/optix/device.h
@@ -1,8 +1,3 @@
-// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-
-
 #pragma once
 
 #include <owl/owl.h>
