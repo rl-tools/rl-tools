eee84fe369faf8346723dfdf666eab693c028c72
Jonas Eschmann <jonas.eschmann@gmail.com>
2026-03-06 21:05:15 -0800

    bump test data

--- tests/download_data.sh
+++ tests/download_data.sh
@@ -1,4 +1,4 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 git clone -b master https://huggingface.co/datasets/rl-tools/test-data $SCRIPT_DIR/data
 cd $SCRIPT_DIR/data
-git checkout b45b6bed966f51b2039f678eea42af500cbc615c
+git checkout 6d0b5ea3845303b669d7687e5d1d1cf0d25121db
