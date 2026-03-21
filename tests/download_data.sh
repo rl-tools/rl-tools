f50bb2406dd656171dc995236f3985e483b27ce7
Jonas Eschmann <jonas.eschmann@gmail.com>
2026-03-21 01:33:33 -0700

    all tests passing [ubuntu]

--- tests/download_data.sh
+++ tests/download_data.sh
@@ -2,4 +2,4 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 git clone -b master "${RL_TOOLS_TEST_DATA_SOURCE:-https://huggingface.co/datasets/rl-tools/test-data}" $SCRIPT_DIR/data
 cd $SCRIPT_DIR/data
 git pull
-git checkout 578c6138f57e80f51f705c6486cf4415b005f38f
+git checkout 517ef18fc7876d04b7ac5815ee69ce17f3d1f50f
