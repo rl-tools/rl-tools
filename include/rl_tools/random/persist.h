97101197aabbcd6cb0ec27d3911f2d8720ee8f12
Jonas Eschmann <jonas.eschmann@gmail.com>
2026-03-23 02:07:24 -0700

    cleaning up ArrayENGINE persist

--- include/rl_tools/random/persist.h
+++ include/rl_tools/random/persist.h
@@ -8,7 +8,7 @@
 RL_TOOLS_NAMESPACE_WRAPPER_START
 namespace rl_tools{
     namespace random_persist{
-        constexpr auto RNG_STATE_MAX_SIZE = 16384; // mt19937 state is ~5KB, ArrayENGINE<1024> as text is ~11KB
+        constexpr auto RNG_STATE_MAX_SIZE = 8192; // mt19937 state is ~5KB
     }
     template<typename DEV_SPEC, typename SPEC, typename GROUP>
     void save(devices::CPU<DEV_SPEC>& device, devices::random::CPU::ENGINE<SPEC>& rng, GROUP& group) {
@@ -41,45 +41,35 @@ namespace rl_tools{
         ss >> rng.engine;
         return !ss.fail();
     }
-    // ArrayENGINE save/load: serialize states as string of space-separated unsigned ints
+    // ArrayENGINE: save states as a double tensor (double has 53-bit mantissa, exactly represents all uint32 values)
     template<typename DEV_SPEC, typename SPEC, typename GROUP>
     void save(devices::CPU<DEV_SPEC>& device, devices::generic::random::ArrayENGINE<SPEC>& rng, GROUP& group) {
         using TI = typename devices::CPU<DEV_SPEC>::index_t;
         constexpr TI NUM_RNGS = SPEC::NUM_RNGS;
-        std::stringstream ss;
-        for(TI i = 0; i < NUM_RNGS; i++){
-            auto element = get(rng.states, 0, i);
-            if(i > 0) ss << " ";
-            ss << element.state;
-        }
-        std::string state_str = ss.str();
-        utils::assert_exit(device, state_str.length() < random_persist::RNG_STATE_MAX_SIZE, "ArrayENGINE state string exceeds maximum size");
-        Tensor<tensor::Specification<uint8_t, TI, tensor::Shape<TI, random_persist::RNG_STATE_MAX_SIZE>>> state_tensor;
+        Tensor<tensor::Specification<double, TI, tensor::Shape<TI, NUM_RNGS>>> state_tensor;
         malloc(device, state_tensor);
-        std::memset(data(state_tensor), 0, random_persist::RNG_STATE_MAX_SIZE);
-        std::memcpy(data(state_tensor), state_str.c_str(), state_str.length() + 1);
-        save(device, state_tensor, group, "state");
+        for(TI i = 0; i < NUM_RNGS; i++){
+            set(device, state_tensor, (double)get(rng.states, 0, i).state, i);
+        }
+        save(device, state_tensor, group, "states");
         free(device, state_tensor);
     }
     template<typename DEV_SPEC, typename SPEC, typename GROUP>
     bool load(devices::CPU<DEV_SPEC>& device, devices::generic::random::ArrayENGINE<SPEC>& rng, GROUP& group) {
         using TI = typename devices::CPU<DEV_SPEC>::index_t;
         constexpr TI NUM_RNGS = SPEC::NUM_RNGS;
-        Tensor<tensor::Specification<uint8_t, TI, tensor::Shape<TI, random_persist::RNG_STATE_MAX_SIZE>>> state_tensor;
+        Tensor<tensor::Specification<double, TI, tensor::Shape<TI, NUM_RNGS>>> state_tensor;
         malloc(device, state_tensor);
-        if(!load(device, state_tensor, group, "state")){
+        if(!load(device, state_tensor, group, "states")){
             free(device, state_tensor);
             return false;
         }
-        std::string state_str(reinterpret_cast<const char*>(data(state_tensor)));
-        free(device, state_tensor);
-        std::stringstream ss(state_str);
         for(TI i = 0; i < NUM_RNGS; i++){
             devices::generic::random::PortableState element;
-            ss >> element.state;
-            if(ss.fail()) return false;
+            element.state = static_cast<unsigned int>(get(device, state_tensor, i));
             set(rng.states, 0, i, element);
         }
+        free(device, state_tensor);
         return true;
     }
 }
