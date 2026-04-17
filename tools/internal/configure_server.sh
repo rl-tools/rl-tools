340c21d68168904d6c1f39270790813c8a329478
Jonas Eschmann <jonas.eschmann@gmail.com>
2026-04-17 14:49:26 -0700

    arch

--- tools/internal/configure_server.sh
+++ tools/internal/configure_server.sh
@@ -1,4 +1,4 @@
-CUDACXX=/home/jonas/.local/opt/cuda-12.8.2/bin/nvcc cmake -B build \
+CUDACXX=/home/jonas/.local/opt/cuda-12.8.2/bin/nvcc CUDAARCHS=90 cmake -B build \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_PREFIX_PATH="$HOME/.local/opt/cudnn" \
   -DRL_TOOLS_EXPERIMENTAL=ON \
